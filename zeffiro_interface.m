6ab80d9610c7a59d90e4c52180bdb29c8dd1fc61
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2019-02-17 18:20:36 +0200

    Add files via upload

--- zeffiro_interface.m
+++ zeffiro_interface.m
@@ -192,8 +192,8 @@ set(zef.h_popupmenu2,'string',{'Cartesian','Normal','Basis'});
 set(zef.h_visualization_type,'string',{'Sigma','Recon. (volume)','Recon. (surface)'});
 set(zef.h_pushbutton31,'string','Visualize volume')
 zef.o_h = findall(zef.h_zeffiro);
-zef.h_axes2 = zef.o_h(368);
-zef.h_axes1 = zef.o_h(367);
+zef.h_axes2 = zef.o_h(372);
+zef.h_axes1 = zef.o_h(371);
 zef=rmfield(zef,'o_h');
 zef.clear_axes1 = 0;
 zef_update;
