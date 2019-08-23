788c613f26f70661862281192e6ecd133319e3b2
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2019-08-23 15:33:00 +0300

    Update fs2zef.sh

--- data/export_and_import_scripts/fs2zef.sh
+++ data/export_and_import_scripts/fs2zef.sh
@@ -1,4 +1,4 @@
-#Commands needed for extracting data of MikJo from Freesurefer to Zeffiro. 
+#Commands needed for extracting data from Freesurefer to Zeffiro. 
 #Atena Rezaei, 2019.
 #System specific variable definitions
 
@@ -93,10 +93,10 @@ mris_convert $SUBJECT_DIR/$SUBJECT/surf/outer_inner_skull_surface $OUT_DIR/inner
 #Cortical Parcellation
 #Command 6:
 
-mri_annotation2label  --subject $SUBJECT --annotation aparc.a2009s --hemi lh --ctab aparc.annot.a2009s --outdir $OUT_DIR/lh.aparc_76
-mri_annotation2label  --subject $SUBJECT --annotation aparc.a2009s --hemi rh --ctab aparc.annot.a2009s --outdir $OUT_DIR/rh.aparc_76
-mri_annotation2label  --subject $SUBJECT --annotation aparc --hemi lh --ctab aparc.annot --outdir $OUT_DIR/lh.aparc_36
-mri_annotation2label  --subject $SUBJECT --annotation aparc --hemi rh --ctab aparc.annot --outdir $OUT_DIR/rh.aparc_36
+mri_annotation2label --subject $SUBJECT  --sd $SUBJECT_DIR --annotation aparc.a2009s --hemi lh --ctab aparc.annot.a2009s --outdir $OUT_DIR/lh.aparc_76
+mri_annotation2label --subject $SUBJECT  --sd $SUBJECT_DIR --annotation aparc.a2009s --hemi rh --ctab aparc.annot.a2009s --outdir $OUT_DIR/rh.aparc_76
+mri_annotation2label --subject $SUBJECT  --sd $SUBJECT_DIR --annotation aparc --hemi lh --ctab aparc.annot --outdir $OUT_DIR/lh.aparc_36
+mri_annotation2label --subject $SUBJECT  --sd $SUBJECT_DIR --annotation aparc --hemi rh --ctab aparc.annot --outdir $OUT_DIR/rh.aparc_36
 
 
 #The data in the .annot files can be read using the matlab script
@@ -112,6 +112,6 @@ mri_mergelabels -d $OUT_DIR/rh.aparc_76/ -o $OUT_DIR/rh_labels_76.asc
 mri_mergelabels -d $OUT_DIR/lh.aparc_36/ -o $OUT_DIR/lh_labels_36.asc
 mri_mergelabels -d $OUT_DIR/rh.aparc_36/ -o $OUT_DIR/rh_labels_36.asc
 
-#matlab -nodisplay -nosplash -nodesktop -r "create_points";
+matlab -nodisplay -nosplash -nodesktop -r "dir_name = '$SUBJECT_DIR/';a = dlmread('lh_labels_76.asc',' ',2,0);a = a(:,[1,3,5,7]);save -ascii lh_points_76.dat a; a = dlmread('rh_labels_76.asc',' ',2,0);a = a(:,[1,3,5,7]); save -ascii rh_points_76.dat a; a = dlmread('lh_labels_36.asc',' ',2,0);a = a(:,[1,3,5,7]);  save -ascii  lh_points_36.dat a; a = dlmread('rh_labels_36.asc',' ',2,0);a = a(:,[1,3,5,7]); save -ascii rh_points_36.dat a";
 
  
