61ed4205800ddde5198614f51a6702dda4e9651b
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2019-12-12 17:49:44 +0200

    Add files via upload

--- m/zef_butterfly_plot.m
+++ m/zef_butterfly_plot.m
@@ -8,15 +8,17 @@ else
 zef.h_butterfly_plot = open('zeffiro_interface_butterfly_plot.fig');
 end
 set(zef.h_butterfly_plot,'Name','ZEFFIRO Interface: Butterfly_plot');
+set(findobj(zef.h_butterfly_plot.Children,'-property','FontUnits'),'FontUnits','pixels')
+set(findobj(zef.h_butterfly_plot.Children,'-property','FontSize'),'FontSize',9);
 zef_init_butterfly_plot;
 if isfield(zef,'measurements')
 if iscell(zef.measurements)
-    set(zef.h_inv_data_segment,'enable','on');
+    set(zef.h_bf_data_segment,'enable','on');
 end
 if not(iscell(zef.measurements))
-    set(zef.h_inv_data_segment,'enable','off'); 
+    set(zef.h_bf_data_segment,'enable','off'); 
 end
 end
-uistack(flipud([zef.h_inv_sampling_frequency ; zef.h_inv_low_cut_frequency ; 
-    zef.h_inv_high_cut_frequency ; zef.h_inv_time_1 ; zef.h_inv_time_2; zef.h_inv_data_segment ; zef.h_inv_cancel ; 
-    zef.h_inv_apply; zef.h_inv_plot  ]),'top'); 
+uistack(flipud([zef.h_bf_sampling_frequency ; zef.h_bf_low_cut_frequency ; 
+    zef.h_bf_high_cut_frequency ; zef.h_bf_time_1 ; zef.h_bf_time_2; zef.h_bf_data_segment ; zef.h_bf_cancel ; 
+    zef.h_bf_apply; zef.h_bf_plot  ]),'top'); 
