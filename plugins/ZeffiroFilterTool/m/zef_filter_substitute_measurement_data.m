a94152ba4629bf119cbf048d203413a76ab6825f
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2019-12-13 04:23:42 +0200

    Add files via upload

---
+++ plugins/ZeffiroFilterTool/m/zef_filter_substitute_measurement_data.m
@@ -0,0 +1,8 @@
+[zef.yesno] = questdlg('Substitute raw data with processed data?','Yes','No'); 
+if isequal(zef.yesno,'Yes');
+zef_filter_raw_data;
+
+zef.raw_data = zef.processed_data;
+
+
+end;
---
+++ plugins/ZeffiroFilterTool/m/zef_filter_substitute_raw_data.m
@@ -0,0 +1,14 @@
+[zef.yesno] = questdlg('Substitute measurement data with processed data?','Yes','No'); 
+if isequal(zef.yesno,'Yes');
+zef_filter_raw_data;
+
+if zef.filter_data_segment > 0 
+    if not(iscell(zef.measurements))
+        zef.measurements = cell(0);
+    end
+zef.measurements{zef.filter_data_segment} = zef.processed_data;
+else
+zef.measurements = zef.processed_data;
+end
+
+end;
