0e227b7ac426fa82a36b24ca3553cccf81df4cbc
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2020-01-17 08:17:39 +0200

    Add files via upload

--- plugins/ZeffiroFilterTool/m/zef_filter_raw_data.m
+++ plugins/ZeffiroFilterTool/m/zef_filter_raw_data.m
@@ -3,11 +3,13 @@ zef.processed_data = zef.raw_data;
 if isstr(zef.filter_pipeline_selected)
     zef.filter_pipeline_selected = {zef.filter_pipeline_selected};
 end
-for zef_i = 1 : length(zef.filter_pipeline_selected)
-    zef_j = find(ismember(zef.filter_pipeline_list,zef.filter_pipeline_selected{zef_i}),1);
+for zef_j = 1 : length(zef.filter_pipeline_list)
+    zef_i = find(ismember(zef.filter_pipeline_selected,zef.filter_pipeline_list{zef_j}),1);
+    if not(isempty(zef_i))
     zef.aux_field = str2func(zef.filter_pipeline{zef_j}.file);
     zef.filter_parameters = zef.filter_pipeline{zef_j}.parameters(:,2);
     zef.processed_data = zef.aux_field(zef.processed_data, zef.filter_parameters{:});
-end
+    end
+    end
 
 clear zef_i zef_j
