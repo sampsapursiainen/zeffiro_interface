eef32d42e526d5cd17f2ccf4574d80e97d7b1465
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2020-06-05 23:22:14 +0300

    Add files via upload

--- zeffiro_interface.m
+++ zeffiro_interface.m
@@ -8,6 +8,7 @@ zef.program_path = cd;
 if not(isdeployed)
 zef.code_path = '/m';
 addpath(genpath([zef.program_path '/m']));
+addpath(genpath([zef.program_path '/mlapp']));
 addpath([zef.program_path '/fig']); 
 addpath(genpath([zef.program_path '/plugins'])); 
 addpath([zef.program_path zef.code_path]); 
@@ -27,9 +28,14 @@ zef.snapshot_vertical_resolution = str2num(zef.ini_cell{1}{14});
 zef.snapshot_horizontal_resolution = str2num(zef.ini_cell{1}{16});
 zef.movie_fps = str2num(zef.ini_cell{1}{18});
 zef.font_size = str2num(zef.ini_cell{1}{20});
+zef.mlapp = str2num(zef.ini_cell{1}{22});
 zef = rmfield(zef,'ini_cell');
-zef_data = zef;
+if zef.mlapp == 1
+zeffiro_interface_segmentation_tool; 
+else
 zef.h_zeffiro_window_main = open('zeffiro_interface_segmentation_tool.fig');
+end
+zef_data = zef;
 set(findobj(zef.h_zeffiro_window_main.Children,'-property','FontUnits'),'FontUnits','pixels')
 set(findobj(zef.h_zeffiro_window_main.Children,'-property','FontSize'),'FontSize',zef.font_size);
 zef_init;
@@ -115,41 +121,77 @@ zef.h_edit429;
 zef.h_edit470;
 zef.h_edit9009;
 ];
+if zef.mlapp == 0
 uistack(flipud(zef.aux_handle_vec),'bottom');
+end
 rmfield(zef,'aux_handle_vec');
 zef.o_h = findall(zef.h_zeffiro_window_main);
-zef.h_axes2 = findobj(zef.o_h,'Type','Axes');
-zef.h_axes2  = zef.h_axes2(1); 
+%zef.h_axes2 = findobj(zef.o_h,'Type','Axes');
+%zef.h_axes2  = zef.h_axes2(1); 
 zef=rmfield(zef,'o_h');
 zef.clear_axes1 = 0;
 
-set(zef.h_pushbutton2,'callback',[get(zef.h_pushbutton2,'callback') 'if zef.w_merge; zef.w_submesh_ind = [zef.w_submesh_ind size(zef.w_triangles,1)]; else zef.w_submesh_ind = [size(zef.w_triangles,1)]; end;']);
-set(zef.h_pushbutton4,'callback',[get(zef.h_pushbutton4,'callback') 'if zef.g_merge; zef.g_submesh_ind = [zef.g_submesh_ind size(zef.g_triangles,1)]; else zef.g_submesh_ind = [size(zef.g_triangles,1)]; end;']);
-set(zef.h_pushbutton6,'callback',[get(zef.h_pushbutton6,'callback') 'if zef.c_merge; zef.c_submesh_ind = [zef.c_submesh_ind size(zef.c_triangles,1)]; else zef.c_submesh_ind = [size(zef.c_triangles,1)]; end;']);
-set(zef.h_pushbutton8,'callback',[get(zef.h_pushbutton8,'callback') 'if zef.sk_merge; zef.sk_submesh_ind = [zef.sk_submesh_ind size(zef.sk_triangles,1)]; else zef.sk_submesh_ind = [size(zef.sk_triangles,1)]; end;']);
-set(zef.h_pushbutton10,'callback',[get(zef.h_pushbutton10,'callback') 'if zef.sc_merge; zef.sc_submesh_ind = [zef.sc_submesh_ind size(zef.sc_triangles,1)]; else zef.sc_submesh_ind = [size(zef.sc_triangles,1)]; end;']);
-set(zef.h_pushbutton102,'callback',[get(zef.h_pushbutton102,'callback') 'if zef.d1_merge; zef.d1_submesh_ind = [zef.d1_submesh_ind size(zef.d1_triangles,1)]; else zef.d1_submesh_ind = [size(zef.d1_triangles,1)]; end;']);
-set(zef.h_pushbutton202,'callback',[get(zef.h_pushbutton202,'callback') 'if zef.d2_merge; zef.d2_submesh_ind = [zef.d2_submesh_ind size(zef.d2_triangles,1)]; else zef.d2_submesh_ind = [size(zef.d2_triangles,1)]; end;']);
-set(zef.h_pushbutton302,'callback',[get(zef.h_pushbutton302,'callback') 'if zef.d3_merge; zef.d3_submesh_ind = [zef.d3_submesh_ind size(zef.d3_triangles,1)]; else zef.d3_submesh_ind = [size(zef.d3_triangles,1)]; end;']);
-set(zef.h_pushbutton402,'callback',[get(zef.h_pushbutton402,'callback') 'if zef.d4_merge; zef.d4_submesh_ind = [zef.d4_submesh_ind size(zef.d4_triangles,1)]; else zef.d4_submesh_ind = [size(zef.d4_triangles,1)]; end;']);
-set(zef.h_d5_button_2,'callback',[get(zef.h_d5_button_2,'callback') 'if zef.d5_merge; zef.d5_submesh_ind = [zef.d5_submesh_ind size(zef.d5_triangles,1)]; else zef.d5_submesh_ind = [size(zef.d5_triangles,1)]; end;']);
-set(zef.h_d6_button_2,'callback',[get(zef.h_d6_button_2,'callback') 'if zef.d6_merge; zef.d6_submesh_ind = [zef.d6_submesh_ind size(zef.d6_triangles,1)]; else zef.d6_submesh_ind = [size(zef.d6_triangles,1)]; end;']);
-set(zef.h_d7_button_2,'callback',[get(zef.h_d7_button_2,'callback') 'if zef.d7_merge; zef.d7_submesh_ind = [zef.d7_submesh_ind size(zef.d7_triangles,1)]; else zef.d7_submesh_ind = [size(zef.d7_triangles,1)]; end;']);
-set(zef.h_d8_button_2,'callback',[get(zef.h_d8_button_2,'callback') 'if zef.d8_merge; zef.d8_submesh_ind = [zef.d8_submesh_ind size(zef.d8_triangles,1)]; else zef.d8_submesh_ind = [size(zef.d8_triangles,1)]; end;']);
-set(zef.h_d9_button_2,'callback',[get(zef.h_d9_button_2,'callback') 'if zef.d9_merge; zef.d9_submesh_ind = [zef.d9_submesh_ind size(zef.d9_triangles,1)]; else zef.d9_submesh_ind = [size(zef.d9_triangles,1)]; end;']);
-set(zef.h_d10_button_2,'callback',[get(zef.h_d10_button_2,'callback') 'if zef.d10_merge; zef.d10_submesh_ind = [zef.d10_submesh_ind size(zef.d10_triangles,1)]; else zef.d10_submesh_ind = [size(zef.d10_triangles,1)]; end;']);
-set(zef.h_d11_button_2,'callback',[get(zef.h_d11_button_2,'callback') 'if zef.d11_merge; zef.d11_submesh_ind = [zef.d11_submesh_ind size(zef.d11_triangles,1)]; else zef.d11_submesh_ind = [size(zef.d11_triangles,1)]; end;']);
-set(zef.h_d12_button_2,'callback',[get(zef.h_d12_button_2,'callback') 'if zef.d12_merge; zef.d12_submesh_ind = [zef.d12_submesh_ind size(zef.d12_triangles,1)]; else zef.d12_submesh_ind = [size(zef.d12_triangles,1)]; end;']);
-set(zef.h_d13_button_2,'callback',[get(zef.h_d13_button_2,'callback') 'if zef.d13_merge; zef.d13_submesh_ind = [zef.d13_submesh_ind size(zef.d13_triangles,1)]; else zef.d13_submesh_ind = [size(zef.d13_triangles,1)]; end;']);
-set(zef.h_d14_button_2,'callback',[get(zef.h_d14_button_2,'callback') 'if zef.d14_merge; zef.d14_submesh_ind = [zef.d14_submesh_ind size(zef.d14_triangles,1)]; else zef.d14_submesh_ind = [size(zef.d14_triangles,1)]; end;']);
-set(zef.h_d15_button_2,'callback',[get(zef.h_d15_button_2,'callback') 'if zef.d15_merge; zef.d15_submesh_ind = [zef.d15_submesh_ind size(zef.d15_triangles,1)]; else zef.d15_submesh_ind = [size(zef.d15_triangles,1)]; end;']);
-set(zef.h_d16_button_2,'callback',[get(zef.h_d16_button_2,'callback') 'if zef.d16_merge; zef.d16_submesh_ind = [zef.d16_submesh_ind size(zef.d16_triangles,1)]; else zef.d16_submesh_ind = [size(zef.d16_triangles,1)]; end;']);
-set(zef.h_d17_button_2,'callback',[get(zef.h_d17_button_2,'callback') 'if zef.d17_merge; zef.d17_submesh_ind = [zef.d17_submesh_ind size(zef.d17_triangles,1)]; else zef.d17_submesh_ind = [size(zef.d17_triangles,1)]; end;']);
-set(zef.h_d18_button_2,'callback',[get(zef.h_d18_button_2,'callback') 'if zef.d18_merge; zef.d18_submesh_ind = [zef.d18_submesh_ind size(zef.d18_triangles,1)]; else zef.d18_submesh_ind = [size(zef.d18_triangles,1)]; end;']);
-set(zef.h_d19_button_2,'callback',[get(zef.h_d19_button_2,'callback') 'if zef.d19_merge; zef.d19_submesh_ind = [zef.d19_submesh_ind size(zef.d19_triangles,1)]; else zef.d19_submesh_ind = [size(zef.d19_triangles,1)]; end;']);
-set(zef.h_d20_button_2,'callback',[get(zef.h_d20_button_2,'callback') 'if zef.d20_merge; zef.d20_submesh_ind = [zef.d20_submesh_ind size(zef.d20_triangles,1)]; else zef.d20_submesh_ind = [size(zef.d20_triangles,1)]; end;']);
-set(zef.h_d21_button_2,'callback',[get(zef.h_d21_button_2,'callback') 'if zef.d21_merge; zef.d21_submesh_ind = [zef.d21_submesh_ind size(zef.d21_triangles,1)]; else zef.d21_submesh_ind = [size(zef.d21_triangles,1)]; end;']);
-set(zef.h_d22_button_2,'callback',[get(zef.h_d22_button_2,'callback') 'if zef.d22_merge; zef.d22_submesh_ind = [zef.d22_submesh_ind size(zef.d22_triangles,1)]; else zef.d22_submesh_ind = [size(zef.d22_triangles,1)]; end;']);
+if zef.mlapp == 1
+
+set(zef.h_pushbutton2,'ButtonPushedFcn',[get(zef.h_pushbutton2,'ButtonPushedFcn') 'if zef.w_merge; zef.w_submesh_ind = [zef.w_submesh_ind size(zef.w_triangles,1)]; else zef.w_submesh_ind = [size(zef.w_triangles,1)]; end;']);
+set(zef.h_pushbutton4,'ButtonPushedFcn',[get(zef.h_pushbutton4,'ButtonPushedFcn') 'if zef.g_merge; zef.g_submesh_ind = [zef.g_submesh_ind size(zef.g_triangles,1)]; else zef.g_submesh_ind = [size(zef.g_triangles,1)]; end;']);
+set(zef.h_pushbutton6,'ButtonPushedFcn',[get(zef.h_pushbutton6,'ButtonPushedFcn') 'if zef.c_merge; zef.c_submesh_ind = [zef.c_submesh_ind size(zef.c_triangles,1)]; else zef.c_submesh_ind = [size(zef.c_triangles,1)]; end;']);
+set(zef.h_pushbutton8,'ButtonPushedFcn',[get(zef.h_pushbutton8,'ButtonPushedFcn') 'if zef.sk_merge; zef.sk_submesh_ind = [zef.sk_submesh_ind size(zef.sk_triangles,1)]; else zef.sk_submesh_ind = [size(zef.sk_triangles,1)]; end;']);
+set(zef.h_pushbutton10,'ButtonPushedFcn',[get(zef.h_pushbutton10,'ButtonPushedFcn') 'if zef.sc_merge; zef.sc_submesh_ind = [zef.sc_submesh_ind size(zef.sc_triangles,1)]; else zef.sc_submesh_ind = [size(zef.sc_triangles,1)]; end;']);
+set(zef.h_pushbutton102,'ButtonPushedFcn',[get(zef.h_pushbutton102,'ButtonPushedFcn') 'if zef.d1_merge; zef.d1_submesh_ind = [zef.d1_submesh_ind size(zef.d1_triangles,1)]; else zef.d1_submesh_ind = [size(zef.d1_triangles,1)]; end;']);
+set(zef.h_pushbutton202,'ButtonPushedFcn',[get(zef.h_pushbutton202,'ButtonPushedFcn') 'if zef.d2_merge; zef.d2_submesh_ind = [zef.d2_submesh_ind size(zef.d2_triangles,1)]; else zef.d2_submesh_ind = [size(zef.d2_triangles,1)]; end;']);
+set(zef.h_pushbutton302,'ButtonPushedFcn',[get(zef.h_pushbutton302,'ButtonPushedFcn') 'if zef.d3_merge; zef.d3_submesh_ind = [zef.d3_submesh_ind size(zef.d3_triangles,1)]; else zef.d3_submesh_ind = [size(zef.d3_triangles,1)]; end;']);
+set(zef.h_pushbutton402,'ButtonPushedFcn',[get(zef.h_pushbutton402,'ButtonPushedFcn') 'if zef.d4_merge; zef.d4_submesh_ind = [zef.d4_submesh_ind size(zef.d4_triangles,1)]; else zef.d4_submesh_ind = [size(zef.d4_triangles,1)]; end;']);
+set(zef.h_d5_button_2,'ButtonPushedFcn',[get(zef.h_d5_button_2,'ButtonPushedFcn') 'if zef.d5_merge; zef.d5_submesh_ind = [zef.d5_submesh_ind size(zef.d5_triangles,1)]; else zef.d5_submesh_ind = [size(zef.d5_triangles,1)]; end;']);
+set(zef.h_d6_button_2,'ButtonPushedFcn',[get(zef.h_d6_button_2,'ButtonPushedFcn') 'if zef.d6_merge; zef.d6_submesh_ind = [zef.d6_submesh_ind size(zef.d6_triangles,1)]; else zef.d6_submesh_ind = [size(zef.d6_triangles,1)]; end;']);
+set(zef.h_d7_button_2,'ButtonPushedFcn',[get(zef.h_d7_button_2,'ButtonPushedFcn') 'if zef.d7_merge; zef.d7_submesh_ind = [zef.d7_submesh_ind size(zef.d7_triangles,1)]; else zef.d7_submesh_ind = [size(zef.d7_triangles,1)]; end;']);
+set(zef.h_d8_button_2,'ButtonPushedFcn',[get(zef.h_d8_button_2,'ButtonPushedFcn') 'if zef.d8_merge; zef.d8_submesh_ind = [zef.d8_submesh_ind size(zef.d8_triangles,1)]; else zef.d8_submesh_ind = [size(zef.d8_triangles,1)]; end;']);
+set(zef.h_d9_button_2,'ButtonPushedFcn',[get(zef.h_d9_button_2,'ButtonPushedFcn') 'if zef.d9_merge; zef.d9_submesh_ind = [zef.d9_submesh_ind size(zef.d9_triangles,1)]; else zef.d9_submesh_ind = [size(zef.d9_triangles,1)]; end;']);
+set(zef.h_d10_button_2,'ButtonPushedFcn',[get(zef.h_d10_button_2,'ButtonPushedFcn') 'if zef.d10_merge; zef.d10_submesh_ind = [zef.d10_submesh_ind size(zef.d10_triangles,1)]; else zef.d10_submesh_ind = [size(zef.d10_triangles,1)]; end;']);
+set(zef.h_d11_button_2,'ButtonPushedFcn',[get(zef.h_d11_button_2,'ButtonPushedFcn') 'if zef.d11_merge; zef.d11_submesh_ind = [zef.d11_submesh_ind size(zef.d11_triangles,1)]; else zef.d11_submesh_ind = [size(zef.d11_triangles,1)]; end;']);
+set(zef.h_d12_button_2,'ButtonPushedFcn',[get(zef.h_d12_button_2,'ButtonPushedFcn') 'if zef.d12_merge; zef.d12_submesh_ind = [zef.d12_submesh_ind size(zef.d12_triangles,1)]; else zef.d12_submesh_ind = [size(zef.d12_triangles,1)]; end;']);
+set(zef.h_d13_button_2,'ButtonPushedFcn',[get(zef.h_d13_button_2,'ButtonPushedFcn') 'if zef.d13_merge; zef.d13_submesh_ind = [zef.d13_submesh_ind size(zef.d13_triangles,1)]; else zef.d13_submesh_ind = [size(zef.d13_triangles,1)]; end;']);
+set(zef.h_d14_button_2,'ButtonPushedFcn',[get(zef.h_d14_button_2,'ButtonPushedFcn') 'if zef.d14_merge; zef.d14_submesh_ind = [zef.d14_submesh_ind size(zef.d14_triangles,1)]; else zef.d14_submesh_ind = [size(zef.d14_triangles,1)]; end;']);
+set(zef.h_d15_button_2,'ButtonPushedFcn',[get(zef.h_d15_button_2,'ButtonPushedFcn') 'if zef.d15_merge; zef.d15_submesh_ind = [zef.d15_submesh_ind size(zef.d15_triangles,1)]; else zef.d15_submesh_ind = [size(zef.d15_triangles,1)]; end;']);
+set(zef.h_d16_button_2,'ButtonPushedFcn',[get(zef.h_d16_button_2,'ButtonPushedFcn') 'if zef.d16_merge; zef.d16_submesh_ind = [zef.d16_submesh_ind size(zef.d16_triangles,1)]; else zef.d16_submesh_ind = [size(zef.d16_triangles,1)]; end;']);
+set(zef.h_d17_button_2,'ButtonPushedFcn',[get(zef.h_d17_button_2,'ButtonPushedFcn') 'if zef.d17_merge; zef.d17_submesh_ind = [zef.d17_submesh_ind size(zef.d17_triangles,1)]; else zef.d17_submesh_ind = [size(zef.d17_triangles,1)]; end;']);
+set(zef.h_d18_button_2,'ButtonPushedFcn',[get(zef.h_d18_button_2,'ButtonPushedFcn') 'if zef.d18_merge; zef.d18_submesh_ind = [zef.d18_submesh_ind size(zef.d18_triangles,1)]; else zef.d18_submesh_ind = [size(zef.d18_triangles,1)]; end;']);
+set(zef.h_d19_button_2,'ButtonPushedFcn',[get(zef.h_d19_button_2,'ButtonPushedFcn') 'if zef.d19_merge; zef.d19_submesh_ind = [zef.d19_submesh_ind size(zef.d19_triangles,1)]; else zef.d19_submesh_ind = [size(zef.d19_triangles,1)]; end;']);
+set(zef.h_d20_button_2,'ButtonPushedFcn',[get(zef.h_d20_button_2,'ButtonPushedFcn') 'if zef.d20_merge; zef.d20_submesh_ind = [zef.d20_submesh_ind size(zef.d20_triangles,1)]; else zef.d20_submesh_ind = [size(zef.d20_triangles,1)]; end;']);
+set(zef.h_d21_button_2,'ButtonPushedFcn',[get(zef.h_d21_button_2,'ButtonPushedFcn') 'if zef.d21_merge; zef.d21_submesh_ind = [zef.d21_submesh_ind size(zef.d21_triangles,1)]; else zef.d21_submesh_ind = [size(zef.d21_triangles,1)]; end;']);
+set(zef.h_d22_button_2,'ButtonPushedFcn',[get(zef.h_d22_button_2,'ButtonPushedFcn') 'if zef.d22_merge; zef.d22_submesh_ind = [zef.d22_submesh_ind size(zef.d22_triangles,1)]; else zef.d22_submesh_ind = [size(zef.d22_triangles,1)]; end;']);
+
+else
+    
+set(zef.h_pushbutton2,'Callback',[get(zef.h_pushbutton2,'Callback') 'if zef.w_merge; zef.w_submesh_ind = [zef.w_submesh_ind size(zef.w_triangles,1)]; else zef.w_submesh_ind = [size(zef.w_triangles,1)]; end;']);
+set(zef.h_pushbutton4,'Callback',[get(zef.h_pushbutton4,'Callback') 'if zef.g_merge; zef.g_submesh_ind = [zef.g_submesh_ind size(zef.g_triangles,1)]; else zef.g_submesh_ind = [size(zef.g_triangles,1)]; end;']);
+set(zef.h_pushbutton6,'Callback',[get(zef.h_pushbutton6,'Callback') 'if zef.c_merge; zef.c_submesh_ind = [zef.c_submesh_ind size(zef.c_triangles,1)]; else zef.c_submesh_ind = [size(zef.c_triangles,1)]; end;']);
+set(zef.h_pushbutton8,'Callback',[get(zef.h_pushbutton8,'Callback') 'if zef.sk_merge; zef.sk_submesh_ind = [zef.sk_submesh_ind size(zef.sk_triangles,1)]; else zef.sk_submesh_ind = [size(zef.sk_triangles,1)]; end;']);
+set(zef.h_pushbutton10,'Callback',[get(zef.h_pushbutton10,'Callback') 'if zef.sc_merge; zef.sc_submesh_ind = [zef.sc_submesh_ind size(zef.sc_triangles,1)]; else zef.sc_submesh_ind = [size(zef.sc_triangles,1)]; end;']);
+set(zef.h_pushbutton102,'Callback',[get(zef.h_pushbutton102,'Callback') 'if zef.d1_merge; zef.d1_submesh_ind = [zef.d1_submesh_ind size(zef.d1_triangles,1)]; else zef.d1_submesh_ind = [size(zef.d1_triangles,1)]; end;']);
+set(zef.h_pushbutton202,'Callback',[get(zef.h_pushbutton202,'Callback') 'if zef.d2_merge; zef.d2_submesh_ind = [zef.d2_submesh_ind size(zef.d2_triangles,1)]; else zef.d2_submesh_ind = [size(zef.d2_triangles,1)]; end;']);
+set(zef.h_pushbutton302,'Callback',[get(zef.h_pushbutton302,'Callback') 'if zef.d3_merge; zef.d3_submesh_ind = [zef.d3_submesh_ind size(zef.d3_triangles,1)]; else zef.d3_submesh_ind = [size(zef.d3_triangles,1)]; end;']);
+set(zef.h_pushbutton402,'Callback',[get(zef.h_pushbutton402,'Callback') 'if zef.d4_merge; zef.d4_submesh_ind = [zef.d4_submesh_ind size(zef.d4_triangles,1)]; else zef.d4_submesh_ind = [size(zef.d4_triangles,1)]; end;']);
+set(zef.h_d5_button_2,'Callback',[get(zef.h_d5_button_2,'Callback') 'if zef.d5_merge; zef.d5_submesh_ind = [zef.d5_submesh_ind size(zef.d5_triangles,1)]; else zef.d5_submesh_ind = [size(zef.d5_triangles,1)]; end;']);
+set(zef.h_d6_button_2,'Callback',[get(zef.h_d6_button_2,'Callback') 'if zef.d6_merge; zef.d6_submesh_ind = [zef.d6_submesh_ind size(zef.d6_triangles,1)]; else zef.d6_submesh_ind = [size(zef.d6_triangles,1)]; end;']);
+set(zef.h_d7_button_2,'Callback',[get(zef.h_d7_button_2,'Callback') 'if zef.d7_merge; zef.d7_submesh_ind = [zef.d7_submesh_ind size(zef.d7_triangles,1)]; else zef.d7_submesh_ind = [size(zef.d7_triangles,1)]; end;']);
+set(zef.h_d8_button_2,'Callback',[get(zef.h_d8_button_2,'Callback') 'if zef.d8_merge; zef.d8_submesh_ind = [zef.d8_submesh_ind size(zef.d8_triangles,1)]; else zef.d8_submesh_ind = [size(zef.d8_triangles,1)]; end;']);
+set(zef.h_d9_button_2,'Callback',[get(zef.h_d9_button_2,'Callback') 'if zef.d9_merge; zef.d9_submesh_ind = [zef.d9_submesh_ind size(zef.d9_triangles,1)]; else zef.d9_submesh_ind = [size(zef.d9_triangles,1)]; end;']);
+set(zef.h_d10_button_2,'Callback',[get(zef.h_d10_button_2,'Callback') 'if zef.d10_merge; zef.d10_submesh_ind = [zef.d10_submesh_ind size(zef.d10_triangles,1)]; else zef.d10_submesh_ind = [size(zef.d10_triangles,1)]; end;']);
+set(zef.h_d11_button_2,'Callback',[get(zef.h_d11_button_2,'Callback') 'if zef.d11_merge; zef.d11_submesh_ind = [zef.d11_submesh_ind size(zef.d11_triangles,1)]; else zef.d11_submesh_ind = [size(zef.d11_triangles,1)]; end;']);
+set(zef.h_d12_button_2,'Callback',[get(zef.h_d12_button_2,'Callback') 'if zef.d12_merge; zef.d12_submesh_ind = [zef.d12_submesh_ind size(zef.d12_triangles,1)]; else zef.d12_submesh_ind = [size(zef.d12_triangles,1)]; end;']);
+set(zef.h_d13_button_2,'Callback',[get(zef.h_d13_button_2,'Callback') 'if zef.d13_merge; zef.d13_submesh_ind = [zef.d13_submesh_ind size(zef.d13_triangles,1)]; else zef.d13_submesh_ind = [size(zef.d13_triangles,1)]; end;']);
+set(zef.h_d14_button_2,'Callback',[get(zef.h_d14_button_2,'Callback') 'if zef.d14_merge; zef.d14_submesh_ind = [zef.d14_submesh_ind size(zef.d14_triangles,1)]; else zef.d14_submesh_ind = [size(zef.d14_triangles,1)]; end;']);
+set(zef.h_d15_button_2,'Callback',[get(zef.h_d15_button_2,'Callback') 'if zef.d15_merge; zef.d15_submesh_ind = [zef.d15_submesh_ind size(zef.d15_triangles,1)]; else zef.d15_submesh_ind = [size(zef.d15_triangles,1)]; end;']);
+set(zef.h_d16_button_2,'Callback',[get(zef.h_d16_button_2,'Callback') 'if zef.d16_merge; zef.d16_submesh_ind = [zef.d16_submesh_ind size(zef.d16_triangles,1)]; else zef.d16_submesh_ind = [size(zef.d16_triangles,1)]; end;']);
+set(zef.h_d17_button_2,'Callback',[get(zef.h_d17_button_2,'Callback') 'if zef.d17_merge; zef.d17_submesh_ind = [zef.d17_submesh_ind size(zef.d17_triangles,1)]; else zef.d17_submesh_ind = [size(zef.d17_triangles,1)]; end;']);
+set(zef.h_d18_button_2,'Callback',[get(zef.h_d18_button_2,'Callback') 'if zef.d18_merge; zef.d18_submesh_ind = [zef.d18_submesh_ind size(zef.d18_triangles,1)]; else zef.d18_submesh_ind = [size(zef.d18_triangles,1)]; end;']);
+set(zef.h_d19_button_2,'Callback',[get(zef.h_d19_button_2,'Callback') 'if zef.d19_merge; zef.d19_submesh_ind = [zef.d19_submesh_ind size(zef.d19_triangles,1)]; else zef.d19_submesh_ind = [size(zef.d19_triangles,1)]; end;']);
+set(zef.h_d20_button_2,'Callback',[get(zef.h_d20_button_2,'Callback') 'if zef.d20_merge; zef.d20_submesh_ind = [zef.d20_submesh_ind size(zef.d20_triangles,1)]; else zef.d20_submesh_ind = [size(zef.d20_triangles,1)]; end;']);
+set(zef.h_d21_button_2,'Callback',[get(zef.h_d21_button_2,'Callback') 'if zef.d21_merge; zef.d21_submesh_ind = [zef.d21_submesh_ind size(zef.d21_triangles,1)]; else zef.d21_submesh_ind = [size(zef.d21_triangles,1)]; end;']);
+set(zef.h_d22_button_2,'Callback',[get(zef.h_d22_button_2,'Callback') 'if zef.d22_merge; zef.d22_submesh_ind = [zef.d22_submesh_ind size(zef.d22_triangles,1)]; else zef.d22_submesh_ind = [size(zef.d22_triangles,1)]; end;']);
+    
+end
 
 zef_figure_tool;
 zef_mesh_tool;
