069aa24266e2724c646c801948ef46abd41669c1
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2020-06-15 20:52:18 +0300

    Add files via upload

--- m/zef_plot_parcellation_time_series.m
+++ m/zef_plot_parcellation_time_series.m
@@ -213,4 +213,61 @@ hold(evalin('base','zef.h_axes1'),'off');
 
 end
 
+if plot_mode == 3
+    
+if iscell(evalin('base','zef.reconstruction'))
+length_reconstruction_cell = length(evalin('base','zef.reconstruction'));
+frame_start = evalin('base','zef.frame_start');
+frame_stop = evalin('base','zef.frame_stop');
+frame_step = evalin('base','zef.frame_step');
+if frame_start == 0
+frame_start = 1;
+end
+if frame_stop == 0
+frame_stop = length_reconstruction_cell;
+end
+frame_start = max(frame_start,1);
+frame_start = min(length_reconstruction_cell,frame_start);
+frame_stop = max(frame_stop,1);
+frame_stop = min(length_reconstruction_cell,frame_stop);
+number_of_frames = length([frame_start : frame_step : frame_stop]);
+f_ind = [frame_start : frame_step : frame_stop];
+x_vals = evalin('base','zef.inv_time_1') + evalin('base','zef.inv_time_2')/2 + frame_step*(f_ind - 1)*evalin('base','zef.inv_time_3');
+axes(evalin('base','zef.h_axes1'));
+cla(evalin('base','zef.h_axes1'));
+hold(evalin('base','zef.h_axes1'),'off');
+[color_sort, color_perm] = sortrows(2*parcellation_colormap(selected_list+1,:));
+[~,~,color_unique] = unique(color_sort,'rows');
+line_style_cell = {'-','--','-.',':','o-','o--','o-.','o:','s-','s--','s-.','s:','d-','d--','d-.','d:','*-','*--','*-.','*:'};
+color_unique = [1;color_unique];
+line_ind = 0;
+for i = 1 : length(selected_list)
+if color_unique(i+1) == color_unique(i)
+  line_ind = line_ind + 1;  
+else
+    line_ind = 1;
+end
+  line_ind = min(line_ind,length(line_style_cell));
+  line_ind = max(line_ind,1);
+h_plot = plot(x_vals,y_vals(color_perm(i),:),line_style_cell{line_ind});
+set(h_plot, 'color',color_sort(i,:),'linewidth',2);
+if i == 1
+hold(evalin('base','zef.h_axes1'),'on');
+end
+end
+%set(evalin('base','zef.h_axes1'),'xticklabel',[]);
+set(evalin('base','zef.h_axes1'),'ticklength',[0 0]);
+set(evalin('base','zef.h_axes1'),'xlim',[x_vals(1) x_vals(end)]);
+set(evalin('base','zef.h_axes1'),'ylim',[0 1.05*max(y_vals(:))]);
+set(evalin('base','zef.h_axes1'),'ygrid','on');
+legend(parcellation_list(selected_list))
+%x_labels = text(x_vals-0.25,-0.01*max(y_vals)*ones(size(x_vals)),parcellation_list(selected_list),'Parent',evalin('base','zef.h_axes1'));
+%y_label = text(1.01*(length(x_vals)+1),1.05*max(y_vals)/2,y_string,'Parent',evalin('base','zef.h_axes1'));
+%set(x_labels,'HorizontalAlignment','right','VerticalAlignment','top', 'Rotation',90, 'Fontsize', 8);
+hold(evalin('base','zef.h_axes1'),'off');
+%set(x_labels,'HorizontalAlignment','right','VerticalAlignment','top', 'Rotation',90, 'Fontsize', 8);
+%set(y_label,'HorizontalAlignment','right','VerticalAlignment','top', 'Rotation',90, 'Fontsize', 8);
+
+end
+
 end
