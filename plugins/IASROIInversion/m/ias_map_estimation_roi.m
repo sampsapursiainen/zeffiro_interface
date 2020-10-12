fcce736bc1de2d98af79aeb26aebd7b946e1add7
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2020-10-12 03:27:08 +0300

    Add files via upload

---
+++ plugins/IASROIInversion/m/ias_map_estimation_roi.m
@@ -0,0 +1,32 @@
+%Copyright © 2018- Sampsa Pursiainen & ZI Development Team
+%See: https://github.com/sampsapursiainen/zeffiro_interface
+if ismac
+zef.h_iasroi_map_estimation = open('ias_map_estimation_roi.fig');
+elseif ispc
+zef.h_iasroi_map_estimation = open('ias_map_estimation_roi.fig');
+else
+zef.h_iasroi_map_estimation = open('ias_map_estimation_roi.fig');    
+end
+set(zef.h_iasroi_map_estimation,'Name','ZEFFIRO Interface: IAS ROI MAP estimation');
+set(findobj(zef.h_iasroi_map_estimation.Children,'-property','FontUnits'),'FontUnits','pixels')
+set(findobj(zef.h_iasroi_map_estimation.Children,'-property','FontSize'),'FontSize',zef.font_size);
+
+
+zef_init_ias_roi;
+if isfield(zef,'measurements')
+if iscell(zef.measurements)
+    set(zef.h_iasroi_data_segment,'enable','on');
+end
+if not(iscell(zef.measurements))
+    set(zef.h_iasroi_data_segment,'enable','off'); 
+end
+end
+uistack(flipud([zef.h_iasroi_roi_mode; zef.h_iasroi_roi_sphere_1;  zef.h_iasroi_roi_sphere_2;  zef.h_iasroi_roi_sphere_3;  
+zef.h_iasroi_roi_sphere_4; 
+zef.h_iasroi_roi_threshold; zef.h_iasroi_hyperprior; 
+    zef.h_iasroi_snr ; zef.h_iasroi_n_map_iterations ; 
+    zef.h_iasroi_sampling_frequency ; zef.h_iasroi_low_cut_frequency ; 
+    zef.h_iasroi_high_cut_frequency ; zef.h_iasroi_time_1 ; zef.h_iasroi_time_2; 
+    zef.h_iasroi_number_of_frames; zef.h_iasroi_time_3; zef.h_iasroi_data_segment ; zef.h_iasroi_cancel ; 
+    zef.h_iasroi_apply; zef.h_iasroi_start ; zef.h_iasroi_plot_source; zef.h_iasroi_rec_source_8; 
+    zef.h_iasroi_rec_source_9  ]),'top'); 
