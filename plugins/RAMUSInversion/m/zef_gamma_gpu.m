4ba01eb3e0f066a8ac2c5d52fa7fcdc6c38cdd43
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2020-10-12 22:39:01 +0300

    Delete zef_gamma_gpu.m

--- plugins/RAMUSInversion/m/zef_gamma_gpu.m
+++
@@ -1,7 +0,0 @@
-%Copyright © 2018- Sampsa Pursiainen & ZI Development Team
-%See: https://github.com/sampsapursiainen/zeffiro_interface
-function [p_val] = zef_gamma_gpu(x, shape,scale)
-
-p_val = (1./(scale.^shape.*gamma(shape))).*x.^(shape-1).*exp(-x./scale);
-
-end
