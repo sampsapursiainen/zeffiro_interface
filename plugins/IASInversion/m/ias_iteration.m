85d3fa4dda4d5daaecb129e2a8dbb74fbe2b00aa
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2020-10-13 13:41:25 +0300

    Add files via upload

--- plugins/IASInversion/m/ias_iteration.m
+++ plugins/IASInversion/m/ias_iteration.m
@@ -2,6 +2,8 @@
 %See: https://github.com/sampsapursiainen/zeffiro_interface
 function [z] = ias_iteration(void)
 
+inverse_gamma_ind = [1:4];
+gamma_ind = [5:10];
 h = waitbar(0,['IAS MAP iteration.']);
 [s_ind_1] = unique(evalin('base','zef.source_interpolation_ind{1}'));
 n_interp = length(s_ind_1);
@@ -382,40 +384,27 @@ if evalin('base','zef.ias_normalize_data')==1;
 else
     normalize_data = 'average';
 end
-if ias_hyperprior == 2
-[beta, theta0] = zef_find_ig_hyperprior(snr_val,L,source_count,normalize_data,1);
-elseif ias_hyperprior == 1
-[beta, theta0] = zef_find_ig_hyperprior(snr_val,L,source_count,normalize_data,0);
-elseif ias_hyperprior == 4
-[beta, theta0] = zef_find_ig_hyperprior_scale(snr_val,1.5,L,source_count,normalize_data,1);
+if ias_hyperprior == 1
+[beta, theta0] = zef_find_ig_hyperprior(snr_val,L_aux_2,source_count,normalize_data,0);
+elseif ias_hyperprior == 2
+[beta, theta0] = zef_find_ig_hyperprior(snr_val,L_aux_2,source_count,normalize_data,1);
 elseif ias_hyperprior == 3
-[beta, theta0] = zef_find_ig_hyperprior_scale(snr_val,1.5,L,source_count,normalize_data,0);
-elseif ias_hyperprior == 6
-[beta, theta0] = zef_find_ig_hyperprior_scale(snr_val,3.0,L,source_count,normalize_data,1);
+[beta, theta0] = zef_find_ig_hyperprior_scale(snr_val,3,L_aux_2,source_count,normalize_data,0);
+elseif ias_hyperprior == 4
+[beta, theta0] = zef_find_ig_hyperprior_scale(snr_val,3,L_aux_2,source_count,normalize_data,1);
 elseif ias_hyperprior == 5
-[beta, theta0] = zef_find_ig_hyperprior_scale(snr_val,3.0,L,source_count,normalize_data,0);
-elseif ias_hyperprior == 8
-[beta, theta0] = zef_find_ig_hyperprior_scale(snr_val,4.4,L,source_count,normalize_data,1);
+[beta, theta0] = zef_find_g_hyperprior(snr_val,L_aux_2,source_count,normalize_data,0);
+elseif ias_hyperprior == 6
+[beta, theta0] = zef_find_g_hyperprior(snr_val,L_aux_2,source_count,normalize_data,1);
 elseif ias_hyperprior == 7
-[beta, theta0] = zef_find_ig_hyperprior_scale(snr_val,4.4,L,source_count,normalize_data,0);
-elseif ias_hyperprior == 10
-[beta, theta0] = zef_find_g_hyperprior(snr_val,L,source_count,normalize_data,1);
+[beta, theta0] = zef_find_g_hyperprior_scale(snr_val,3,L_aux_2,source_count,normalize_data,0);
+elseif ias_hyperprior == 8
+[beta, theta0] = zef_find_g_hyperprior_scale(snr_val,3,L_aux_2,source_count,normalize_data,1);
 elseif ias_hyperprior == 9
-[beta, theta0] = zef_find_g_hyperprior(snr_val,L,source_count,normalize_data,0);
-elseif ias_hyperprior == 12
-[beta, theta0] = zef_find_g_hyperprior_scale(snr_val,1.5,L,source_count,normalize_data,1);
-elseif ias_hyperprior == 11
-[beta, theta0] = zef_find_g_hyperprior_scale(snr_val,1.5,L,source_count,normalize_data,0);
-elseif ias_hyperprior == 14
-[beta, theta0] = zef_find_g_hyperprior_scale(snr_val,3.0,L,source_count,normalize_data,1);
-elseif ias_hyperprior == 13
-[beta, theta0] = zef_find_g_hyperprior_scale(snr_val,3.0,L,source_count,normalize_data,0);
-elseif ias_hyperprior == 16
-[beta, theta0] = zef_find_g_hyperprior_scale(snr_val,4.4,L,source_count,normalize_data,1);
-elseif ias_hyperprior == 15
-[beta, theta0] = zef_find_g_hyperprior_scale(snr_val,4.4,L,source_count,normalize_data,0);
+[beta, theta0] = zef_find_g_hyperprior_ig(snr_val,L_aux_2,source_count,normalize_data,0);
+elseif ias_hyperprior == 10
+[beta, theta0] = zef_find_g_hyperprior_ig(snr_val,L_aux_2,source_count,normalize_data,1);
 end
-
 
 
 if evalin('base','zef.use_gpu') == 1 & gpuDeviceCount > 0
@@ -479,13 +468,13 @@ z_aux = zeros(3*size(L,2),1);
 end
 z_vec = ones(size(L,2),1); 
 
-if ismember(ias_hyperprior,[1:8])
+if ismember(ias_hyperprior,inverse_gamma_ind)
 if length(theta0) > 1  || length(beta) > 1
 theta = theta0./(beta-1);
 else
 theta = (theta0./(beta-1))*ones(size(L_aux_2,2),1);
 end
-elseif ismember(ias_hyperprior,[9:16])
+elseif ismember(ias_hyperprior,gamma_ind)
 if length(theta0) > 1  || length(beta) > 1
 theta = theta0.*beta;
 else
@@ -538,9 +527,9 @@ z_vec = d_sqrt.*(L'*((L*L' + S_mat)\f));
 if evalin('base','zef.use_gpu') == 1 & gpuDeviceCount > 0
 z_vec = gather(z_vec);
 end
-if ismember(ias_hyperprior,[1:8])
+if ismember(ias_hyperprior,inverse_gamma_ind)
 theta = (theta0+0.5*z_vec.^2)./(beta + 1.5);
-elseif ismember(ias_hyperprior,[9:16])
+elseif ismember(ias_hyperprior,gamma_ind)
 theta = theta0.*(beta-1.5 + sqrt((1./(2.*theta0)).*z_vec.^2 + (beta+1.5).^2)); 
 end
 end;
