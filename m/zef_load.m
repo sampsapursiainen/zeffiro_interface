90f34e50c44cf0af02f0c29b43ecdf515c74d812
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2020-10-17 23:50:00 +0300

    Add files via upload

--- m/zef_load.m
+++ m/zef_load.m
@@ -21,6 +21,24 @@ load([zef.file_path zef.file]);
  clear zef_i;
  zef = rmfield(zef,'fieldnames');
           
+ zef.h_zeffiro = fopen('zeffiro_interface.ini');
+zef.ini_cell = textscan(zef.h_zeffiro,'%s');
+zef.save_file_path = zef.ini_cell{1}{2};
+zef.save_file = zef.ini_cell{1}{4};
+zef.video_codec = zef.ini_cell{1}{6};
+zef.use_gpu = str2num(zef.ini_cell{1}{8});
+zef.gpu_num = str2num(zef.ini_cell{1}{10});
+if gpuDeviceCount > 0 & zef.use_gpu == 1
+gpuDevice(zef.gpu_num);
+end
+zef.parallel_vectors = str2num(zef.ini_cell{1}{12});
+zef.snapshot_vertical_resolution = str2num(zef.ini_cell{1}{14});
+zef.snapshot_horizontal_resolution = str2num(zef.ini_cell{1}{16});
+zef.movie_fps = str2num(zef.ini_cell{1}{18});
+zef.font_size = str2num(zef.ini_cell{1}{20});
+zef.mlapp = str2num(zef.ini_cell{1}{22});
+zef = rmfield(zef,'ini_cell');
+ 
 clear zef_data;
 zef_update;
 zef_mesh_tool;
