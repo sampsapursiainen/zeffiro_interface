f008ef22aef6588d7a193b9b9e44c507a9e3f04f
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-01-08 03:37:52 +0200

    Add files via upload

--- plugins/IASInversion/m/ias_iteration.m
+++ plugins/IASInversion/m/ias_iteration.m
@@ -391,9 +391,9 @@ else
     balance_spatially = 0;
 end
 if evalin('base','zef.inv_hyperprior') == 1
-[beta, theta0] = zef_find_ig_hyperprior(snr_val,L,size(L,2),normalize_data,balance_spatially,evalin('base','zef.inv_hyperprior_weight'));
+[beta, theta0] = zef_find_ig_hyperprior(snr_val,evalin('base','zef.inv_hyperprior_tail_length_db'),L,size(L,2),normalize_data,balance_spatially,evalin('base','zef.inv_hyperprior_weight'));
 elseif evalin('base','zef.inv_hyperprior') == 2 
-[beta, theta0] = zef_find_g_hyperprior(snr_val,L,size(L,2),normalize_data,balance_spatially,evalin('base','zef.inv_hyperprior_weight'));
+[beta, theta0] = zef_find_g_hyperprior(snr_val,evalin('base','zef.inv_hyperprior_tail_length_db'),L,size(L,2),normalize_data,balance_spatially,evalin('base','zef.inv_hyperprior_weight'));
 end
 
 
