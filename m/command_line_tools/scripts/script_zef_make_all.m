08b7055625ba794f57ead978cabf11465f74d040
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-01-29 16:02:33 +0200

    Add files via upload

---
+++ m/command_line_tools/scripts/script_zef_make_all.m
@@ -0,0 +1,13 @@
+run('../../../zeffiro_interface_nodisplay');
+
+zef.file_path = [zef.program_path '/data/'];
+zef.file = 'sampsa_debug_version.mat';
+
+zef_load_nodisplay;
+zef_make_all_nodisplay;
+
+zef.file = 'sampsa_debug_version.mat';
+zef.save_switch = 7;
+zef_save_nodisplay;
+
+exit;
