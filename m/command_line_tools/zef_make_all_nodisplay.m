1e00610dcecfe394e1848cdcd51797ce065242b7
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-01-29 14:29:41 +0200

    Add files via upload

--- m/command_line_tools/zef_make_all_nodisplay.m
+++ m/command_line_tools/zef_make_all_nodisplay.m
@@ -1,12 +1,3 @@
-addpath('./m');
-
-%project_file_name = 'Inverted_project10.mat';%'P20-N20_synthetic_1accuracy-1mmresolution_200.mat';
-load(project_file_name);
-zef = zef_data;
-clear zef_data;
-
-zef.n_sources = 100000;
-
 zef.source_interpolation_on = 1; 
 [zef.sensors,zef.reuna_p,zef.reuna_t] = process_meshes([]); 
 [zef.nodes,zef.nodes_b,zef.tetra,zef.sigma_ind,zef.surface_triangles]=fem_mesh([]);
@@ -18,11 +9,4 @@ zef.source_ind = [];
 [zef.sensors_attached_volume] = attach_sensors_volume(zef.sensors);
 lead_field_matrix;
 
-zef_data = zef;
-zef_data.fieldnames = fieldnames(zef);
-zef_data = rmfield(zef_data,zef_data.fieldnames(find(startsWith(zef_data.fieldnames, 'h_'))));
-%zef_data = rmfield(zef_data,{'fieldnames','h'});
-save([file_name],'zef_data','-v7.3');
-clear zef_data;
 
-exit;
