d7c6542405150a75d99d3bd9d744579997f3b4ca
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-01-29 14:33:25 +0200

    Add files via upload

--- m/command_line_tools/scripts/script_make_all.m
+++ m/command_line_tools/scripts/script_make_all.m
@@ -1,1 +1,19 @@
+home_folder = ['../../../'];
 
+run([home_folder 'zeffiro_interface_nodisplay;']);
+
+
+load(project_file_name);
+zef = zef_data;
+clear zef_data;
+
+zef_make_all_nodisplay;
+
+zef_data = zef;
+zef_data.fieldnames = fieldnames(zef);
+zef_data = rmfield(zef_data,zef_data.fieldnames(find(startsWith(zef_data.fieldnames, 'h_'))));
+%zef_data = rmfield(zef_data,{'fieldnames','h'});
+save([file_name],'zef_data','-v7.3');
+clear zef_data;
+
+exit;
