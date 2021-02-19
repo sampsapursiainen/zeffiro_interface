1668b5fe811b062d4331fee05c27d18354bd4563
GitFernandoGalaz <58070951+GitFernandoGalaz@users.noreply.github.com>
2021-02-19 14:35:08 +0200

    Update print_meshes.m

--- m/print_meshes.m
+++ m/print_meshes.m
@@ -1,7 +1,6 @@
 %Copyright © 2018- Sampsa Pursiainen & ZI Development Team
 %See: https://github.com/sampsapursiainen/zeffiro_interface
 function [void] = print_meshes(void);
- 'moi'
 void = []; 
 sensors_point_like = [];
 
