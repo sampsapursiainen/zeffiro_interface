397770cbcf6eff248a468d28aabae507a3f74934
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-02-22 04:25:34 +0200

    Add files via upload

--- m/zef_field_downsampling.m
+++ m/zef_field_downsampling.m
@@ -2,6 +2,7 @@
 zef.rand_vec_aux = randperm(size(zef.source_positions,1));
 zef.rand_vec_aux = zef.rand_vec_aux(1:min(zef.n_sources,size(zef.source_positions,1)));
 
+if isfield(zef,'source_positions_original_field')
 if isempty(zef.source_positions_original_field)
 
 zef.source_positions_original_field = zef.source_positions;
@@ -13,6 +14,13 @@ else
 zef.source_positions = zef.source_positions_original_field;
 zef.source_directions = zef.source_directions_original_field;
 zef.L = zef.L_original_field;
+end
+
+else
+    
+zef.source_positions_original_field = zef.source_positions;
+zef.source_directions_original_field = zef.source_directions;
+zef.L_original_field = zef.L;
 
 end
 
@@ -22,7 +30,7 @@ if ismember(zef.source_direction_mode,2)
 zef.rand_vec_aux = 3*(zef.rand_vec_aux(:)'-1);
 zef.rand_vec_aux = [zef.rand_vec_aux + 1; zef.rand_vec_aux + 2; zef.rand_vec_aux + 3];
 zef.rand_vec_aux = zef.rand_vec_aux(:);
-zef.L = zef_L(:,zef.rand_vec_aux);
+zef.L = zef.L(:,zef.rand_vec_aux);
 elseif ismember(zef.source_direction_mode,[1 3])
 zef.L = zef.L(zef.rand_vec_aux,:);
 if not(isempty(zef.source_directions))
