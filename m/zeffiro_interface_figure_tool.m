5a7f6428c7ed921a7ebfb6a6e78caf00d5446f24
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-02-24 13:54:40 +0200

    Add files via upload

--- m/zeffiro_interface_figure_tool.m
+++ m/zeffiro_interface_figure_tool.m
@@ -30,7 +30,7 @@ zef.h_zeffiro = figure(...
 addToolbarExplorationButtons(zef.h_zeffiro);
 
 zef.stop_movie = 0;
-zef.h_axes1 = uiaxes('Parent',zef.h_zeffiro,'visible','off','Units','normalized','Position',[0.05 0.43 0.9 0.55],'FontSize',0.587962962962963);
+zef.h_axes1 = uiaxes('Parent',zef.h_zeffiro,'visible','on','Units','normalized','Position',[0.05 0.43 0.9 0.55],'FontSize',0.587962962962963);
 zef.h_stop_movie = uicontrol('Style','PushButton','Parent',zef.h_zeffiro,'visible','on','Units','normalized','Position',[0.67 0.34 0.15 0.06],'String','Stop','Callback','if zef.stop_movie == 1; zef.stop_movie = 0; set(zef.h_stop_movie,''foregroundcolor'',[0 0 0]); set(zef.h_stop_movie,''string'',''Stop'');else; zef.stop_movie = 1; set(zef.h_stop_movie,''foregroundcolor'',[1 0 0]); set(zef.h_stop_movie,''string'',''Stopped''); end; set(zef.h_stop_movie,''value'',zef.stop_movie);');
 zef.h_pause_movie = uicontrol('Style','PushButton','Parent',zef.h_zeffiro,'visible','on','Units','normalized','Position',[0.82 0.34 0.15 0.06],'String','Pause','Callback','if zef.stop_movie == 1; zef.stop_movie = 0; set(zef.h_pause_movie,''foregroundcolor'',[0 0 0]); set(zef.h_pause_movie,''string'',''Pause'');else; zef.stop_movie = 1; set(zef.h_pause_movie,''foregroundcolor'',[1 0 0]); set(zef.h_pause_movie,''string'',''Paused''); end; set(zef.h_pause_movie,''value'',zef.stop_movie);');
 zef.h_loop_movie = uicontrol('Style','Checkbox','Parent',zef.h_zeffiro,'visible','on','Units','normalized','Position',[0.3 0.35 0.25 0.04],'String','Loop on / count:','Callback','zef.loop_movie = get(zef.h_loop_movie,''value'');','HorizontalAlignment','left');
@@ -38,7 +38,7 @@ zef.h_loop_movie_count = uicontrol('Style','Edit','Parent',zef.h_zeffiro,'visibl
 set(zef.h_loop_movie_count,'string',num2str(zef.loop_movie_count));
 
 imagesc(zef.h_axes1,imread('zeffiro_interface.jpg'));
-axis(zef.h_axes1,'equal');
+%axis(zef.h_axes1,'equal');
 
 %***********************
 
@@ -675,5 +675,9 @@ uicontrol(...
 'Tag','text361',...
 'FontSize',0.587962962962963,...
 'FontWeight','bold');
+
+axis(zef.h_axes1,'tight');
+set(zef.h_axes1,'xtick',[]);
+set(zef.h_axes1,'ytick',[]):
 
 
