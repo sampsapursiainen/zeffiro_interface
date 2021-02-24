0bf5dd0cf8ec6961763e15ea537d6e976f1922c7
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-02-24 21:45:04 +0200

    Add files via upload

--- plugins/FindSyntheticSource/m/find_synthetic_source.m
+++ plugins/FindSyntheticSource/m/find_synthetic_source.m
@@ -92,4 +92,9 @@ zef.find_synth_source.h_plot_switch.ValueChangedFcn = 'zef.synth_source_updated_
 set(findobj(zef.find_synth_source.h_find_synth_source.Children,'-property','FontSize'),'FontSize',zef.font_size);
 
 
+set(zef.find_synth_source,'AutoResizeChildren','off');
+zef.find_synth_source_current_size = get(zef.find_synth_source,'Position');
+set(zef.find_synth_source,'SizeChangedFcn','zef.find_synth_source_current_size = zef_change_size_function(zef.find_synth_source,zef.find_synth_source_current_size);');
+
+
 clear zef_i zef_j zef_n zef_N
