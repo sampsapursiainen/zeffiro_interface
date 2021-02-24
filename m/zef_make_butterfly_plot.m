05087c2e083f82aff6eee620485591c20c08e254
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-02-24 19:43:22 +0200

    Add files via upload

--- m/zef_make_butterfly_plot.m
+++ m/zef_make_butterfly_plot.m
@@ -53,12 +53,12 @@ axes(evalin('base','zef.h_axes1'));
 cla(evalin('base','zef.h_axes1'));
 hold(evalin('base','zef.h_axes1'),'off');
 h_plot = plot(t_vec',f');
-set(h_plot,'linewidth',1.5);
+set(h_plot,'linewidth',0.5);
 set(gca,'xlim',[t_vec(1) t_vec(end)]);
 f_range = max(f(:))-min(f(:));
 set(gca,'ylim',[min(f(:))-0.05*f_range max(f(:))+0.05*f_range]);
 set(evalin('base','zef.h_axes1'),'ygrid','on'); 
 set(evalin('base','zef.h_axes1'),'xgrid','on');
 set(evalin('base','zef.h_axes1'),'fontsize',14);
-set(evalin('base','zef.h_axes1'),'linewidth',2);
+set(evalin('base','zef.h_axes1'),'linewidth',0.5);
 end
