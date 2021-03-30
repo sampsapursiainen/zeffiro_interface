6d5e21589ffbea6b01e207443e8be455b9ec4cac
GitFernandoGalaz <fernando.galazprieto@tuni.fi>
2021-03-30 18:38:14 +0300

    Update zef_toggle_lock_on.m

--- m/zef_toggle_lock_on.m
+++ m/zef_toggle_lock_on.m
@@ -4,7 +4,7 @@ zef.h_menu_lock_on.Text = 'Toggle ''On'' unlocked';
 zef.h_menu_lock_on.ForegroundColor = [0 0 0];
 elseif isequal(zef.lock_on,1)
     zef.h_compartment_table.ColumnEditable = logical(ones(1,size(zef.h_compartment_table.Data,2)));
-    zef.h_compartment_table.ColumnEditable(3) = 0;
+    zef.h_compartment_table.ColumnEditable(3) = logical(0);
     zef.h_menu_lock_on.Text = 'Toggle ''On'' locked';
 zef.h_menu_lock_on.ForegroundColor = [1 0 0];
 end
