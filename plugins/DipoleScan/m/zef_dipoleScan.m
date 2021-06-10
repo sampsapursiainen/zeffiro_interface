333cd64fb5360fdca3eda31520345fcf6acc51e4
frank-neugebauer <37830689+frank-neugebauer@users.noreply.github.com>
2021-06-10 13:13:03 +0300

    Add files via upload

---
+++ plugins/DipoleScan/m/zef_dipoleScan.m
@@ -0,0 +1,196 @@
+%Copyright © 2018- Sampsa Pursiainen & ZI Development Team
+%See: https://github.com/sampsapursiainen/zeffiro_interface
+function [z] = zef_dipoleScan
+
+
+h = waitbar(0,'Dipole scanning');
+
+number_of_frames = evalin('base','zef.number_of_frames');
+
+source_direction_mode = evalin('base','zef.source_direction_mode');
+
+[L,n_interp, procFile] = zef_processLeadfields(source_direction_mode);
+
+if evalin('base','zef.use_gpu') == 1 && gpuDeviceCount > 0
+    L = gpuArray(L);
+end
+
+    z = cell(number_of_frames,1);
+f_data = zef_getFilteredData;
+
+tic;
+for f_ind = 1 : number_of_frames
+    
+        time_val = toc;
+    if f_ind > 1
+        date_str = datestr(datevec(now+(number_of_frames/(f_ind-1) - 1)*time_val/86400)); %what does that do?
+        waitbar(100,h,['Step ' int2str(f_ind) ' of ' int2str(number_of_frames) '. Ready: ' date_str '.' ]);
+
+    end
+    
+    f=zef_getTimeStep(f_data, f_ind, true);
+
+    z_vec = nan(size(L,2),1);
+    
+    if evalin('base','zef.use_gpu') == 1 && gpuDeviceCount > 0
+        f = gpuArray(f);
+    end
+     
+    
+%% inversion starts here
+    
+    directionWise=false;
+    
+    switch source_direction_mode
+        
+        case {1,2}
+            
+            if directionWise
+                
+                for i=1:size(L,2)
+                    
+                    
+%                     if f_ind == 1
+%                         waitbar(size(L,2),h,['Step ' int2str(f_ind) ' of ' int2str(number_of_frames)]);
+%                     else
+%                         waitbar(size(L,2),h,['Dipole Scanning. Time step ' int2str(f_ind) ' of ' int2str(number_of_frames) '.' ]);
+%                     end
+                    
+                    
+                    
+                    
+                    lf=L(:,i);
+                    
+                    
+                    [U,S,V]=svd(lf, 'econ');
+                    
+                    %     if cfg.reduceDim < size(lf, 2)
+                    %         lf=lf*V(:, 1:cfg.reduceDim); %columns of V are the right singular vectors
+                    %         [U,S,V]=svd(lf, 'econ');
+                    %
+                    %     end
+                    
+                    
+                    mom(i,:)=V*inv(S)*U'*f;
+                    
+                    %resvar(i)=sum(f'*f)-sum(f'*(U*U')*f);
+                    
+                    
+                    pot = lf*mom(i,:)';
+                    %relativ residual variance
+                    z_vec(i) = 1 - sum((f-pot).^2) ./ sum(f.^2); %goodnes of fit
+ 
+                end
+            else
+                   % z_vec = ones(size(L,2),1);
+
+                   normal=procFile.s_ind_4;
+                   notNormal=setdiff(1:length(procFile.s_ind_0), procFile.s_ind_4);
+                  % mom=cell(size(L,2)/3);
+                 %for i=1:size(L,2)/3
+                  for j=1:length(normal)  
+                      i=normal(j);
+                     
+%                      if isequal(L(:,i),L(:,i+n_interp), L(:,i+2*n_interp))
+                          lf=L(:,i);
+%                      else
+%                          lf=[L(:,i), L(:,i+n_interp), L(:,i+2*n_interp)];
+%                      end
+                   
+                    
+                    %[U,S,V]=svd(lf, 'econ'); %this will create problems of L=Ln
+                    
+                    %     if cfg.reduceDim < size(lf, 2)
+                    %         lf=lf*V(:, 1:cfg.reduceDim); %columns of V are the right singular vectors
+                    %         [U,S,V]=svd(lf, 'econ');
+                    %
+                    %     end
+                    
+                    %mom{i}=V*S\U'*f; %S\U is better than inv(S)*U
+                    %mom=V*S\U'*f;
+                    mom=pinv(lf)*f;
+                    %resvar(i)=sum(f'*f)-sum(f'*(U*U')*f);
+                    
+                    
+                   % pot = lf*mom{i}';
+                    pot = lf*mom;
+
+                    %relativ residual variance
+                    z_vec(i) = 1 - sum((f-pot).^2) ./ sum(f.^2); %goodnes of fit
+                    z_vec(i+n_interp)=z_vec(i);
+                    z_vec(i+2*n_interp)=z_vec(i);
+                    
+                    
+                    
+                    
+                  end
+                 
+                  for j=1:length(notNormal)
+                      i=notNormal(j);
+                  
+                  
+                           
+%                      if isequal(L(:,i),L(:,i+n_interp), L(:,i+2*n_interp))
+   %                       lf=L(:,i);
+%                      else
+                          lf=[L(:,i), L(:,i+n_interp), L(:,i+2*n_interp)];
+%                      end
+                   
+                    
+                    %[U,S,V]=svd(lf, 'econ'); %this will create problems of L=Ln
+                    
+                    %     if cfg.reduceDim < size(lf, 2)
+                    %         lf=lf*V(:, 1:cfg.reduceDim); %columns of V are the right singular vectors
+                    %         [U,S,V]=svd(lf, 'econ');
+                    %
+                    %     end
+                    
+                    %mom{i}=V*S\U'*f; %S\U is better than inv(S)*U
+                    %mom=V*S\U'*f;
+                    mom=pinv(lf)*f;
+                    %resvar(i)=sum(f'*f)-sum(f'*(U*U')*f);
+                    
+                    
+                   % pot = lf*mom{i}';
+                    pot = lf*mom;
+
+                    %relativ residual variance
+                    z_vec(i) = 1 - sum((f-pot).^2) ./ sum(f.^2); %goodnes of fit
+                                      z_vec(i+n_interp)=z_vec(i);
+                    z_vec(i+2*n_interp)=z_vec(i);
+                  end
+                      
+            end
+            
+    end
+    %%%%%
+    
+    
+    onlymax=false;
+    
+    if onlymax
+        [z_max, z_ind]=max(z_vec);
+        
+        z_vec=z_vec-z_vec;
+        z_vec(z_ind)=z_max;
+    end
+        
+    if evalin('base','zef.use_gpu') == 1 && gpuDeviceCount > 0
+        z_vec = gather(z_vec);
+    end
+    
+    z{f_ind}=z_vec;
+    
+    
+%%
+    
+    
+    
+
+    
+end
+
+z = zef_postProcessInverse(z, procFile);
+z = zef_normalizeInverseReconstruction(z);
+
+close(h);
---
+++ plugins/DipoleScan/zef_dipole_start.m
@@ -0,0 +1,73 @@
+%This is the startup script for dipole_app app. One must add this as launch
+%script to zeffiro_plugins file:
+%dipoleScan, inverse_tools, zef_dipole_start
+
+zef.dipole_app = dipole_app;
+appName='dipole_app';
+
+%_ Names of leadfield regularization methods _
+% zef_bf_names = {'Basic'
+%                 '2nd order Taylor minimum error'
+%                 'Pseudoinverse'};
+%             
+% zef.beamformer.L_reg_type.Items = zef_bf_names;
+% zef.beamformer.L_reg_type.ItemsData = strsplit(num2str(1:length(zef_bf_names)));
+% zef.beamformer.L_reg_type.Value = '1';
+% if ~isfield(zef,'L_reg_type')
+%     zef.L_reg_type = 1;
+% end
+
+
+%Import or set initial values
+
+zef.inv_names={'inv_leadfield_lambda', 'inv_snr', 'inv_sampling_frequency', 'inv_low_cut_frequency',...
+    'inv_high_cut_frequency', 'inv_time_1', 'inv_time_2', 'number_of_frames', 'inv_time_3', 'inv_data_segment'...
+    'normalize_leadfield', 'L_reg_type'};
+zef.inv_default={1e-3, 30, 2400, 2, 80, 0, 0, 0, 0, 1, 1, 1};
+for invNames=1:length(zef.inv_names)
+    if ~isfield(zef,zef.inv_names{invNames})
+        zef.(zef.inv_names{invNames})=zef.inv_default{invNames};
+    end
+    %if isfield(zef.(appName),zef.inv_names{invNames})
+%        zef.(appName).(zef.inv_names{invNames}).Value=zef.(zef.inv_names{invNames});
+    %end
+end
+
+% % this is quite elegant
+%
+% %set parameters if saved in ZI: 
+% %(Naming concept: zef.(app)."field" = zef."field")
+ zef_props = properties(zef.(appName));
+for zef_i = 1:length(zef_props)
+    if isfield(zef,zef_props{zef_i})
+        zef.(appName).(zef_props{zef_i}).Value = num2str(zef.(zef_props{zef_i}));
+    end
+end
+clear zef_props zef_i
+
+if zef.L_reg_type==2 || zef.L_reg_type==3
+    zef.dipole.inv_leadfield_lambda.Enable = 'off';
+end
+
+
+%_ Functions _
+zef.dipole_app.dipole_type.ValueChangedFcn = 'zef.dipole_type = str2num(zef.dipole_app.dipole_type.Value);';
+zef.dipole_app.inv_leadfield_lambda.ValueChangedFcn = 'zef.inv_leadfield_lambda = str2num(zef.dipole_app.inv_leadfield_lambda.Value);';
+zef.dipole_app.L_reg_type.ValueChangedFcn = 'zef.L_reg_type = str2num(zef.dipole_app.L_reg_type.Value); if zef.L_reg_type==2 || zef.L_reg_type==3; zef.dipole_app.inv_leadfield_lambda.Enable = ''off''; else; zef.dipole_app.inv_leadfield_lambda.Enable = ''on''; end;';
+zef.dipole_app.inv_snr.ValueChangedFcn = 'zef.inv_snr = str2num(zef.dipole_app.inv_snr.Value);';
+zef.dipole_app.inv_sampling_frequency.ValueChangedFcn = 'zef.inv_sampling_frequency = str2num(zef.dipole_app.inv_sampling_frequency.Value);';
+zef.dipole_app.inv_low_cut_frequency.ValueChangedFcn = 'zef.inv_low_cut_frequency = str2num(zef.dipole_app.inv_low_cut_frequency.Value);';
+zef.dipole_app.inv_high_cut_frequency.ValueChangedFcn = 'zef.inv_high_cut_frequency = str2num(zef.dipole_app.inv_high_cut_frequency.Value);';
+zef.dipole_app.inv_time_1.ValueChangedFcn = 'zef.inv_time_1 = str2num(zef.dipole_app.inv_time_1.Value);';
+zef.dipole_app.inv_time_2.ValueChangedFcn = 'zef.inv_time_2 = str2num(zef.dipole_app.inv_time_2.Value);';
+zef.dipole_app.number_of_frames.ValueChangedFcn = 'zef.number_of_frames = str2num(zef.dipole_app.number_of_frames.Value);';
+zef.dipole_app.inv_time_3.ValueChangedFcn = 'zef.inv_time_3 = str2num(zef.dipole_app.inv_time_3.Value);';
+zef.dipole_app.inv_data_segment.ValueChangedFcn = 'zef.inv_data_segment = str2num(zef.dipole_app.inv_data_segment.Value);';
+zef.dipole_app.normalize_data.ValueChangedFcn = 'zef.normalize_data = str2num(zef.dipole_app.normalize_data.Value);';
+zef.dipole_app.normalize_leadfield.ValueChangedFcn = 'zef.normalize_leadfield = str2num(zef.dipole_app.normalize_leadfield.Value);';
+
+zef.dipole_app.StartButton.ButtonPushedFcn = 'zef.reconstruction=zef_dipoleScan;';
+zef.dipole_app.CloseButton.ButtonPushedFcn = 'delete(zef.dipole_app);';
+
+%set fonts
+%set(findobj(zef.dipole_app.UIFigure.Children,'-property','FontSize'),'FontSize',zef.font_size);
