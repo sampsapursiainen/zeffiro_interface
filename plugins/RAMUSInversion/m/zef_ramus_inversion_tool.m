ee62fbad23c35edd206cb8a0dda77d209f0130be
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-06-12 16:56:32 +0300

    Add files via upload

--- plugins/RAMUSInversion/m/zef_ramus_inversion_tool.m
+++ plugins/RAMUSInversion/m/zef_ramus_inversion_tool.m
@@ -14,6 +14,9 @@ set(zef.h_ramus_ias_map_estimation,'Name','ZEFFIRO Interface: RAMUS Inversion');
 set(findobj(zef.h_ramus_ias_map_estimation.Children,'-property','FontUnits'),'FontUnits','pixels')
 set(findobj(zef.h_ramus_ias_map_estimation.Children,'-property','FontSize'),'FontSize',zef.font_size);
 
+set(zef.h_ramus_start,'Callback','zef_update_ias; [zef.reconstruction, zef.reconstruction_information] = zef_ramus_iteration([]);');
+
+
 zef_init_ramus_inversion_tool;
 uistack(flipud([zef.h_ramus_multires_n_levels; zef.h_ramus_multires_sparsity; zef.h_ramus_make_multires_dec; zef.h_ramus_hyperprior; zef.h_ramus_snr ; zef.h_ramus_multires_n_iter ; 
     zef.h_ramus_sampling_frequency; zef.h_ramus_low_cut_frequency; 
