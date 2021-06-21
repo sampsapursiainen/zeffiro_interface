f130f568bfbb140eedadf0c14ab28edfc9d5bc4e
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-06-21 11:11:34 +0300

    Update zef_update_lead_field_id.m

--- m/zef_update_lead_field_id.m
+++ m/zef_update_lead_field_id.m
@@ -1,5 +1,16 @@
-function [lead_field_id] = zef_update_lead_field_id(lead_field_id)
+function [lead_field_id,lead_field_id_max] = zef_update_lead_field_id(lead_field_id,lead_field_id_max,varargin)
 
-lead_field_id = lead_field_id + 1;
+lead_field_event = 'create';
+if not(isempty(varargin))
+lead_field_event = varargin{1};
+end
+
+if isequal(lead_field_event,'bank')
+lead_field_id = lead_field_id;
+lead_field_id_max = lead_field_id_max;
+else
+lead_field_id_max = lead_field_id_max + 1;
+lead_field_id = lead_field_id_max;
+end
 
 end
