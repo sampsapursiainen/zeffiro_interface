6c5b1508e2050cdde346ef9591673f2aa6b12a0a
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-06-28 09:31:45 +0300

    Update zef_update_ias.m

--- plugins/IASInversion/m/zef_update_ias.m
+++ plugins/IASInversion/m/zef_update_ias.m
@@ -14,6 +14,8 @@
     zef.inv_time_1 = str2num(get(zef.h_ias_time_1,'string')); 
     zef.inv_time_2 = str2num(get(zef.h_ias_time_2,'string')); 
     zef.inv_time_3 = str2num(get(zef.h_ias_time_3,'string')); 
+    zef.inv_low_cut_frequency = str2num(get(zef.h_ias_low_cut_frequency,'string')); 
+    zef.inv_high_cut_frequency = str2num(get(zef.h_ias_high_cut_frequency,'string'));  
     zef.inv_sampling_frequency = str2num(get(zef.h_ias_sampling_frequency,'string')); 
     zef.number_of_frames = str2num(get(zef.h_ias_number_of_frames,'string')); 
     zef.inv_snr = zef.ias_snr;
