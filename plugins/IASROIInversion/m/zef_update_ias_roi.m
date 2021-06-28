eb2489c9bda19264df43b07afb919c31baab8a24
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-06-28 09:28:27 +0300

    Update zef_update_ias_roi.m

--- plugins/IASROIInversion/m/zef_update_ias_roi.m
+++ plugins/IASROIInversion/m/zef_update_ias_roi.m
@@ -26,6 +26,8 @@ zef.iasroi_roi_threshold = str2num(get(zef.h_iasroi_roi_threshold ,'string'));
     zef.inv_time_2 = str2num(get(zef.h_iasroi_time_2,'string')); 
     zef.inv_time_3 = str2num(get(zef.h_iasroi_time_3,'string'));
     zef.inv_sampling_frequency = str2num(get(zef.h_iasroi_sampling_frequency,'string')); 
+    zef.inv_low_cut_frequency = str2num(get(zef.h_iasroi_low_cut_frequency,'string')); 
+    zef.inv_high_cut_frequency = str2num(get(zef.h_iasroi_high_cut_frequency,'string')); 
     zef.number_of_frames = str2num(get(zef.h_iasroi_number_of_frames,'string')); 
     zef.inv_snr = zef.iasroi_snr;
     
