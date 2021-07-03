a9172b2cc51660777f4562e7cb50164027a2bfef
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-07-03 21:24:53 +0300

    Add files via upload

--- m/zef_visualize_volume.m
+++ m/zef_visualize_volume.m
@@ -1,4 +1,4 @@
-[zef.sensors,zef.reuna_p,zef.reuna_t,zef.reuna_p_inf] = process_meshes([]); 
+[zef.sensors,zef.reuna_p,zef.reuna_t,zef.reuna_p_inf] = process_meshes(zef.explode_everything); 
 zef.on_screen = 1;
 zef_update_fig_details;plot_volume([]); 
 zef.stop_movie = 0; 
