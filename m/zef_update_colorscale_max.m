c147879e31bdb8f228f916b15f1f596aa0a58306
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-08-12 13:56:32 +0300

    Add files via upload

--- m/zef_update_colorscale_max.m
+++ m/zef_update_colorscale_max.m
@@ -3,7 +3,7 @@ function slider_value_new = zef_update_colorscale_max
 slider_value_old = evalin('base','zef.colorscale_max_slider');
 slider_value_new = evalin('base','zef.h_colorscale_max_slider.Value');
 clim_vec = evalin('base','zef.h_axes1.CLim');
-clim_vec(2) = clim_vec(2)/10^(-slider_value_old);
+clim_vec(2) = clim_vec(2)/10^(slider_value_old);
 clim_vec(2) = clim_vec(2)/10^(slider_value_new);
 
 evalin('base',['zef.h_axes1.CLim = [' num2str(clim_vec) ' ];']);
