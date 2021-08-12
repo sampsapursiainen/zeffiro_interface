38f5fb0d266e5312e2d864b9b7eb9ae3765333bd
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-08-12 13:58:31 +0300

    Add files via upload

--- m/zef_update_colorscale_max.m
+++ m/zef_update_colorscale_max.m
@@ -4,7 +4,7 @@ slider_value_old = evalin('base','zef.colorscale_max_slider');
 slider_value_new = evalin('base','zef.h_colorscale_max_slider.Value');
 clim_vec = evalin('base','zef.h_axes1.CLim');
 clim_vec(2) = clim_vec(2)/10^(slider_value_old);
-clim_vec(2) = clim_vec(2)/10^(slider_value_new);
+clim_vec(2) = clim_vec(2)*10^(slider_value_new);
 
 evalin('base',['zef.h_axes1.CLim = [' num2str(clim_vec) ' ];']);
 
--- m/zef_update_colorscale_min.m
+++ m/zef_update_colorscale_min.m
@@ -4,7 +4,7 @@ slider_value_old = evalin('base','zef.colorscale_min_slider');
 slider_value_new = evalin('base','zef.h_colorscale_min_slider.Value');
 clim_vec = evalin('base','zef.h_axes1.CLim');
 clim_vec(1) = clim_vec(1)/10^(slider_value_old);
-clim_vec(1) = clim_vec(1)/10^(slider_value_new);
+clim_vec(1) = clim_vec(1)*10^(slider_value_new);
 
 evalin('base',['zef.h_axes1.CLim = [' num2str(clim_vec) ' ];']);
 
