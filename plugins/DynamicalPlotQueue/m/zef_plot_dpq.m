3edb2bac27527047164139fec619abd47c971767
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-08-17 10:50:11 +0300

    Add files via upload

--- plugins/DynamicalPlotQueue/m/zef_plot_dpq.m
+++ plugins/DynamicalPlotQueue/m/zef_plot_dpq.m
@@ -4,7 +4,7 @@ dpq_table = evalin('base','zef.dynamical_plot_queue_table');
 
 for dpq_ind = 1 : size(dpq_table,1)
 
-if dpq_table{dpq_ind,2}
+if str2num(dpq_table{dpq_ind,2})
 if isequal(dpq_table{dpq_ind,3},type)
 
 evalin('caller', dpq_table{dpq_ind,1});
