5e9c7c1b784a211b42d88da50b8b03be43181ed7
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-08-18 18:13:04 +0300

    Add files via upload

--- m/zef_decompose_dof_space.m
+++ m/zef_decompose_dof_space.m
@@ -24,67 +24,22 @@ if isempty(source_ind)
 source_ind = brain_ind;
 end
 
-if evalin('caller','exist(''h'')')
-    if isvalid(evalin('caller','h'))
-h = evalin('caller','h');
-    end
-end
-
-
 center_points = (nodes(tetrahedra(:,1),:) + nodes(tetrahedra(:,2),:) + nodes(tetrahedra(:,3),:)+ nodes(tetrahedra(:,4),:))/4;
-source_points = center_points(source_ind,:)';
 center_points = center_points(brain_ind,:);
 
 if dof_decomposition_type == 1
-
-center_points = center_points';    
-    
+   
+   
 dof_positions = (nodes(tetrahedra(source_ind,1),:) + nodes(tetrahedra(source_ind,2),:) + nodes(tetrahedra(source_ind,3),:)+ nodes(tetrahedra(source_ind,4),:))/4;
 
 size_center_points = size(center_points,2); 
 size_source_points = size(center_points,2); 
 
-par_num = evalin('base','zef.parallel_vectors');
-bar_ind = ceil(size_center_points/(50*par_num));
+MdlKDT = KDTreeSearcher(dof_positions);
+decomposition_ind  = knnsearch(MdlKDT,center_points);
 
-use_gpu  = evalin('base','zef.use_gpu');
-gpu_num  = evalin('base','zef.gpu_num');
 
-waitbar(1/size_center_points,h,['Source decomposition.']); 
-
-source_interpolation_aux = zeros(size_center_points,1);
-ones_vec = ones(size(source_points,2),1);
-
-if use_gpu == 1 & gpuDeviceCount > 0
-center_points = gpuArray(center_points);
-source_points = gpuArray(source_points);
-source_interpolation_aux = gpuArray(source_interpolation_aux);
-end 
-
-i_ind = 0;
-tic
-for i = 1 : par_num : size_center_points
-
-i_ind = i_ind + 1;
-block_ind = [i: min(i+par_num-1,size_center_points)];
-aux_vec = center_points(:,block_ind);
-aux_vec = reshape(aux_vec,3,1,length(block_ind));
-norm_vec = sum((source_points(:,:,ones(1,length(block_ind))) - aux_vec(:,ones_vec,:)).^2);
-[min_val min_ind] = min(norm_vec,[],2);
-source_interpolation_aux(block_ind) = min_ind(:);
-
-time_val = toc;
-if i == 1 
-waitbar(i/size_center_points,h,['Source decomposition.']); 
-elseif mod(i_ind,bar_ind)==0 
-waitbar(i/size_center_points,h,['Source decomposition. Ready: ' datestr(datevec(now+(size_center_points/i - 1)*time_val/86400)) '.']);
-end
-
-end
-
-source_interpolation_aux = gather(source_interpolation_aux);
-decomposition_ind = source_interpolation_aux;
-[aux_vec, i_a, i_c] = unique(source_interpolation_aux);
+[aux_vec, i_a, i_c] = unique(decomposition_ind);
 decomposition_count = accumarray(i_c,1);
 decomposition_pointe = i_a;
 
