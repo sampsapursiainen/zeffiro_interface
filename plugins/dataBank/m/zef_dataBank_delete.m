7084c463908b57b75c0233c8462f469f3cda31a2
frank-neugebauer <37830689+frank-neugebauer@users.noreply.github.com>
2021-08-18 14:20:37 +0300

    Add files via upload

--- plugins/dataBank/m/zef_dataBank_delete.m
+++ plugins/dataBank/m/zef_dataBank_delete.m
@@ -5,7 +5,7 @@ hashNames=fieldnames(tree);
 
 for i=1:length(hashNames)
     
-    if startsWith(hashNames{i}, parentHash)
+    if startsWith(hashNames{i}, strcat(parentHash, '_')) || strcmp(hashNames{i}, parentHash) %only children should be deleted. node_11 should not be deleted by node_1
         if isobject(tree.(hashNames{i}).data)
             delete(tree.(hashNames{i}).data.Properties.Source);
         end
