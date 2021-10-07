282c6b193593d075bab6d3638b496e6009babeb3
JoonasJL <65402073+JoonasJL@users.noreply.github.com>
2021-10-07 21:00:47 +0300

    Add files via upload

--- plugins/GMMClustering/v2/m/zef_GMM_subs_time_vars.m
+++ plugins/GMMClustering/v2/m/zef_GMM_subs_time_vars.m
@@ -7,21 +7,35 @@ function zef_GMM_subs_time_vars(char_var)
 if evalin('base','isfield(zef.GMM,''time_variables'')')
 if evalin('base','~isempty(zef.GMM.time_variables)')
 if strcmp(char_var,'in')
+    if evalin('base','isfield(zef.GMM.time_variables,''time_1'')')
     evalin('base','zef_temp_time_1 = zef.inv_time_1;');
-    evalin('base','zef_temp_time_2 = zef.inv_time_2;');
-    evalin('base','zef_temp_time_3 = zef.inv_time_3;');
-    evalin('base','zef_temp_sampling_frequency = zef.inv_sampling_frequency;');
-    
     evalin('base','zef.inv_time_1 = zef.GMM.time_variables.time_1;');
+    end
+    if evalin('base','isfield(zef.GMM.time_variables,''time_2'')')
+    evalin('base','zef_temp_time_2 = zef.inv_time_2;');
     evalin('base','zef.inv_time_2 = zef.GMM.time_variables.time_2;');
+    end
+    if evalin('base','isfield(zef.GMM.time_variables,''time_3'')')
+    evalin('base','zef_temp_time_3 = zef.inv_time_3;');
     evalin('base','zef.inv_time_3 = zef.GMM.time_variables.time_3;');
+    end
+    if evalin('base','isfield(zef.GMM.time_variables,''sampling_frequency'')')
+    evalin('base','zef_temp_sampling_frequency = zef.inv_sampling_frequency;');
     evalin('base','zef.inv_sampling_frequency = zef.GMM.time_variables.sampling_frequency;');
+    end
 elseif strcmp(char_var,'out')
-    evalin('base','zef.inv_time_1 = zef_temp_time_1;');
-    evalin('base','zef.inv_time_2 = zef_temp_time_2;');
-    evalin('base','zef.inv_time_3 = zef_temp_time_3;');
-    evalin('base','zef.inv_sampling_frequency = zef_temp_sampling_frequency;');
-    evalin('base','clear zef_temp_time_1 zef_temp_time_2 zef_temp_time_3 zef_temp_sampling_frequency;');
+    if evalin('base','isfield(zef.GMM.time_variables,''time_1'')')
+    evalin('base','zef.inv_time_1 = zef_temp_time_1; clear zef_temp_time_1;');
+    end
+    if evalin('base','isfield(zef.GMM.time_variables,''time_2'')')
+    evalin('base','zef.inv_time_2 = zef_temp_time_2; clear zef_temp_time_2;');
+    end
+    if evalin('base','isfield(zef.GMM.time_variables,''time_3'')')
+    evalin('base','zef.inv_time_3 = zef_temp_time_3; clear zef_temp_time_2;');
+    end
+    if evalin('base','isfield(zef.GMM.time_variables,''sampling_frequency'')')
+    evalin('base','zef.inv_sampling_frequency = zef_temp_sampling_frequency; clear zef_temp_sampling_frequency;');
+    end
 end
 end
 end
