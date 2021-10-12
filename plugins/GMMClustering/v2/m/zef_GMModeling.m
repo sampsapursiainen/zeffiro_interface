83e1335a29480968bdf11426250aba450e26de02
JoonasJL <65402073+JoonasJL@users.noreply.github.com>
2021-10-12 13:48:51 +0300

    Update zef_GMModeling.m

--- plugins/GMMClustering/v2/m/zef_GMModeling.m
+++ plugins/GMMClustering/v2/m/zef_GMModeling.m
@@ -68,6 +68,13 @@ clear p_ind_aux_1 p_ind_aux_2 p_ind p_selected source_ind_aux
 threshold = str2num(parameters{5});
 reg_value = str2num(parameters{15});
 amp_est_type = str2num(parameters{21});
+meta1 = evalin('base','zef.GMM.meta{1}');
+smooth_std = str2num(parameters{meta1+6});
+initial_mode = parameters{meta1+2};
+model_criterion = parameters{meta1+1};
+replicates =str2num(parameters{meta1+3});
+logpdfThreshold = 10^(-str2num(parameters{meta1+4})/20);
+r_squared = chi2inv(str2num(parameters{meta1+5}),5);
 
 if iscell(z_vec)
     T=str2num(parameters{18});
@@ -86,7 +93,7 @@ end
 waitbar(0,h,['Step 1 of ',num2str(T),'. Please wait.']);
 tic;
 for t=t_start:T
-    best_BIC = Inf;
+    best_fit = Inf;
     if T > 1
         time_val = toc;
     end
@@ -125,6 +132,16 @@ for t=t_start:T
     end
     J = sqrt(z);      %current density
     z = z./max(z);
+    %Gaussian moothing step
+    if smooth_std > 0
+        ind = find(z>=threshold);
+        for i = 1:length(ind)
+            G = exp(-sum((source_positions(ind,:)-source_positions(ind(i),:)).^2,2)/(2*smooth_std^2));
+            G = G'/sum(G);
+            z(ind(i)) = G*z(ind);
+        end
+    end
+  
     %Maximally expected sampling step:
     ind = z<threshold;
     z(ind) = 0;
@@ -136,25 +153,97 @@ for t=t_start:T
     ind2 = zeros(1,z_cum(end));
     ind2(z_cum-z(ind)+1)=1;
     activity_pos = source_positions(ind,:);
-    %activity_dir = direct(ind,:);
     activity_dir = [atan2(sqrt(sum((direct(ind,[1,2]).^2),2)),direct(ind,3)),atan2(direct(ind,2),direct(ind,1))];
     
     activity_space = [activity_pos(cumsum(ind2),:),activity_dir(cumsum(ind2),:)];    
-    %calculate Gaussian mixature models:
-    try
-        GMModel_aux = fitgmdist(activity_space,k,'CovarianceType',Sigma, ...
-            'SharedCovariance',SharedCovariance,'Options',options);
-    catch
-        GMModel_aux = fitgmdist(activity_space,k,'CovarianceType',Sigma, ...
-            'SharedCovariance',SharedCovariance,'RegularizationValue',reg_value,'Options',options);
+    
+    if strcmp(initial_mode,'1')
+        %calculate Gaussian mixature models:
+        try
+            GMModel_aux = fitgmdist(activity_space,k,'CovarianceType',Sigma, ...
+                'SharedCovariance',SharedCovariance,'Start','plus','Replicates',replicates,'Options',options);
+        catch
+            GMModel_aux = fitgmdist(activity_space,k,'CovarianceType',Sigma, ...
+                'SharedCovariance',SharedCovariance,'Start','plus','Replicates',replicates,'RegularizationValue',reg_value,'Options',options);
+        end
+    elseif strcmp(initial_mode,'2') || strcmp(initial_mode,'3')
+        index_vec1=ones(size(activity_space,1),1);
+        if strcmp(initial_mode,'2')
+        if k>1
+        [index_vec1, ~,~, mylogpdf]=cluster(GMModel_aux, activity_space);
+        counter=0;
+        index_vec2=index_vec1;
+
+        while length(unique(index_vec2))~=k &&counter<256/logpdfThreshold
+            index_vec2=index_vec1;
+            counter=counter+1;
+            ind1=find(mylogpdf<max(mylogpdf)-counter*logpdfThreshold);
+       
+            index_vec2(ind1)=k;
+        end
+
+        index_vec1=index_vec2;
+        
+        if length(unique(index_vec1))~=k
+            disp('breaking');
+            break;
+        end
+        
+        
+        end
+        elseif strcmp(initial_mode,'3')
+            if k>1
+                [index_vec1, ~,~, ~, MahalanobisD2]=cluster(GMModel_aux, activity_space);
+                ind = ones(size(MahalanobisD2,1),1);
+                for kk = 1:(k-1)
+                    ind1=MahalanobisD2(:,kk)<r_squared;
+                    index_vec1(ind1)=kk;
+                    ind = ind & not(ind1);
+                    index_vec1(ind)=k;
+                end      
+             if length(unique(index_vec1))~=k
+                disp(['The ',num2str(k),'-component GMM was not realized.'])
+                break;
+            end
+            end
+        end
+
+        %modified Start-Option
+        try
+            GMModel_aux = fitgmdist(activity_space,k,'CovarianceType',Sigma, ...
+                'SharedCovariance',SharedCovariance,'Start',index_vec1, 'Options',options);
+        catch
+            GMModel_aux = fitgmdist(activity_space,k,'CovarianceType',Sigma, ...
+                'SharedCovariance',SharedCovariance,'Start',index_vec1,'RegularizationValue',reg_value,'Options',options);
+        end
     end
     
-    if GMModel_aux.BIC < best_BIC
-            best_BIC = GMModel_aux.BIC;
+    if strcmp(model_criterion,'1')
         if T>1
             GMModel{t} = GMModel_aux;
         else
             GMModel = GMModel_aux;
+        end
+    elseif strcmp(model_criterion,'2')
+        if GMModel_aux.BIC < best_fit
+                best_fit = GMModel_aux.BIC;
+            if T>1
+                GMModel{t} = GMModel_aux;
+            else
+                GMModel = GMModel_aux;
+            end
+        end
+    elseif strcmp(model_criterion,'3')
+        density = pdf(gmdistribution(GMModel_aux.mu(:,1:3),GMModel_aux.Sigma(1:3,1:3,:),GMModel_aux.ComponentProportion),source_positions);
+        fit = sum((z/sum(z)-density/sum(density)).^2);
+        disp(fit)
+        if fit < best_fit
+            best_fit = fit;
+            if T>1
+                GMModel{t} = GMModel_aux;
+            else
+                GMModel = GMModel_aux;
+            end
         end
     end
     
@@ -169,8 +258,7 @@ for t=t_start:T
         for k = 1:size(GMModel{t}.mu,1)
             [~,ind2(k)] = min(sum((GMModel{t}.mu(k,1:3)-source_positions).^2,2));
         end
-        disp(['Relative centroid current densities at the frame ',num2str(t),': ',num2str(J(ind2)'/max(J))])
-        %GMModelDipoles{t} = J(ind2).*GMModel{t}.mu(:,4:6);
+        disp(['Relative centroid point current densities at the frame ',num2str(t),': ',num2str(J(ind2)'/max(J))])
         GMModelDipoles{t} = [cos(GMModel{t}.mu(:,5)).*sin(GMModel{t}.mu(:,4)),sin(GMModel{t}.mu(:,5)).*sin(GMModel{t}.mu(:,4)),cos(GMModel{t}.mu(:,4))];
         if amp_est_type == 1
             amp = J(ind2);
@@ -181,14 +269,13 @@ for t=t_start:T
         end
         GMModelDipoles{t} = amp.*GMModelDipoles{t};
         GMModelAmplitudes{t} = amp;
-        %GMModelAmplitudes{t} = GMModelAmplitudes{t}/max(GMModelAmplitudes{t});
         waitbar((t-t_start+1)/(T-t_start+1),h,['Frame ',num2str(t),' of ',num2str(T),'. ',date_str]);
     else
        ind2 = [];
         for k = 1:size(GMModel.mu,1)
             [~,ind2(k)] = min(sum((GMModel.mu(k,1:3)-source_positions).^2,2));
         end
-        disp(['Relative centroid current densities: ',num2str(J(ind2)'/max(J))]) 
+        disp(['Relative centroid point current densities: ',num2str(J(ind2)'/max(J))]) 
         %GMModelDipoles = J(ind2).*GMModel.mu(:,4:6);
         GMModelDipoles = [cos(GMModel.mu(:,5)).*sin(GMModel.mu(:,4)),sin(GMModel.mu(:,5)).*sin(GMModel.mu(:,4)),cos(GMModel.mu(:,4))];
         if amp_est_type == 1
@@ -200,7 +287,6 @@ for t=t_start:T
         end
         GMModelDipoles = amp.*GMModelDipoles;
         GMModelAmplitudes = amp;
-        %GMModelAmplitudes = GMModelAmplitudes/max(GMModelAmplitudes);
     end
 
 end     %end of t loop
