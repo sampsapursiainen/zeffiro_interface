567cb03e16d5e8c3658df41f3f5db84167507ae0
JoonasJL <65402073+JoonasJL@users.noreply.github.com>
2021-10-15 15:33:45 +0300

    Add files via upload

--- plugins/DynamicalPlotQueue/m/dynamical_plot_queue_bank/zef_dpq_plot_GMM.m
+++ plugins/DynamicalPlotQueue/m/dynamical_plot_queue_bank/zef_dpq_plot_GMM.m
@@ -2,35 +2,37 @@ function zef_PlotGMModel
 %This function plots the point clouds created by the GMM tool.
 
 t = evalin('caller','f_ind');
+parameters = evalin('base','zef.GMM.parameters.Values');
+num_of_vars2 = evalin('base','zef.GMM.meta{2}');
 
-m_size = str2num(evalin('base','zef.GMM.parameters.Values{8}'));
-m_width = str2num(evalin('base','zef.GMM.parameters.Values{9}'));
-m_sym = evalin('base','zef.GMM.parameters.Values{7}');
-s_length = str2num(evalin('base','zef.GMM.parameters.Values{16}'))^2;
-plot_ellipsoids = evalin('base','zef.GMM.parameters.Values{11}');
-ellip_trans = str2num(evalin('base','zef.GMM.parameters.Values{12}'));
-comp_ord = evalin('base','zef.GMM.parameters.Values{21}');
-ellip_coloring_type = evalin('base','zef.GMM.parameters.Values{26}');
-ellip_components = str2num(evalin('base','zef.GMM.parameters.Values{25}'));
-dip_components = str2num(evalin('base','zef.GMM.parameters.Values{24}'));
+m_size = str2num(parameters{8});
+m_width = str2num(parameters{9});
+m_sym = parameters{7};
+s_length = str2num(parameters{16})^2;
+plot_ellipsoids = parameters{11};
+ellip_trans = str2num(parameters{12});
+comp_ord = parameters{num_of_vars2+1};
+ellip_coloring_type = parameters{num_of_vars2+6};
+ellip_components = str2num(parameters{num_of_vars2+5});
+dip_components = str2num(parameters{num_of_vars2+4});
 if strcmp(comp_ord,'3')
-    ellip_num = min(str2num(evalin('base','zef.GMM.parameters.Values{23}')),length(ellip_components));
-    dip_num = min(str2num(evalin('base','zef.GMM.parameters.Values{22}')),length(dip_components));
+    ellip_num = min(str2num(parameters{num_of_vars2+3}),length(ellip_components));
+    dip_num = min(str2num(parameters{num_of_vars2+2}),length(dip_components));
 else
-    ellip_num = str2num(evalin('base','zef.GMM.parameters.Values{23}'));
-    dip_num = str2num(evalin('base','zef.GMM.parameters.Values{22}'));
+    ellip_num = str2num(parameters{num_of_vars2+3});
+    dip_num = str2num(parameters{num_of_vars2+2});
 end
 
-start_t = str2num(evalin('base','zef.GMM.parameters.Values{13}'));
-stop_t = str2num(evalin('base','zef.GMM.parameters.Values{14}'));
+start_t = str2num(parameters{13});
+stop_t = str2num(parameters{14});
 
-identcov = evalin('base','zef.GMM.parameters.Values{4}');
-covtype = evalin('base','zef.GMM.parameters.Values{3}');
+identcov = parameters{4};
+covtype = parameters{3};
 
-K = max(str2num(evalin('base','zef.GMM.parameters.Values{1}')));
+K = max(str2num(parameters{1}));
 GMModel = evalin('base','zef.GMM.model');
 GMMdipoles = evalin('base','zef.GMM.dipoles');
-alpha = str2num(evalin('base','zef.GMM.parameters.Values{6}'))/100;
+alpha = str2num(parameters{6})/100;
 r = sqrt(chi2inv(alpha,3));
 
 
@@ -42,7 +44,7 @@ if strcmp(ellip_coloring_type,'1')
         colors = colors(randperm(K),:);
     end
 else
-    colors = evalin('base','zef.GMM.parameters.Values{27}');
+    colors = parameters{num_of_vars2+7};
     if ~iscell(colors)
         colors = str2num(colors);
         if size(colors,2) < 3 || size(colors,2) > 3
@@ -174,7 +176,8 @@ end
 %plot centroid marks:
 plot3(h,GMModel.mu(dip_ind,1),GMModel.mu(dip_ind,2),GMModel.mu(dip_ind,3),m_sym,'LineWidth',m_width,'MarkerSize',m_size)
 %set direction vectors (original can be non-unit length)
-direct = s_length*GMModel.mu(dip_ind,4:6)./sqrt(sum(GMModel.mu(dip_ind,4:6).^2,2));
+%direct = s_length*GMModel.mu(dip_ind,4:6)./sqrt(sum(GMModel.mu(dip_ind,4:6).^2,2));
+direct = s_length*[cos(GMModel.mu(dip_ind,5)).*sin(GMModel.mu(dip_ind,4)),sin(GMModel.mu(dip_ind,5)).*sin(GMModel.mu(dip_ind,1)),cos(GMModel.mu(dip_ind,4))];
 quiver3(h,GMModel.mu(dip_ind,1),GMModel.mu(dip_ind,2),GMModel.mu(dip_ind,3),direct(:,1),direct(:,2),direct(:,3),0,'color',erase(m_sym,'o'), 'linewidth',m_width,'MarkerSize',m_size);
 hold(h,'off')%set old time parameters back to their places:
 evalin('base','zef_GMM_subs_time_vars(''out'')');
@@ -260,9 +263,8 @@ end
 %plot centroid marks:
 plot3(h,GMModel{t}.mu(dip_ind,1),GMModel{t}.mu(dip_ind,2),GMModel{t}.mu(dip_ind,3),m_sym,'LineWidth',m_width,'MarkerSize',m_size)
 %set direction vectors (original can be non-unit length)
-direct = s_length*GMModel{t}.mu(dip_ind,4:6)./sqrt(sum(GMModel{t}.mu(dip_ind,4:6).^2,2));
+direct = s_length*[cos(GMModel{t}.mu(dip_ind,5)).*sin(GMModel{t}.mu(dip_ind,4)),sin(GMModel{t}.mu(dip_ind,5)).*sin(GMModel{t}.mu(dip_ind,4)),cos(GMModel{t}.mu(dip_ind,4))];
 quiver3(h,GMModel{t}.mu(dip_ind,1),GMModel{t}.mu(dip_ind,2),GMModel{t}.mu(dip_ind,3),direct(:,1),direct(:,2),direct(:,3),0,'color',erase(m_sym,'o'),'linewidth',m_width,'MarkerSize',m_size);
-
 
 %set old frame values and time parameters back to their places:
 
