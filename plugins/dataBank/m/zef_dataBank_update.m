92c7a717d7192033a9e3b18151ad3fc5b7b031bc
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-11-26 16:20:32 +0200

    Add files via upload

---
+++ plugins/dataBank/m/zef_dataBank_update.m
@@ -0,0 +1,3 @@
+zef.dataBank.var_starttime = zef.dataBank.app.StarttimeSpinner.Value;
+zef.dataBank.var_endtime = zef.dataBank.app.EndtimeSpinner.Value;
+zef.dataBank.var_sampling_frequency = zef.dataBank.app.SfreqSpinner.Value;
