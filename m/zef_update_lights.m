bf2d11eb71c0752d26cd13dcc4c84a72bf8e3359
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-11-28 04:09:39 +0200

    Add files via upload

--- m/zef_update_lights.m
+++ m/zef_update_lights.m
@@ -8,6 +8,8 @@ end
 
 h_2 = evalin('base','findobj(get(gcf,''Children''),''Tag'',''lightsselection'');');
 h_3 = findobj(h_1.Children,'Type','Light');
+h_axes1 = evalin('base','zef.h_axes1');
+lights_vec = evalin('base','zef.update_lights');
 
 if h_2.Value == 1
 
@@ -16,30 +18,44 @@ delete(h_3);
 light(h_1,'Position',[0 0 1],'Style','infinite');
 light(h_1,'Position',[0 0 -1],'Style','infinite');
 
+lights_vec = 1;
    
 elseif h_2.Value == 2
 
 delete(h_3);
+lights_vec = [];    
 
 elseif h_2.Value == 3
 
 light(h_1,'Position',[1 0 0],'Style','infinite');
 light(h_1,'Position',[-1 0 0 ],'Style','infinite');
 
+lights_vec = [lights_vec ; 3];
+
+
 elseif h_2.Value == 4
 
 light(h_1,'Position',[0 1 0],'Style','infinite');
 light(h_1,'Position',[0 -1 0 ],'Style','infinite');
+    
+lights_vec = [lights_vec ; 4];
 
 elseif h_2.Value == 5
 
 light(h_1,'Position',[0 0 1],'Style','infinite');
 light(h_1,'Position',[0 0 -1 ],'Style','infinite');
+    
+lights_vec = [lights_vec ; 5];
 
 elseif h_2.Value == 6
 
 camlight(h_1,'headlight')
+lights_vec = [lights_vec ; 6];
     
+end
+
+if not(isequal(h_1,h_axes1))
+    lights_vec = evalin('base','zef.update_lights');
 end
 
 end
