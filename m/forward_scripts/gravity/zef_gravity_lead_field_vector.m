896634b81d25348f557f0c9e3cf38a81f95e2dd1
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2022-01-12 02:07:05 +0200

    Add files via upload

---
+++ m/forward_scripts/gravity/zef_gravity_lead_field_vector.m
@@ -0,0 +1,6 @@
+warning('off');
+zef.gravity_field_type = 4; 
+zef_delete_original_field; 
+zef_process_meshes; 
+[zef.L,  zef.bg_data, zef.source_positions, zef.source_directions] = lead_field_gravity(zef.nodes,zef.tetra,zef.rho,zef.sensors,zef.brain_ind); 
+warning('on');
