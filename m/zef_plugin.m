7c205c5c268b4d001328d7dfac2467cf969ab1aa
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2022-01-13 02:40:58 +0200

    Add files via upload

--- m/zef_plugin.m
+++ m/zef_plugin.m
@@ -5,9 +5,14 @@ if isempty(zef.plugin_cell)
 zef.plugin_cell = readcell([zef.program_path '/profile/' zef.profile_name '/zeffiro_plugins.ini'],'filetype','text','delimiter',',');
 end
 for zef_i = 1 : size(zef.plugin_cell,1)
-zef.h_menu = findobj(zef.h_zeffiro_window_main,'Tag',zef.plugin_cell{zef_i,2});
-zef.h_menu = uimenu(zef.h_menu,'label',zef.plugin_cell{zef_i,1},'callback',[zef.plugin_cell{zef_i,3} '; zef_update;'] );
+zef.h_menu_1 = findobj(zef.h_zeffiro_window_main,'Tag',zef.plugin_cell{zef_i,2});
+zef.h_menu_2 = findobj(zef.h_menu_1.Children,'label',zef.plugin_cell{zef_i,1});
+if not(isempty(zef.h_menu_2))
+    set(zef.h_menu_2,'Callback',[zef.plugin_cell{zef_i,3} '; zef_update;']);
+else
+zef.h_menu_2 = uimenu(zef.h_menu_1,'label',zef.plugin_cell{zef_i,1},'callback',[zef.plugin_cell{zef_i,3} '; zef_update;'] );
+end
 end
 clear zef_i;
-zef = rmfield(zef,'h_menu');
+zef = rmfield(zef,{'h_menu_1','h_menu_2'});
 
