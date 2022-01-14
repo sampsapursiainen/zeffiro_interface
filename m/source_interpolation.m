4b9232ade0859a9c701b6c1bbb3d4f7c3c9d34f8
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2022-01-14 16:01:52 +0200

    Update source_interpolation.m

--- m/source_interpolation.m
+++ m/source_interpolation.m
@@ -119,6 +119,11 @@ waitbar(1,h,['Interpolation 3.']);
 
 close(h)
 
+if nargout == 0
+assignin('base','zef_data', struct('source_interpolation_ind',source_interpolation_ind));
+evalin('base','zef_assign_data');
+end
+
 end
 
 end
