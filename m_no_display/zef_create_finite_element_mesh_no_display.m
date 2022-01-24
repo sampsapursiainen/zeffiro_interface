824606eb59bcd52ca972fdf0def85e585681a919
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2022-01-24 09:57:20 +0200

    Delete zef_create_finite_element_mesh_no_display.m

--- m_no_display/zef_create_finite_element_mesh_no_display.m
+++
@@ -1,9 +0,0 @@
-zef.file = 'multicompartment_head_project';
-zef.file_path = [zef.program_path '/data/'];
-zef_load;
-zef_create_finite_element_mesh;
-zef.save_switch = 1; 
-zef.file = 'multicompartment_head_project_fem_mesh';
-zef_save; 
-zef.file = 'multicompartment_head_fem_mesh';
-zef_export_fem_mesh_as;
