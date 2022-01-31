fc7d84e862f9404d489d9e5f464350cdbe8c89a9
GitFernandoGalaz <fernando.galazprieto@tuni.fi>
2022-01-31 16:50:31 +0200

    Add files via upload

--- m/zef_triangulate_surface.m
+++ m/zef_triangulate_surface.m
@@ -2,12 +2,12 @@ function [nodes, triangles] = zef_triangulate_surface(X, Y, Z, varargin)
 
 max_faces = 0;
 if not(isempty(varargin))
-max_faces = varargin{1};
+    max_faces = varargin{1};
 end
 
 patch_data = surf2patch(X,Y,Z,'triangles');
 if max_faces > 0
-patch_data = reducepatch(patch_data,max_faces);
+    patch_data = reducepatch(patch_data,max_faces);
 end
 nodes = patch_data.vertices;
 triangles = patch_data.faces;
