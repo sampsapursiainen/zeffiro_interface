2dfe55565fa159519071b8ffd75b93a0aa99f1d0
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2022-02-03 13:16:09 +0200

    Add files via upload

--- m/zef_apply_system_settings.m
+++ m/zef_apply_system_settings.m
@@ -7,3 +7,13 @@ evalin('base',['zef.' zef.ini_cell{zef_i,3} ' = ' num2str(zef.ini_cell{zef_i,2})
 end
 end
 zef = rmfield(zef,'ini_cell');
+if isfield(zef,'ini_cell_mod')
+for zef_i =  1 : size(zef.ini_cell_mod,1)   
+if isequal(zef.ini_cell_mod{zef_i,4},'number')
+evalin('base',['zef.' zef.ini_cell_mod{zef_i,3} ' = ' num2str(zef.ini_cell_mod{zef_i,2}) ';']);
+    elseif isequal(zef.ini_cell_mod{zef_i,4},'string')
+        evalin('base',['zef.' zef.ini_cell_mod{zef_i,3} ' = ''' (zef.ini_cell_mod{zef_i,2}) ''';']);
+end
+end
+zef = rmfield(zef,'ini_cell_mod');
+end
