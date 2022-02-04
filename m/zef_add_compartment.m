28b3e1691b8f8d57f8e23083725c292a637cd43f
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2022-02-04 21:17:03 +0200

    Add files via upload

--- m/zef_add_compartment.m
+++ m/zef_add_compartment.m
@@ -1,18 +1,21 @@
-zef.aux_field = zef.h_compartment_table.Data;
-zef_create_compartment(['c' num2str(length(zef.compartment_tags)+1)]);
-zef_i = size(zef.aux_field,1) + 1;
-zef.aux_field{zef_i,1} = evalin('base',['zef.' zef.compartment_tags{1} '_priority']);
-zef.aux_field{zef_i,2} = evalin('base',['zef.' zef.compartment_tags{1} '_name']);
-zef.aux_field{zef_i,3} = evalin('base',['zef.' zef.compartment_tags{1} '_on']);
-zef.aux_field{zef_i,4} = evalin('base',['zef.' zef.compartment_tags{1} '_visible']);
-zef.aux_field{zef_i,5} = evalin('base',['zef.' zef.compartment_tags{1} '_merge']);
-zef.aux_field{zef_i,6} = evalin('base',['zef.' zef.compartment_tags{1} '_invert']);
-zef.aux_field{zef_i,7} = zef.h_compartment_table.ColumnFormat{7}{2+evalin('base',['zef.' zef.compartment_tags{1} '_sources'])};
-zef.aux_field{zef_i,8} = evalin('base',['zef.' zef.compartment_tags{1} '_sigma']);
+zef.aux_field_1 = zef.h_compartment_table.Data;
 
-zef.h_compartment_table.Data = zef.aux_field;
+zef_create_compartment(zef_compartment_tag);
+zef_i = size(zef.aux_field_1,1) + 1;
+zef_j = 1; 
 
-zef = rmfield(zef,'aux_field');
+zef_init_fields_compartment_table;
+
+zef_apply_parameter_profile;
+
+zef_i = size(zef.aux_field_1,1);
+zef_j = 1;
+
+zef_init_fields_compartment_table_profile;
+
+zef.h_compartment_table.Data = zef.aux_field_1;
+
+zef = rmfield(zef,'aux_field_1');
 clear zef_i;
 
 zef_update;
