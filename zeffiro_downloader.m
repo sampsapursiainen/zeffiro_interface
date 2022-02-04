0b8f7f6c74725b02b6b2e3daa6871dc685c2264b
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2022-02-04 11:04:35 +0200

    Update zeffiro_downloader.m

--- zeffiro_downloader.m
+++ zeffiro_downloader.m
@@ -1,1 +1,4 @@
-!git clone https://github.com/sampsapursiainen/zeffiro_interface
+function zeffiro_downloader(varargin)
+%This script will install Zeffiro Interface in the current folder of this script.
+!git clone https://github.com/sampsapursiainen/zeffiro_interface ./
+end
