b64b7037f70ee8fe2a122b3e633e208ec2c8ff33
Sampsa Pursiainen (TAU) <pursiain@wks-95038-mac.ad.tuni.fi>
2022-02-06 20:23:03 +0200

    Zeffiro developer: A regular push adding the changes made in the current local repository to the remote origin. Contents of the folders ./data/ and ./profile/ are ignored. The update necessitates creating a personal access token.

--- plugins/GithubPusher/m/zef_github_updater_start.m
+++ plugins/GithubPusher/m/zef_github_updater_start.m
@@ -1,11 +1,10 @@
 zef_data = zef_github_updater;
 zef_assign_data;
-zef.h_github_updater.Name = 'ZEFFIRO Interface: GitHub updater tool';
-zef.h_github_message.Value = 'Regular push.';
+zef.h_github_updater.Name = 'ZEFFIRO Interface: GitHub pusher tool';
+zef.h_github_message.Value = 'A regular push adding the changes made in the current local repository to the remote origin. Contents of the folders ./data/ and ./profile/ are ignored. The update necessitates creating a personal access token.';
 zef.h_github_author.Value = zef.user_tag;
 
 zef.h_github_updater_button.ButtonPushedFcn = 'zef_githup_updater_script;';
-
 
 set(findobj(zef.h_github_updater.Children,'-property','FontSize'),'FontSize',zef.font_size);
 
