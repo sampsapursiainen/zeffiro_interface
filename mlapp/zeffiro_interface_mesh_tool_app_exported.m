c821fe18ef86f38e2551e9bc76b34d7ff0abdfb0
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2022-02-06 03:28:33 +0200

    Add files via upload

--- mlapp/zeffiro_interface_mesh_tool_app_exported.m
+++ mlapp/zeffiro_interface_mesh_tool_app_exported.m
@@ -34,7 +34,6 @@ classdef zeffiro_interface_mesh_tool_app_exported < matlab.apps.AppBase
         CuttingplanecoeffEditFieldLabel_9  matlab.ui.control.Label
         h_inflate_strength            matlab.ui.control.NumericEditField
         h_inflate_n_iterations        matlab.ui.control.NumericEditField
-        h_plot_condition              matlab.ui.control.Button
         h_forward_simulation_table    matlab.ui.control.Table
         h_save_forward_simulation_profile  matlab.ui.control.Button
         h_forward_simulation_script   matlab.ui.control.TextArea
@@ -44,8 +43,6 @@ classdef zeffiro_interface_mesh_tool_app_exported < matlab.apps.AppBase
         h_menu_forward_simulation_table_delete  matlab.ui.container.Menu
     end
 
-
-
     % Component initialization
     methods (Access = private)
 
@@ -54,160 +51,155 @@ classdef zeffiro_interface_mesh_tool_app_exported < matlab.apps.AppBase
 
             % Create h_mesh_tool and hide until all components are created
             app.h_mesh_tool = uifigure('Visible', 'off');
-            app.h_mesh_tool.Position = [1000 200 720 411];
+            app.h_mesh_tool.Position = [1000 200 719 367];
             app.h_mesh_tool.Name = 'ZEFFIRO Interface: Mesh tool';
 
             % Create h_pushbutton21
             app.h_pushbutton21 = uibutton(app.h_mesh_tool, 'push');
-            app.h_pushbutton21.Position = [17 363 165 39];
-            app.h_pushbutton21.Text = {'Create'; 'finite element mesh'};
+            app.h_pushbutton21.Position = [17 319 165 39];
+            app.h_pushbutton21.Text = {'Create'; 'FEM mesh'};
 
             % Create h_run_forward_simulation
             app.h_run_forward_simulation = uibutton(app.h_mesh_tool, 'push');
-            app.h_run_forward_simulation.Position = [599 14 105 39];
+            app.h_run_forward_simulation.Position = [599 17 105 39];
             app.h_run_forward_simulation.Text = 'Run script';
 
             % Create h_interpolate
             app.h_interpolate = uibutton(app.h_mesh_tool, 'push');
-            app.h_interpolate.Position = [16 271 165 39];
+            app.h_interpolate.Position = [16 227 165 39];
             app.h_interpolate.Text = 'Source interpolation';
 
             % Create h_field_downsampling
             app.h_field_downsampling = uibutton(app.h_mesh_tool, 'push');
-            app.h_field_downsampling.Position = [16 317 165 39];
+            app.h_field_downsampling.Position = [16 273 165 39];
             app.h_field_downsampling.Text = 'Downsample field';
 
             % Create h_surface_downsampling
             app.h_surface_downsampling = uibutton(app.h_mesh_tool, 'push');
-            app.h_surface_downsampling.Position = [190 317 165 39];
+            app.h_surface_downsampling.Position = [190 273 165 39];
             app.h_surface_downsampling.Text = 'Downsample surfaces';
 
             % Create h_checkbox_mesh_smoothing_on
             app.h_checkbox_mesh_smoothing_on = uicheckbox(app.h_mesh_tool);
             app.h_checkbox_mesh_smoothing_on.Text = 'Mesh smoothing';
-            app.h_checkbox_mesh_smoothing_on.Position = [192 171 164 22];
+            app.h_checkbox_mesh_smoothing_on.Position = [192 174 164 22];
 
             % Create h_refinement_on
             app.h_refinement_on = uicheckbox(app.h_mesh_tool);
             app.h_refinement_on.Text = 'Refinement';
-            app.h_refinement_on.Position = [20 171 102 22];
+            app.h_refinement_on.Position = [20 174 102 22];
 
             % Create h_source_interpolation_on
             app.h_source_interpolation_on = uicheckbox(app.h_mesh_tool);
             app.h_source_interpolation_on.Text = 'LF source interp.';
-            app.h_source_interpolation_on.Position = [20 195 161 22];
+            app.h_source_interpolation_on.Position = [20 198 161 22];
 
             % Create h_downsample_surfaces
             app.h_downsample_surfaces = uicheckbox(app.h_mesh_tool);
             app.h_downsample_surfaces.Text = 'Downsample surf.';
-            app.h_downsample_surfaces.Position = [192 195 163 22];
+            app.h_downsample_surfaces.Position = [192 198 163 22];
 
             % Create SurfacetrianglesmaxLabel
             app.SurfacetrianglesmaxLabel = uilabel(app.h_mesh_tool);
-            app.SurfacetrianglesmaxLabel.Position = [20 90 102 22];
+            app.SurfacetrianglesmaxLabel.Position = [20 93 102 22];
             app.SurfacetrianglesmaxLabel.Text = 'Surface triangles max.:';
 
             % Create CuttingplanecoeffEditFieldLabel_4
             app.CuttingplanecoeffEditFieldLabel_4 = uilabel(app.h_mesh_tool);
-            app.CuttingplanecoeffEditFieldLabel_4.Position = [131 141 100 22];
+            app.CuttingplanecoeffEditFieldLabel_4.Position = [131 144 100 22];
             app.CuttingplanecoeffEditFieldLabel_4.Text = 'Mesh resolution:';
 
             % Create CuttingplanecoeffEditFieldLabel_5
             app.CuttingplanecoeffEditFieldLabel_5 = uilabel(app.h_mesh_tool);
-            app.CuttingplanecoeffEditFieldLabel_5.Position = [245 141 87 22];
+            app.CuttingplanecoeffEditFieldLabel_5.Position = [245 144 87 22];
             app.CuttingplanecoeffEditFieldLabel_5.Text = 'Meshing accuracy:';
 
             % Create CuttingplanecoeffEditFieldLabel_6
             app.CuttingplanecoeffEditFieldLabel_6 = uilabel(app.h_mesh_tool);
-            app.CuttingplanecoeffEditFieldLabel_6.Position = [20 141 85 22];
+            app.CuttingplanecoeffEditFieldLabel_6.Position = [20 144 85 22];
             app.CuttingplanecoeffEditFieldLabel_6.Text = 'Source/Field count:';
 
             % Create CuttingplanecoeffEditFieldLabel_7
             app.CuttingplanecoeffEditFieldLabel_7 = uilabel(app.h_mesh_tool);
-            app.CuttingplanecoeffEditFieldLabel_7.Position = [131 90 89 22];
+            app.CuttingplanecoeffEditFieldLabel_7.Position = [131 93 89 22];
             app.CuttingplanecoeffEditFieldLabel_7.Text = 'Smoothing strength:';
 
             % Create UnitLabel
             app.UnitLabel = uilabel(app.h_mesh_tool);
             app.UnitLabel.Visible = 'off';
-            app.UnitLabel.Position = [245 172 47 22];
+            app.UnitLabel.Position = [245 175 47 22];
             app.UnitLabel.Text = 'Unit:';
 
             % Create h_popupmenu6
             app.h_popupmenu6 = uidropdown(app.h_mesh_tool);
             app.h_popupmenu6.Visible = 'off';
-            app.h_popupmenu6.Position = [298 171 49 22];
+            app.h_popupmenu6.Position = [298 174 49 22];
 
             % Create DirectionsDropDownLabel
             app.DirectionsDropDownLabel = uilabel(app.h_mesh_tool);
-            app.DirectionsDropDownLabel.Position = [245 37 111 22];
+            app.DirectionsDropDownLabel.Position = [245 40 111 22];
             app.DirectionsDropDownLabel.Text = 'Directions:';
 
             % Create h_popupmenu2
             app.h_popupmenu2 = uidropdown(app.h_mesh_tool);
-            app.h_popupmenu2.Position = [265 14 82 22];
+            app.h_popupmenu2.Position = [265 17 82 22];
 
             % Create CuttingplanecoeffEditFieldLabel_8
             app.CuttingplanecoeffEditFieldLabel_8 = uilabel(app.h_mesh_tool);
-            app.CuttingplanecoeffEditFieldLabel_8.Position = [245 90 110 22];
+            app.CuttingplanecoeffEditFieldLabel_8.Position = [245 93 110 22];
             app.CuttingplanecoeffEditFieldLabel_8.Text = 'Solver tolerance:';
 
             % Create h_edit65
             app.h_edit65 = uieditfield(app.h_mesh_tool, 'numeric');
-            app.h_edit65.Position = [186 115 50 22];
+            app.h_edit65.Position = [186 118 50 22];
 
             % Create h_edit_meshing_accuracy
             app.h_edit_meshing_accuracy = uieditfield(app.h_mesh_tool, 'numeric');
-            app.h_edit_meshing_accuracy.Position = [297 114 50 22];
+            app.h_edit_meshing_accuracy.Position = [297 117 50 22];
 
             % Create h_smoothing_strength
             app.h_smoothing_strength = uieditfield(app.h_mesh_tool, 'numeric');
-            app.h_smoothing_strength.Position = [186 63 50 22];
+            app.h_smoothing_strength.Position = [186 66 50 22];
 
             % Create h_edit76
             app.h_edit76 = uieditfield(app.h_mesh_tool, 'numeric');
-            app.h_edit76.Position = [297 62 50 22];
+            app.h_edit76.Position = [297 65 50 22];
 
             % Create h_edit75
             app.h_edit75 = uieditfield(app.h_mesh_tool, 'numeric');
-            app.h_edit75.Position = [75 116 50 22];
+            app.h_edit75.Position = [75 119 50 22];
 
             % Create h_max_surface_face_count
             app.h_max_surface_face_count = uieditfield(app.h_mesh_tool, 'numeric');
-            app.h_max_surface_face_count.Position = [75 64 50 22];
+            app.h_max_surface_face_count.Position = [75 67 50 22];
 
             % Create h_pushbutton23
             app.h_pushbutton23 = uibutton(app.h_mesh_tool, 'push');
-            app.h_pushbutton23.Position = [190 271 165 39];
+            app.h_pushbutton23.Position = [190 227 165 39];
             app.h_pushbutton23.Text = 'Apply transform';
 
             % Create h_pushbutton34
             app.h_pushbutton34 = uibutton(app.h_mesh_tool, 'push');
-            app.h_pushbutton34.Position = [191 363 165 39];
-            app.h_pushbutton34.Text = {'Postprocess'; 'finite element mesh'};
+            app.h_pushbutton34.Position = [191 319 165 39];
+            app.h_pushbutton34.Text = {'Postprocess'; 'FEM mesh'};
 
             % Create InflatingiterationsLabel
             app.InflatingiterationsLabel = uilabel(app.h_mesh_tool);
-            app.InflatingiterationsLabel.Position = [21 37 104 22];
+            app.InflatingiterationsLabel.Position = [21 40 104 22];
             app.InflatingiterationsLabel.Text = 'Inflating iterations:';
 
             % Create CuttingplanecoeffEditFieldLabel_9
             app.CuttingplanecoeffEditFieldLabel_9 = uilabel(app.h_mesh_tool);
-            app.CuttingplanecoeffEditFieldLabel_9.Position = [132 37 99 22];
+            app.CuttingplanecoeffEditFieldLabel_9.Position = [132 40 99 22];
             app.CuttingplanecoeffEditFieldLabel_9.Text = 'Inflating strength:';
 
             % Create h_inflate_strength
             app.h_inflate_strength = uieditfield(app.h_mesh_tool, 'numeric');
-            app.h_inflate_strength.Position = [186 12 50 22];
+            app.h_inflate_strength.Position = [186 15 50 22];
 
             % Create h_inflate_n_iterations
             app.h_inflate_n_iterations = uieditfield(app.h_mesh_tool, 'numeric');
-            app.h_inflate_n_iterations.Position = [75 12 50 22];
-
-            % Create h_plot_condition
-            app.h_plot_condition = uibutton(app.h_mesh_tool, 'push');
-            app.h_plot_condition.Position = [192 225 164 39];
-            app.h_plot_condition.Text = {'Plot FEM mesh'; 'condition'};
+            app.h_inflate_n_iterations.Position = [75 15 50 22];
 
             % Create h_forward_simulation_table
             app.h_forward_simulation_table = uitable(app.h_mesh_tool);
@@ -215,20 +207,20 @@ classdef zeffiro_interface_mesh_tool_app_exported < matlab.apps.AppBase
             app.h_forward_simulation_table.RowName = {};
             app.h_forward_simulation_table.ColumnSortable = true;
             app.h_forward_simulation_table.ColumnEditable = true;
-            app.h_forward_simulation_table.Position = [366 171 338 231];
+            app.h_forward_simulation_table.Position = [366 175 338 183];
 
             % Create h_save_forward_simulation_profile
             app.h_save_forward_simulation_profile = uibutton(app.h_mesh_tool, 'push');
-            app.h_save_forward_simulation_profile.Position = [366 14 105 39];
+            app.h_save_forward_simulation_profile.Position = [366 17 105 39];
             app.h_save_forward_simulation_profile.Text = 'Save profile';
 
             % Create h_forward_simulation_script
             app.h_forward_simulation_script = uitextarea(app.h_mesh_tool);
-            app.h_forward_simulation_script.Position = [366 62 338 101];
+            app.h_forward_simulation_script.Position = [366 65 338 101];
 
             % Create h_forward_simulation_update_from_profile
             app.h_forward_simulation_update_from_profile = uibutton(app.h_mesh_tool, 'push');
-            app.h_forward_simulation_update_from_profile.Position = [483 14 105 39];
+            app.h_forward_simulation_update_from_profile.Position = [483 17 105 39];
             app.h_forward_simulation_update_from_profile.Text = {'Update from'; 'profile'};
 
             % Create h_menu_forward_simulation_table_context
