9a5fd84929d62789930b11ddfa6d818c76dcbad7
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2022-02-07 11:53:20 +0200

    Update zeffiro_interface.m

--- zeffiro_interface.m
+++ zeffiro_interface.m
@@ -9,7 +9,7 @@ function zeffiro_interface(varargin)
 %different operations. The list of properties (and their values) is the
 %following: 
 %
-%start_mode
+%start_mode (display/nodisplay), 
 
 if evalin('base','exist(''zef'');')
 error('It looks like that another instance of Zeffiro interface already open. To enable this script, clear zef by command ''clear zef'' in the base workspace.')
