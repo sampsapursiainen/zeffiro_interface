cdbf35aff0db359d496a742ee6e20236a529d852
kolorun <koloruntobi@gmail.com>
2022-02-08 18:53:30 +0200

    tobi:  The quick brown fox  Jumps over the lazy dog

--- plugins/GithubPusher/m/zef_githup_updater_script.m
+++ plugins/GithubPusher/m/zef_githup_updater_script.m
@@ -1,1 +1,1 @@
-zef_git_push(zef.h_github_pat.Value,'message',['''''''' zef.h_github_author.Value  ': ' char(join(string(zef.h_github_message.Value))) '''''''']);
+zef_git_push(zef.h_github_pat.Value,'message',[zef.h_github_author.Value  ': ' char(join(string(zef.h_github_message.Value)))]);
