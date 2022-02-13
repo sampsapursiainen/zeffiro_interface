785b827dd47f944f832aee776679c7846878a8ea
Sampsa Pursiainen (TAU) <pursiain@wks-95038-mac.ad.tuni.fi>
2022-02-14 01:11:25 +0200

    Sampsa: A regular push adding the changes made in the current local repository to the remote origin. Contents of the folders ./data/ and ./profile/ are ignored. The update necessitates creating a personal access token.

--- m/zef_fix_negatives.m
+++ m/zef_fix_negatives.m
@@ -44,13 +44,34 @@ if not(isempty(I))
 vec_2 = nodes(setdiff(tetra_c(g(I(1)),:),tetra_c(c(i),:)),:);
 tetra_ind = find(sum(ismember(tetra_c,tetra_c(c(i),j)),2));
 tri = zef_surface_mesh(tetra_c, [], tetra_ind);
+u_tri = unique(tri);
+vec_3 = mean(nodes(u_tri,:));
+
+p_in_c = zef_point_in_cluster(nodes,tri,vec_3);
+if p_in_c < evalin('base','zef.meshing_threshold') 
 [~, lambda_1] = zef_find_intersecting_triangle(vec_1, vec_2,1,tri,nodes,'nonconvex');
 if not(isempty(lambda_1))
 vec_3 = vec_1 - lambda_1*(vec_2 - vec_1);
 [~, lambda_1] = zef_find_intersecting_triangle(vec_3, vec_2,-1,tri,nodes,'nonconvex');
 if not(isempty(lambda_1))
-nodes(tetra_c(c(i),j),:) = vec_3 - fix_param*lambda_1*(vec_2 - vec_3);
+vec_3 = vec_3 - fix_param*lambda_1*(vec_2 - vec_3);
 end
+end
+end
+
+node_ind = 0;
+p_in_c = zef_point_in_cluster(nodes,tri,vec_3);
+while  isempty(p_in_c)  && node_ind < length(u_tri)
+node_ind = node_ind + 1;
+vec_2 = nodes(u_tri(node_ind),:);
+[~, lambda_1] = zef_find_intersecting_triangle(vec_3, vec_2,-1,tri,nodes,'nonconvex');
+if not(isempty(lambda_1))
+vec_3 = vec_3 - fix_param*lambda_1*(vec_2 - vec_3);
+end
+end
+
+nodes(tetra_c(c(i),j),:) = vec_3;
+
 end
 end
 
@@ -58,7 +79,6 @@ if isempty(find(zef_condition_number(nodes,tetra_c([c(i);g(:)],:))<0))
 fix_ind = j;
 end
 
-end
 
 if fix_ind == 0
 nodes(tetra_c(c(i),j),:) = vec_1;
