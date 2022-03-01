107b8f902e20aa3331e4de7e91d147b47edcb7a1
Sampsa Pursiainen (TAU) <pursiain@wks-95038-mac.ad.tuni.fi>
2022-03-01 10:50:04 +0200

    Sampsa: Conflicts resolved.

--- m/zef_init_init_profile.m
+++ m/zef_init_init_profile.m
@@ -3,7 +3,6 @@ for zef_i = 1 : size(zef.init_profile,1)
     if not(isstring(zef.init_profile{zef_i,2}))
         zef.init_profile{zef_i,2} = num2str(zef.init_profile{zef_i,2});
     end
-<<<<<<< HEAD
     
     if isequal(zef.init_profile{zef_i,4},'string') 
     evalin('base',['zef.' zef.init_profile{zef_i,3} '= ''' zef.init_profile{zef_i,2}  ''';']); 
@@ -11,13 +10,6 @@ for zef_i = 1 : size(zef.init_profile,1)
     evalin('base',['zef.' zef.init_profile{zef_i,3} '= ' zef.init_profile{zef_i,2}  ';']); 
     elseif isequal(zef.init_profile{zef_i,4},'evaluate') 
     evalin('base',zef.init_profile{zef_i,2}); 
-=======
-
-    if isequal(zef.init_profile{zef_i,4},'string')
-    evalin('base',['zef.' zef.init_profile{zef_i,3} '= ''' zef.init_profile{zef_i,2}  ''';']);
-    elseif isequal(zef.init_profile{zef_i,4},'number')
-    evalin('base',['zef.' zef.init_profile{zef_i,3} '= ' zef.init_profile{zef_i,2}  ';']);
->>>>>>> 5b5c6faff4d84098629188095d4264ae682a8077
     end
 
 end
