f92c05f43bd493515b0a72494c9c294f06a125d2
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-03-29 15:12:07 +0300

    zef_tetra_gradient_field: renamed argument eval_points → eval_point_inds
    These are the indices of the evaluation points, not the points themselves.

--- m/zef_tetra_gradient_field.m
+++ m/zef_tetra_gradient_field.m
@@ -1,4 +1,4 @@
-function [G1, G2, G3] = zef_tetra_gradient_field(nodes, tetrahedra, tensor, eval_points, K, N)
+function [G1, G2, G3] = zef_tetra_gradient_field(nodes, tetrahedra, tensor, eval_point_inds, K, N)
 
 % Calculates a gradient field [G1,G2,G3] for a given set of nodes, tetrahedra,
 % volume current tensor, gradient field evaluation points and matrix size K
@@ -15,13 +15,13 @@ function [G1, G2, G3] = zef_tetra_gradient_field(nodes, tetrahedra, tensor, eval
 
      for i = 1 : 4
 
-        grad = cross(nodes(tetrahedra(eval_points,ind_m(i,2)),:)'-nodes(tetrahedra(eval_points,ind_m(i,1)),:)', nodes(tetrahedra(eval_points,ind_m(i,3)),:)'-nodes(tetrahedra(eval_points,ind_m(i,1)),:)')/6;
-        grad = repmat(sign(dot(grad,(nodes(tetrahedra(eval_points,i),:)'-nodes(tetrahedra(eval_points,ind_m(i,1)),:)'))),3,1).*grad;
-        grad = grad ./ tilavuus(eval_points);
+        grad = cross(nodes(tetrahedra(eval_point_inds,ind_m(i,2)),:)'-nodes(tetrahedra(eval_point_inds,ind_m(i,1)),:)', nodes(tetrahedra(eval_point_inds,ind_m(i,3)),:)'-nodes(tetrahedra(eval_point_inds,ind_m(i,1)),:)')/6;
+        grad = repmat(sign(dot(grad,(nodes(tetrahedra(eval_point_inds,i),:)'-nodes(tetrahedra(eval_point_inds,ind_m(i,1)),:)'))),3,1).*grad;
+        grad = grad ./ tilavuus(eval_point_inds);
 
-        entry_vec_1 = zeros(1,size(eval_points,1));
-        entry_vec_2 = zeros(1,size(eval_points,1));
-        entry_vec_3 = zeros(1,size(eval_points,1));
+        entry_vec_1 = zeros(1,size(eval_point_inds,1));
+        entry_vec_2 = zeros(1,size(eval_point_inds,1));
+        entry_vec_3 = zeros(1,size(eval_point_inds,1));
         for k = 1 : 6
             switch k
                 case 1
@@ -46,33 +46,33 @@ function [G1, G2, G3] = zef_tetra_gradient_field(nodes, tetrahedra, tensor, eval
             if k <= 3
                 switch k_1
                     case 1
-                        entry_vec_1 = entry_vec_1 + tensor(k,eval_points).*grad(k_2,:);
+                        entry_vec_1 = entry_vec_1 + tensor(k,eval_point_inds).*grad(k_2,:);
                     case 2
-                        entry_vec_2 = entry_vec_2 + tensor(k,eval_points).*grad(k_2,:);
+                        entry_vec_2 = entry_vec_2 + tensor(k,eval_point_inds).*grad(k_2,:);
                     case 3
-                        entry_vec_3 = entry_vec_3 + tensor(k,eval_points).*grad(k_2,:);
+                        entry_vec_3 = entry_vec_3 + tensor(k,eval_point_inds).*grad(k_2,:);
                 end
             else
                 switch k_1
                     case 1
-                        entry_vec_1 = entry_vec_1 + tensor(k,eval_points).*grad(k_2,:);
+                        entry_vec_1 = entry_vec_1 + tensor(k,eval_point_inds).*grad(k_2,:);
                     case 2
-                        entry_vec_2 = entry_vec_2 + tensor(k,eval_points).*grad(k_2,:);
+                        entry_vec_2 = entry_vec_2 + tensor(k,eval_point_inds).*grad(k_2,:);
                     case 3
-                        entry_vec_3 = entry_vec_3 + tensor(k,eval_points).*grad(k_2,:);
+                        entry_vec_3 = entry_vec_3 + tensor(k,eval_point_inds).*grad(k_2,:);
                 end
                 switch k_2
                     case 1
-                        entry_vec_1 = entry_vec_1 + tensor(k,eval_points).*grad(k_1,:);
+                        entry_vec_1 = entry_vec_1 + tensor(k,eval_point_inds).*grad(k_1,:);
                     case 2
-                        entry_vec_2 = entry_vec_2 + tensor(k,eval_points).*grad(k_1,:);
+                        entry_vec_2 = entry_vec_2 + tensor(k,eval_point_inds).*grad(k_1,:);
                     case 3
-                        entry_vec_3 = entry_vec_3 + tensor(k,eval_points).*grad(k_1,:);
+                        entry_vec_3 = entry_vec_3 + tensor(k,eval_point_inds).*grad(k_1,:);
                 end
             end
         end
-        Grad_1 = Grad_1 + sparse([1:K]',tetrahedra(eval_points,i), entry_vec_1,K,N);
-        Grad_2 = Grad_2 + sparse([1:K]',tetrahedra(eval_points,i), entry_vec_2,K,N);
-        Grad_3 = Grad_3 + sparse([1:K]',tetrahedra(eval_points,i), entry_vec_3,K,N);
+        Grad_1 = Grad_1 + sparse([1:K]',tetrahedra(eval_point_inds,i), entry_vec_1,K,N);
+        Grad_2 = Grad_2 + sparse([1:K]',tetrahedra(eval_point_inds,i), entry_vec_2,K,N);
+        Grad_3 = Grad_3 + sparse([1:K]',tetrahedra(eval_point_inds,i), entry_vec_3,K,N);
     end
 end
