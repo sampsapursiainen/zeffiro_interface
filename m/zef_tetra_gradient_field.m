d0c9653f821081a4c901a8424f0d3b5fb5a1e268
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-03-29 15:54:03 +0300

    zef_tetra_gradient_field now gets a volume array as an argument
    … as it was needed in the computation. Also fixed a typo in
    lead_field_tes_fem: Grad1 → Grad_1.

--- m/zef_tetra_gradient_field.m
+++ m/zef_tetra_gradient_field.m
@@ -1,12 +1,12 @@
-function [G1, G2, G3] = zef_tetra_gradient_field(nodes, tetrahedra, tensor, eval_point_inds, K, N)
+function [G1, G2, G3] = zef_tetra_gradient_field(nodes, tetrahedra, volume, tensor, eval_point_inds, K, N)
 
 % Calculates a gradient field [G1,G2,G3] for a given set of nodes, tetrahedra,
-% volume current tensor, gradient field evaluation points and matrix size K
-% and N.
+% volume, volume current tensor, gradient field evaluation points and matrix
+% size K and N.
 
-    Grad_1 = spalloc(K,N,0);
-    Grad_2 = spalloc(K,N,0);
-    Grad_3 = spalloc(K,N,0);
+    G1 = spalloc(K,N,0);
+    G2 = spalloc(K,N,0);
+    G3 = spalloc(K,N,0);
 
     ind_m = [ 2 3 4 ;
               3 4 1 ;
@@ -17,12 +17,14 @@ function [G1, G2, G3] = zef_tetra_gradient_field(nodes, tetrahedra, tensor, eval
 
         grad = cross(nodes(tetrahedra(eval_point_inds,ind_m(i,2)),:)'-nodes(tetrahedra(eval_point_inds,ind_m(i,1)),:)', nodes(tetrahedra(eval_point_inds,ind_m(i,3)),:)'-nodes(tetrahedra(eval_point_inds,ind_m(i,1)),:)')/6;
         grad = repmat(sign(dot(grad,(nodes(tetrahedra(eval_point_inds,i),:)'-nodes(tetrahedra(eval_point_inds,ind_m(i,1)),:)'))),3,1).*grad;
-        grad = grad ./ tilavuus(eval_point_inds);
+        grad = grad ./ volume(eval_point_inds);
 
         entry_vec_1 = zeros(1,size(eval_point_inds,1));
         entry_vec_2 = zeros(1,size(eval_point_inds,1));
         entry_vec_3 = zeros(1,size(eval_point_inds,1));
+
         for k = 1 : 6
+
             switch k
                 case 1
                     k_1 = 1;
@@ -43,7 +45,9 @@ function [G1, G2, G3] = zef_tetra_gradient_field(nodes, tetrahedra, tensor, eval
                     k_1 = 2;
                     k_2 = 3;
             end
+
             if k <= 3
+
                 switch k_1
                     case 1
                         entry_vec_1 = entry_vec_1 + tensor(k,eval_point_inds).*grad(k_2,:);
@@ -52,7 +56,9 @@ function [G1, G2, G3] = zef_tetra_gradient_field(nodes, tetrahedra, tensor, eval
                     case 3
                         entry_vec_3 = entry_vec_3 + tensor(k,eval_point_inds).*grad(k_2,:);
                 end
+
             else
+
                 switch k_1
                     case 1
                         entry_vec_1 = entry_vec_1 + tensor(k,eval_point_inds).*grad(k_2,:);
@@ -61,6 +67,7 @@ function [G1, G2, G3] = zef_tetra_gradient_field(nodes, tetrahedra, tensor, eval
                     case 3
                         entry_vec_3 = entry_vec_3 + tensor(k,eval_point_inds).*grad(k_2,:);
                 end
+
                 switch k_2
                     case 1
                         entry_vec_1 = entry_vec_1 + tensor(k,eval_point_inds).*grad(k_1,:);
@@ -69,10 +76,13 @@ function [G1, G2, G3] = zef_tetra_gradient_field(nodes, tetrahedra, tensor, eval
                     case 3
                         entry_vec_3 = entry_vec_3 + tensor(k,eval_point_inds).*grad(k_1,:);
                 end
+
             end
         end
-        Grad_1 = Grad_1 + sparse([1:K]',tetrahedra(eval_point_inds,i), entry_vec_1,K,N);
-        Grad_2 = Grad_2 + sparse([1:K]',tetrahedra(eval_point_inds,i), entry_vec_2,K,N);
-        Grad_3 = Grad_3 + sparse([1:K]',tetrahedra(eval_point_inds,i), entry_vec_3,K,N);
+
+        G1 = G1 + sparse([1:K]',tetrahedra(eval_point_inds,i), entry_vec_1,K,N);
+        G2 = G2 + sparse([1:K]',tetrahedra(eval_point_inds,i), entry_vec_2,K,N);
+        G3 = G3 + sparse([1:K]',tetrahedra(eval_point_inds,i), entry_vec_3,K,N);
+
     end
 end
