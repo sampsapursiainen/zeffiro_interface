952f8b470519e0c4d7cee15201f75ebe74528923
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2022-03-30 21:59:50 +0300

    Update zeffiro_setup.m

--- zeffiro_setup.m
+++ zeffiro_setup.m
@@ -1,4 +1,4 @@
 mkdir(program_path,'external')
-eval(['!git clone https://github.com/cvxr/CVX ' program_path filesep 'external'])
+%eval(['!git clone https://github.com/cvxr/CVX ' program_path filesep 'external'])
 eval(['!git clone https://github.com/i-am-sorri/SESAME_core ' program_path filesep 'external'])
 
