46309c162f3f37c840b1be993bec43cfc32f812b
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2022-03-31 00:24:00 +0300

    Update zeffiro_setup.m

--- zeffiro_setup.m
+++ zeffiro_setup.m
@@ -1,4 +1,15 @@
-mkdir(program_path,'external')
-%eval(['!git clone https://github.com/cvxr/CVX ' program_path filesep 'external'])
-eval(['!git clone https://github.com/i-am-sorri/SESAME_core ' program_path filesep 'external'])
+%%% Install SDPT3 BEGIN %%%
+eval(['!git clone https://github.com/sqlp/sdpt3 ' program_path filesep 'external/SDPT3'])
+run([program_path filesep '/external/SDPT3/install_sdpt3.m']);
+%%% Install SDPT3 END %%%
 
+%%% Install CVX BEGIN %%%
+mkdir(program_path,'external')
+eval(['!git clone https://github.com/cvxr/CVX ' program_path filesep 'external/CVX'])
+run([program_path filesep '/external/CVX/cvx_setup.m']);
+%%% Install CVX BEGIN %%%
+
+%%% Install SESAME BEGIN %%%
+eval(['!git clone https://github.com/i-am-sorri/SESAME_core ' program_path filesep 'external/SESAME'])
+addpath(genpath([program_path '/external/SESAME']));
+%%% Install SESAME END %%%
