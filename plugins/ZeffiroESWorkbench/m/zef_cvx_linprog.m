5d582d1184d9bd7cdb3c4ce83ce45ca4cc9efc7d
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2022-03-31 09:41:09 +0300

    Add files via upload

--- plugins/ZeffiroESWorkbench/m/zef_cvx_linprog.m
+++ plugins/ZeffiroESWorkbench/m/zef_cvx_linprog.m
@@ -2,8 +2,17 @@ function [x, flag_val] = zef_cvx_linprog(z,L,b,varargin)
 
 opts = [];
 
+max_1_norm = [];
+max_infty_norm = [];
+
 if not(isempty(varargin))
-        opts = varargin{1};
+    max_1_norm = varargin{1};
+    if length(varargin) > 1
+        max_infty_norm = varargin{2};
+    end
+    if length(varargin) > 2
+        opts = varargin{3};
+    end
 end
 
 flag_val = -2;
@@ -21,6 +30,12 @@ variable x(n)
 minimize(sum(z.*x))
 subject to: 
 L*x - b <= 0;
+if not(isempty(max_1_norm))
+sum(abs(x)) <= max_1_norm;
+end
+if not(isempty(max_infty_norm))
+max(abs(x)) <= max_infty_norm;
+end
 cvx_end
 
 
