0583efe9f8317e5df3b2159421d7756d6ab290ee
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2022-04-13 12:18:32 +0300

    Add files via upload

--- m/zef_easter_colormap.m
+++ m/zef_easter_colormap.m
@@ -3,14 +3,13 @@ function [colormap_vec] = zef_easter_colormap(colortune_param, colormap_size)
 
 c_aux_1 = floor(colormap_size/2);
 colormap_vec = [[c_aux_1:-1:1]/c_aux_1 zeros(1,colormap_size-c_aux_1); ... 
-                 [1:c_aux_1]/c_aux_1 [colormap_size-c_aux_1:-1:1]/(colormap_size-c_aux_1); ...
-                 zeros(1,c_aux_1) [1:colormap_size-c_aux_1]/(colormap_size - c_aux_1)];
-colormap_vec(2,:) = colormap_vec(2,:) + colormap_vec(1,:);
+                 0.6*[1:c_aux_1]/c_aux_1 0.6*[colormap_size-c_aux_1:-1:1]/(colormap_size-c_aux_1); ...
+                 zeros(1,c_aux_1) 0.7*[1:colormap_size-c_aux_1]/(colormap_size - c_aux_1)];
+colormap_vec(2,:) = colormap_vec(2,:) + colormap_vec(1,:).^2;
 colormap_vec(3,:) = colormap_vec(3,:) + colormap_vec(1,:).^4;
-colormap_vec =  log(2*colormap_vec./colortune_param.^4+1);
+colormap_vec =  log(0.1*colormap_vec./colortune_param.^4+1);
 colormap_vec = colormap_vec'/max(colormap_vec(:));
 colormap_vec = flipud(colormap_vec);
-
 
 
 end
