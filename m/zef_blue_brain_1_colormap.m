ae1f7ebae1d251aa1ba06c07aa8d5289b72c115d
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2022-04-13 09:54:25 +0300

    Update zef_blue_brain_1_colormap.m

--- m/zef_blue_brain_1_colormap.m
+++ m/zef_blue_brain_1_colormap.m
@@ -6,7 +6,6 @@ colormap_vec = [(colormap_size/2)*[c_aux_1:-1:1]/c_aux_1 zeros(1,colormap_size-c
                  (colormap_size/3)*[1:c_aux_1]/c_aux_1 (colormap_size/3)*[colormap_size-c_aux_1:-1:1]/(colormap_size-c_aux_1); ...
                  zeros(1,c_aux_1) (colormap_size/3)*[1:colormap_size-c_aux_1]/(colormap_size - c_aux_1)];
 colormap_vec =  log(colormap_vec./colortune_param.^2+1);
-%colormap_vec =  colormap_vec.^(colortune_param);
 colormap_vec = colormap_vec'/max(colormap_vec(:));
 colormap_vec = flipud(colormap_vec);
 
