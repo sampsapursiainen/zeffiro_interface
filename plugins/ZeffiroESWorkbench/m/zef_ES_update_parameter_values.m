38f7b17642c67a3ed3226b064bb5b5b789555ed5
GitFernandoGalaz <fernando.galazprieto@tuni.fi>
2022-05-10 17:44:12 +0300

    Update zef_ES_update_parameter_values.m
    commented out delta and L1_iter.
    Added roi_range

--- plugins/ZeffiroESWorkbench/m/zef_ES_update_parameter_values.m
+++ plugins/ZeffiroESWorkbench/m/zef_ES_update_parameter_values.m
@@ -4,13 +4,13 @@ switch n_ES
         zef.ES_alpha        = 10^(str2double(zef.h_ES_parameter_table.Data{1,2})/20);
         zef.ES_alpha_max    = 10^(str2double(zef.h_ES_parameter_table.Data{2,2})/20);
         if n_ES == 1
-                        zef.ES_beta_min  = 10^(str2double(zef.h_ES_parameter_table.Data{3,2})/20);
-            zef.ES_beta       = 10^(str2double(zef.h_ES_parameter_table.Data{4,2})/20);
+            zef.ES_beta_min = 10^(str2double(zef.h_ES_parameter_table.Data{3,2})/20);
+            zef.ES_beta     = 10^(str2double(zef.h_ES_parameter_table.Data{4,2})/20);
         elseif n_ES == 2
-            zef.ES_kval        = 10^(str2double(zef.h_ES_parameter_table.Data{3,2})/20);
-            zef.ES_kval_max        = 10^(str2double(zef.h_ES_parameter_table.Data{4,2})/20);
-            zef.ES_delta        = str2double(zef.h_ES_parameter_table.Data{15,2});
-            zef.ES_L1_iter      = str2double(zef.h_ES_parameter_table.Data{16,2});
+            zef.ES_kval     = 10^(str2double(zef.h_ES_parameter_table.Data{3,2})/20);
+            zef.ES_kval_max = 10^(str2double(zef.h_ES_parameter_table.Data{4,2})/20);
+            %zef.ES_delta    = str2double(zef.h_ES_parameter_table.Data{15,2});
+            %zef.ES_L1_iter  = str2double(zef.h_ES_parameter_table.Data{16,2});
         end
         zef.ES_total_max_current            = str2double(zef.h_ES_parameter_table.Data{5,2});
         zef.ES_max_current_channel          = str2double(zef.h_ES_parameter_table.Data{6,2});
@@ -22,6 +22,7 @@ switch n_ES
         zef.ES_relative_source_amplitude    = str2double(zef.h_ES_parameter_table.Data{12,2});
         zef.ES_acceptable_threshold         = str2double(zef.h_ES_parameter_table.Data{13,2});
         zef.ES_boundary_color_limit         = str2double(zef.h_ES_parameter_table.Data{14,2});
+        zef.ES_roi_range                    = str2double(zef.h_ES_parameter_table.Data{15,2});
         
     case 3
         zef.ES_separation_angle             = str2double(zef.h_ES_parameter_table.Data{1,2});
