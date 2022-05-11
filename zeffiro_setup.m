58c821baea37062eea5f931bfa0a263b57ba4c6a
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2022-05-11 18:17:53 +0300

    Update zeffiro_setup.m

--- zeffiro_setup.m
+++ zeffiro_setup.m
@@ -1,22 +1,22 @@
 if exist([fileparts(mfilename('fullpath')) filesep 'm' filesep 'zeffiro_interface_start_config.m'])
-zef.fid_temp = fopen([fileparts(mfilename('fullpath')) filesep 'm' filesep 'zeffiro_interface_start_config.m'],'a');
+zef_data.fid_temp = fopen([fileparts(mfilename('fullpath')) filesep 'm' filesep 'zeffiro_interface_start_config.m'],'a');
 else
-zef.fid_temp = fopen([fileparts(mfilename('fullpath')) filesep 'm' filesep 'zeffiro_interface_start_config.m'],'w');
+zef_data.fid_temp = fopen([fileparts(mfilename('fullpath')) filesep 'm' filesep 'zeffiro_interface_start_config.m'],'w');
 end
 
 %%% Install SDPT3 BEGIN %%%
 eval(['!git clone https://github.com/sqlp/sdpt3 ' fileparts(mfilename('fullpath')) filesep 'external/SDPT3'])
 run([fileparts(mfilename('fullpath')) filesep '/external/SDPT3/install_sdpt3.m']);
-zef.str_temp = 'addpath([zef.program_path filesep ''/external/SDPT3/'']);';
-fprintf(zef.fid_temp, ['\n' zef.str_temp]);
+zef_data.str_temp = 'addpath([zef.program_path filesep ''/external/SDPT3/'']);';
+fprintf(zef_data.fid_temp, ['\n' zef_data.str_temp]);
 %%% Install SDPT3 END %%%
 
 %%% Install CVX BEGIN %%%
 mkdir(fileparts(mfilename('fullpath')),'external')
 eval(['!git clone https://github.com/cvxr/CVX ' fileparts(mfilename('fullpath')) filesep 'external/CVX'])
 run([fileparts(mfilename('fullpath')) filesep '/external/CVX/cvx_setup.m']);
-zef.str_temp = 'addpath([zef.program_path filesep ''/external/CVX/'']);';
-fprintf(zef.fid_temp, ['\n' zef.str_temp]);
+zef_data.str_temp = 'addpath([zef.program_path filesep ''/external/CVX/'']);';
+fprintf(zef_data.fid_temp, ['\n' zef_data.str_temp]);
 %%% Install CVX BEGIN %%%
 
 %%% Install SESAME BEGIN %%%
@@ -24,5 +24,6 @@ eval(['!git clone https://github.com/i-am-sorri/SESAME_core ' fileparts(mfilenam
 addpath(genpath([fileparts(mfilename('fullpath')) '/external/SESAME']));
 %%% Install SESAME END %%%
 
-fclose(zef.fid_temp);
+fclose(zef_data.fid_temp);
+clear zef_data;
 
