308d40be52a153e4215e0c087302b6f9ab78c2f6
paavo <paavo.ronni@gmail.com>
2022-05-13 15:32:51 +0300

    Improvements to cluster script

--- plugins/Kalman/clusterScripts/runKalmanScript.m
+++ plugins/Kalman/clusterScripts/runKalmanScript.m
@@ -1,19 +1,18 @@
 %================================================
-% Some of the setting are already applied to the 
-% saved project. For example leadfield is selected
-% and each source noise levels are already set to
-% the project file.
-% Also some of the parcellation tool settings are 
-% already selected at the project file.
+% Some of the setting might already applied to the 
+% saved project. 
 
 %
 % This script:
-% 1. creates a measurement data 
-% 2. runs kalman
-% 3. saves measurement and reconstruction to 
-%   data bank
-% 4. creates a figure with parcellation tool
-% 5. saves the figure
+
+% 1. Set noise of the measurement
+% 2. Selects the leadfield
+% 3. creates a measurement data 
+% 4. runs kalman
+% 5. saves measurement and reconstruction to 
+%    data bank
+% 6. creates a figure with parcellation tool
+% 7. saves the figure
 
 %================================================
 
@@ -22,8 +21,35 @@ zef_start_dataBank;
 zef_kf_start;
 
 find_synthetic_source;
+
+zef_parcellation_tool;
 zef_update;
-for i = 1:50
+
+% SNR 1, 3, 5, 10. 0dB, 9.542dB, 13.98dB, 20dB
+% Set noise level
+noise_level = -20;
+for zef_n = 1:length(zef.synth_source_data)
+    zef.synth_source_data{zef_n}.parameters{8,2} = num2str(noise_level);
+end
+% Default -46dB ,-25 -15, -5
+bg_noise = -46;
+zef.find_synth_source.h_bg_noise.Value = num2str(bg_noise);
+zef.fss_bg_noise = str2num(zef.find_synth_source.h_bg_noise.Value);
+
+%Click toggle buttton twice
+zef.find_synth_source.h_plot_switch.Value = '1';
+zef.find_synth_source.h_plot_switch.Value = '0';
+zef.synth_source_updated_true = false;
+
+
+% Load leadfield
+zef.dataBank.hash = 'node_5';
+zef.dataBank.loadParents=false; 
+zef_dataBank_getHashForMenu;
+zef_dataBank_setData;
+
+zef_update;
+for i = 1:2
 %find synth source
 
 
@@ -35,34 +61,61 @@ zef_update_fss;
 zef_update_fss; 
 zef.measurements = zef_find_source;
 
+inv_type = 'MNE';
+switch inv_type
+    case 'kalman'
+        % RUN KALMAN
+        zef.KF.inv_multires_n_levels.Value = '1';
+        zef.KF.inv_multires_sparsity.Value = '1';
+        zef.KF.inv_multires_n_decompositions.Value = '1';
 
 
-% RUN KALMAN
-zef.KF.inv_multires_n_levels.Value = '1';
-zef.KF.inv_multires_sparsity.Value = '1';
-zef.KF.inv_multires_n_decompositions.Value = '1';
-[zef.kf_multires_dec, zef.kf_multires_ind, zef.kf_multires_count] = make_multires_dec();
+        % Basic Kalman,EnKF,Kalman sLORETA 1,2,3
+        zef.KF.filter_type.Value = '1';
+        zef.KF.number_of_ensembles.Value = '100';
+        %Spatially balanced, Spatially constant 1,2
+        zef.KF.mne_prior.Value = '1';
+        zef.KF.inv_snr.Value = '26';
+        zef.KF.inv_sampling_frequency.Value = '5000';
+        zef.KF.inv_low_cut_frequency.Value = '0';
+        zef.KF.inv_high_cut_frequency.Value = '0';
+        zef.KF.inv_time_1.Value = '0.121';
+        zef.KF.inv_time_2.Value = '0';
+        zef.KF.number_of_frames.Value = '48';
+        zef.KF.inv_time_3.Value = '0.0002';
 
-% Basic Kalman,EnKF,Kalman sLORETA 1,2,3
-zef.KF.filter_type.Value = '1';
-zef.KF.number_of_ensembles.Value = '100';
-%Spatially balanced, Spatially constant 1,2
-zef.KF.mne_prior.Value = '1';
-zef.KF.inv_snr.Value = '26';
-zef.KF.inv_sampling_frequency.Value = '5000';
-zef.KF.inv_low_cut_frequency.Value = '0';
-zef.KF.inv_high_cut_frequency.Value = '0';
-zef.KF.inv_time_1.Value = '0.121';
-zef.KF.inv_time_2.Value = '0';
-zef.KF.number_of_frames.Value = '20';
-zef.KF.inv_time_3.Value = '0.0005';
+        zef.KF.normalize_data.Value = '4';
+        zef.KF.kf_smoothing.Value = '1';
 
-zef.KF.normalize_data.Value = '4';
-zef.KF.kf_smoothing.Value = '1';
+        %eval('base',zef.KF.ApplyButton.ButtonPushedFcn);
+        zef_props = properties(zef.KF); 
+        for zef_i = 1:length(zef_props) 
+            if isprop(zef.KF.(zef_props{zef_i}),'Value') 
+                zef.(zef_props{zef_i}) = str2num(zef.KF.(zef_props{zef_i}).Value); 
+            end
+        end
+        clear zef_props zef_i;
+        [zef.kf_multires_dec, zef.kf_multires_ind, zef.kf_multires_count] = make_multires_dec();
+        [zef.reconstruction, zef.reconstruction_information] = zef_KF;
+        % END KALMAN
+    case 'MNE'
+        % balanced, constant 1, 2
+        zef.mne_prior = 2;
+        % 1,2,3 MNE, dspm, sloreta
+        zef.mne_type = 3;
+        zef.mne_sampling_frequency = 5000;
+        zef.mne_low_cut_frequency = 0;
+        zef.mne_high_cut_frequency = 0;
+        %1,2,3,4
+        zef.mne_normalize_data = 4;
+        zef.mne_time_1 = 0.121;
+        zef.mne_time_2 = 0;
+        zef.mne_time_3 = 0.0002;
+        zef.mne_number_of_frames = 48;
+        [zef.reconstruction, zef.reconstruction_information] = zef_find_mne_reconstruction([]);
+end
 
-eval('base',zef.KF.ApplyButton.ButtonPushedFcn);
 
-[zef.reconstruction, zef.reconstruction_information] = zef_KF;
 
 % save to databank
 
@@ -72,11 +125,14 @@ zef_dataBank_addButtonPress;
 zef.dataBank.app.Entrytype.Value = 'reconstruction';
 zef_dataBank_addButtonPress;
 
-zef_parcellation_tool;
-zef_update;
+
 
 zef.h_parcellation_plot_type.Value = 20;
 zef.h_time_series_tools_list.Value = 21;
+
+% Take sg006 and lh023
+zef.h_parcellation_list.Value = [23,78];
+zef.parcellation_selected = get(zef.h_parcellation_list,'value');
 
 zef.parcellation_time_series = zef_parcellation_time_series([]);
 zef_plot_parcellation_time_series([]);
--- plugins/Kalman/clusterScripts/run_cluster_job.m
+++ plugins/Kalman/clusterScripts/run_cluster_job.m
@@ -1,10 +1,13 @@
-function ret = run_cluster_job(file_path)
+function ret = run_cluster_job(file_path, profiler_on)
 try
-profile on
+if profiler_on
+    profile on
+end
 ret = cell(0);
 ret{1} = maxNumCompThreads;
 %file_path = '/scratch/project_2002680/Kalman/Noise14dB/data/';
-%file_path = '/media/datadisk/paavo/thesis_results/Noise14dB50Cases/';
+%file_path = '/media/datadisk/paavo/thesis_results/TestProject/';
+
 orig_file = 'P20N20_SEP_withbig.mat';
 save_file = 'P20N20_SEP_withbig_save.mat';
 fullpath = fullfile(file_path,orig_file);
@@ -13,12 +16,14 @@ zeffiro_interface('start_mode','nodisplay',...
     'open_project', fullpath, ...
     'run_script', append('zef.save_file_path = ', '''',file_path,'''', ';'),...
     'run_script', append('zef.save_file = ', '''',save_file,'''', ';'),...
-    'run_script', 'untitled2',...
+    'run_script', 'runKalmanScript',...
     'run_script', 'zef.save_switch = 7;',...
     'run_script', 'zef_save_nodisplay;');
 ret{2} = toc;
-profile off
-profsave(profile('info'), fullfile(file_path,'myResults'));
+if profiler_on
+    profile off
+    profsave(profile('info'), fullfile(file_path,'myResults'));
+end
 catch exception
     ret{3} = exception;
 end
