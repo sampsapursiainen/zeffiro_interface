ae8fe79e2c727a168e612356538d22477fd13b5f
JoonasJL <65402073+JoonasJL@users.noreply.github.com>
2022-07-14 19:55:42 +0300

    Add files via upload

--- plugins/ClassicalSparseMethods/zef_CSM_iteration.m
+++ plugins/ClassicalSparseMethods/zef_CSM_iteration.m
@@ -42,7 +42,9 @@ reconstruction_information.number_of_frames = evalin('base','zef.number_of_frame
 
 [L,n_interp, procFile] = zef_processLeadfields(source_direction_mode);
 
+
 [theta0] = zef_find_gaussian_prior(snr_val-pm_val,L,size(L,2),evalin('base','zef.normalize_data'),0);
+
 
 if evalin('base','zef.use_gpu') == 1 && gpuDeviceCount > 0
     L = gpuArray(L);
@@ -51,6 +53,7 @@ end
 z = cell(number_of_frames,1);
 f_data = zef_getFilteredData;
 size_f = size(f_data,2);
+
 
 %_ Time Serie Loop _
 tic;
@@ -62,7 +65,7 @@ for f_ind = 1 : number_of_frames
     end
     f=zef_getTimeStep(f_data, f_ind, false);
     z_vec = nan(size(L,2),1);
-
+    
     if evalin('base','zef.use_gpu') == 1 && gpuDeviceCount > 0
         f = gpuArray(f);
     end
@@ -73,18 +76,19 @@ end
 
 %___ Calculations start ___
 
+    
     if method_type == 1 || method_type == 2 || method_type == 3
-
+        
             S_mat = (std_lhood^2/theta0)*eye(size(L,1));
     if evalin('base','zef.use_gpu') == 1 && gpuDeviceCount > 0
 
         S_mat = gpuArray(S_mat);
     end
-
+    
     if size_f > 1
         f = mean(f,2);
     end
-
+        
     %__ dSPM __
     %Source covariance
     P = L'/(L*L'+S_mat);
@@ -136,12 +140,12 @@ end
             end
         end
     end
-    if f_ind > 1;
+    if f_ind > 1;    
         waitbar(f_ind/number_of_frames,h,['Step ' int2str(f_ind) ' of ' int2str(number_of_frames) '. Ready: ' date_str '.' ]);
     end
-
+    
 elseif method_type == 4
-
+    
         S_mat = (std_lhood^2)*eye(size(L,1));
     if evalin('base','zef.use_gpu') == 1 && gpuDeviceCount > 0
         S_mat = gpuArray(S_mat);
@@ -159,7 +163,7 @@ elseif method_type == 4
         const = gather(const);
         L = gather(L);
     end
-
+    
     for i = 1:size(L,2)
         const(i) = 1/(rank(L(:,i)*L(:,i)')*size(f,2));
     end
@@ -168,13 +172,13 @@ elseif method_type == 4
         gamma = gpuArray(gamma);
         L = gpuArray(L);
     end
-
+   
     for i = 1:n_iter
-        if f_ind > 1;
+        if f_ind > 1;    
             waitbar(i/n_iter,h,['Step ' int2str(f_ind) ' of ' int2str(number_of_frames) '. Ready: ' date_str '.' ]);
         else
             waitbar(i/n_iter,h,['SBL MAP iteration. Time step ' int2str(f_ind) ' of ' int2str(number_of_frames) '.' ]);
-        end;
+        end;        
         f_aux = inv_sqrt_C*f;
         L_aux = inv_sqrt_C*L;
         gamma = const.*gamma.*sum((L_aux'*f_aux).^2,2)./(size(L,2)-gamma.*sum(L_aux.^2,1)');
@@ -192,7 +196,7 @@ z_vec = gather(z_vec);
 end
 
 %-------------Calculations end---------------
-z{f_ind}=z_vec;
+z{f_ind}=z_vec;   
 end
 
 z = zef_postProcessInverse(z, procFile);
