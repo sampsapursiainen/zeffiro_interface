bd7481c254019d87886071d105e4e39c59b555e9
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-08-24 15:25:35 +0300

    MeshTool: added some widgets to the left layout

--- +app/@MeshTool/MeshTool.m
+++ +app/@MeshTool/MeshTool.m
@@ -29,6 +29,65 @@ classdef MeshTool < handle
 
         rightlayout3 matlab.ui.container.GridLayout
 
+        % leftlayout1 components
+
+        fem_mesh_btn matlab.ui.control.Button
+
+        pp_fem_mesh_btn matlab.ui.control.Button
+
+        resamble_field_btn matlab.ui.control.Button
+
+        resample_surf_btn matlab.ui.control.Button
+
+        source_interpolation_btn matlab.ui.control.Button
+
+        apply_transform_btn matlab.ui.control.Button
+
+        % leftlayout2 components
+
+        lf_source_int_cb matlab.ui.control.CheckBox
+        downsample_surf_cb matlab.ui.control.CheckBox
+        refinement_cb matlab.ui.control.CheckBox
+        mesh_smoothing_cb matlab.ui.control.CheckBox
+
+        % leftlayout3 components
+
+        source_count_ef matlab.ui.control.EditField
+
+        surf_tri_max_ef matlab.ui.control.EditField
+
+        inflating_iter_ef matlab.ui.control.EditField
+
+        mesh_resolution_ef matlab.ui.control.EditField
+
+        smoothing_str_ef matlab.ui.control.EditField
+
+        inflating_str_ef matlab.ui.control.EditField
+
+        meshing_acc_ef matlab.ui.control.EditField
+
+        solver_tol_ef matlab.ui.control.EditField
+
+        dir_dd matlab.ui.control.DropDown
+
+        source_count_label matlab.ui.control.Label
+
+        surf_tri_max_label matlab.ui.control.Label
+
+        inflating_iter_label matlab.ui.control.Label
+
+        mesh_resolution_label matlab.ui.control.Label
+
+        smoothing_str_label matlab.ui.control.Label
+
+        inflating_str_label matlab.ui.control.Label
+
+        meshing_acc_label matlab.ui.control.Label
+
+        solver_tol_label matlab.ui.control.Label
+
+        dir_label matlab.ui.control.Label
+
     end % properties
 
     methods
@@ -47,7 +106,7 @@ classdef MeshTool < handle
 
             self.leftlayout1 = uigridlayout(self.leftlayout, [3,2]);
 
-            self.leftlayout2 = uigridlayout(self.leftlayout, [2,4]);
+            self.leftlayout2 = uigridlayout(self.leftlayout, [2,2]);
 
             self.leftlayout3 = uigridlayout(self.leftlayout, [3,6]);
 
@@ -60,6 +119,68 @@ classdef MeshTool < handle
             self.rightlayout3 = uigridlayout(self.rightlayout, [3,1]);
 
             % Place widgets into layouts
+            %
+            % leftlayout1
+
+            self.fem_mesh_btn = uibutton(self.leftlayout1, 'Text', 'Create FEM Mesh');
+
+            self.pp_fem_mesh_btn = uibutton(self.leftlayout1, 'Text', 'Postprocess FEM Mesh');
+
+            self.resamble_field_btn = uibutton(self.leftlayout1, 'Text', 'Resample field');
+
+            self.resample_surf_btn = uibutton(self.leftlayout1, 'Text', 'Resample surfaces');
+
+            self.source_interpolation_btn = uibutton(self.leftlayout1, 'Text', 'Source interpolation');
+
+            self.apply_transform_btn = uibutton(self.leftlayout1, 'Text', 'Apply transform');
+
+            % leftlayout 2
+
+            self.lf_source_int_cb = uicheckbox(self.leftlayout2, 'Text', 'LF source interpolation');
+
+            self.downsample_surf_cb = uicheckbox(self.leftlayout2, 'Text', 'Refinement');
+
+            self.refinement_cb = uicheckbox(self.leftlayout2, 'Text', 'Downsample surface');
+
+            self.mesh_smoothing_cb = uicheckbox(self.leftlayout2, 'Text', 'Mesh smoothing');
+
+            % leftlayout 3
+
+            self.source_count_label = uilabel(self.leftlayout3);
+
+            self.source_count_ef = uieditfield(self.leftlayout3);
+
+            self.surf_tri_max_label = uilabel(self.leftlayout3);
+
+            self.surf_tri_max_ef = uieditfield(self.leftlayout3);
+
+            self.inflating_iter_label = uilabel(self.leftlayout3);
+
+            self.inflating_iter_ef = uieditfield(self.leftlayout3);
+
+            self.mesh_resolution_label = uilabel(self.leftlayout3);
+
+            self.mesh_resolution_ef = uieditfield(self.leftlayout3);
+
+            self.smoothing_str_label = uilabel(self.leftlayout3);
+
+            self.smoothing_str_ef = uieditfield(self.leftlayout3);
+
+            self.inflating_str_label = uilabel(self.leftlayout3);
+
+            self.inflating_str_ef = uieditfield(self.leftlayout3);
+
+            self.meshing_acc_label = uilabel(self.leftlayout3);
+
+            self.meshing_acc_ef = uieditfield(self.leftlayout3);
+
+            self.solver_tol_label = uilabel(self.leftlayout3);
+
+            self.solver_tol_ef = uieditfield(self.leftlayout3);
+
+            self.dir_label = uilabel(self.leftlayout3);
+
+            self.dir_dd = uidropdown(self.leftlayout3);
 
         end
 
