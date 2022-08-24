d81e9aef914a9198c563fd786c3f3f56fc95f248
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-08-24 14:45:24 +0300

    MeshTool: added layouts

--- +app/@MeshTool/MeshTool.m
+++ +app/@MeshTool/MeshTool.m
@@ -13,6 +13,22 @@ classdef MeshTool < handle
 
         layout matlab.ui.container.GridLayout
 
+        leftlayout matlab.ui.container.GridLayout
+
+        leftlayout1 matlab.ui.container.GridLayout
+
+        leftlayout2 matlab.ui.container.GridLayout
+
+        leftlayout3 matlab.ui.container.GridLayout
+
+        rightlayout matlab.ui.container.GridLayout
+
+        rightlayout1 matlab.ui.container.GridLayout
+
+        rightlayout2 matlab.ui.container.GridLayout
+
+        rightlayout3 matlab.ui.container.GridLayout
+
     end % properties
 
     methods
@@ -23,7 +39,27 @@ classdef MeshTool < handle
 
             self.tab = uitab(tabs, 'Title', 'Mesh Tool');
 
-            self.layout = uigridlayout(self.tab, [1,1], 'Scrollable', 'on');
+            % Create layouts
+
+            self.layout = uigridlayout(self.tab, [1,2], 'Scrollable', 'on');
+
+            self.leftlayout = uigridlayout(self.layout, [3,1]);
+
+            self.leftlayout1 = uigridlayout(self.leftlayout, [3,2]);
+
+            self.leftlayout2 = uigridlayout(self.leftlayout, [2,4]);
+
+            self.leftlayout3 = uigridlayout(self.leftlayout, [3,6]);
+
+            self.rightlayout = uigridlayout(self.layout, [3,1]);
+
+            self.rightlayout1 = uigridlayout(self.rightlayout, [3,1]);
+
+            self.rightlayout2 = uigridlayout(self.rightlayout, [3,1]);
+
+            self.rightlayout3 = uigridlayout(self.rightlayout, [3,1]);
+
+            % Place widgets into layouts
 
         end
 
