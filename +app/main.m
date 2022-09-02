52c127b953d76cf162966084dbc471619c4f4811
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-09-02 09:52:34 +0300

    Added app.main for starting Zeffiro Interface in a controlled and automated manner

---
+++ +app/main.m
@@ -0,0 +1,73 @@
+function zef_or_zi = main(project_file_path, use_gui)
+
+    % app.main
+    %
+    % The main function of Zeffiro Interface.
+    %
+    % Input:
+    %
+    % - project_file
+    %
+    %   A Zeffiro Interface project file, which is loaded into the back-end
+    %   before a GUI is loaded, if it is loaded. If this is empty, "none" or
+    %   "no project", the back-end is loaded with null fields, but still
+    %   usable.
+    %
+    % - use_gui
+    %
+    %   A boolean, which determines hether to start the program with or
+    %   without a GUI. If no GUI is used, an object of type Zef is returned,
+    %   which should be captured on the command line if one wishes to use the
+    %   back-end of the program and its methods as-is.
+    %
+    %   default: false
+    %
+    % Output:
+    %
+    % - zef_or_zi
+    %
+    %   Either an instance of the back-end object Zef, or a handle to a main
+    %   GUI object ZeffiroInterface, depending on whether use_gui was set to
+    %   true.
+
+    arguments
+
+        project_file_path (1,1) string { mustBeText }
+
+        use_gui (1,1) logical = false
+
+    end
+
+    % Remove possible typos in project path.
+
+    fixed_project_file_path = strtrim(project_file_path);
+
+    fixed_project_file_path = regexprep(fixed_project_file_path,' +', ' ');
+
+    % Load an instance of Zef.
+
+    if strcmp(fixed_project_file_path, "") ...
+    || strcmp(fixed_project_file_path, "none") ...
+    || strcmp(fixed_project_file_path, "no project") ...
+
+        zef = app.Zef(struct);
+
+    else
+
+        zef = app.Zef.load_from_file(project_file_path);
+
+    end
+
+    % Then start gui, if use_gui was set.
+
+    if use_gui
+
+        zef_or_zi = app.ZeffiroInterface(zef);
+
+    else
+
+        zef_or_zi = zef;
+
+    end
+
+end
