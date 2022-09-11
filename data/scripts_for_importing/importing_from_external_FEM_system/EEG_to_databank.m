a68d86cb8105553ffb3dc344a25d937d17e14ef7
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2022-09-12 01:58:04 +0300

    Delete data/scripts_for_importing directory

--- data/scripts_for_importing/importing_from_external_FEM_system/EEG_to_databank.m
+++
@@ -1,3 +0,0 @@
-zef_dataBank_add_data_item('custom',[],'EEG');
-zef_dataBank_add_data_item('data','EEG','EEG measurements');
-zef_dataBank_add_data_item('leadfield','EEG','EEG leadfield');
--- data/scripts_for_importing/importing_from_external_FEM_system/MEG_to_databank.m
+++
@@ -1,3 +0,0 @@
-zef_dataBank_add_data_item('custom',[],'MEG');
-zef_dataBank_add_data_item('data','MEG','MEG measurements');
-zef_dataBank_add_data_item('leadfield','MEG','MEG leadfield');
