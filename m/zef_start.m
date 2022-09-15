4091899432eb2299c6d249e015c6ea05e7261e7f
Sampsa Pursiainen (TAU) <pursiain@wks-95038-mac.ad.tuni.fi>
2022-09-15 13:49:08 +0300

    Zeffiro developer: A regular push adding the changes made in the current local repository to the remote origin. Contents of the folders ./data/ and ./profile/ are ignored. The update necessitates creating a personal access token.

--- m/zef_start.m
+++ m/zef_start.m
@@ -53,7 +53,7 @@ if zef.use_log
     zef.current_log_file = [zef.program_path filesep 'data' filesep 'log' filesep zef.zeffiro_log_file_name '_' num2str(length(dir([zef.program_path filesep 'data' filesep 'log']))-1) '.log'];
 fid = fopen(zef.current_log_file,'a'); 
 fprintf(fid,'%s',['**************************************************************************' newline]);
-fprintf(fid,'%s',['ZEFFIRO Interface ' num2str(zef.current_version) ' Log file: ' datestr(now) newline]);
+fprintf(fid,'%s',['ZEFFIRO Interface ' num2str(zef.current_version) ', Date: ' datestr(now) newline]);
 fprintf(fid,'%s',['**************************************************************************' newline] );
 fprintf(fid,'%s',['Path: ' zef.program_path newline]);
 fprintf(fid,'%s',['**************************************************************************' newline]);
