1b7df8ed93658dea5ed9e81b86f1512e3cff0a5b
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-09-17 16:08:19 +0300

    Added method Sensor.from_arrays and made Sensor < handle…
    … to make modifying sensor arrays less cumbersome. Also added some
    documentation.

--- +zef_as_class/@Sensor/Sensor.m
+++ +zef_as_class/@Sensor/Sensor.m
@@ -1,4 +1,4 @@
-classdef Sensor
+classdef Sensor < handle
 
 %
 % Sensor
@@ -43,7 +43,7 @@ classdef Sensor
 
         outer_radius (1,1) double { mustBeReal , mustBeNonnegative} = 0;
 
-        impedance (1,1) double { mustBeReal , mustBeNonnegative} = Inf;
+        impedance (1,1) double { mustBeReal , mustBePositive} = Inf;
 
     end % properties
 
@@ -55,10 +55,28 @@ classdef Sensor
         %
         % The constructor of this class.
         %
+        % Inputs:
+        %
+        % - position
+        %
+        % - direction
+        %
+        % - inner_radius
+        %
+        % - outer_radius
+        %
+        % - impedance
+        %
+        % Output
+        %
+        % - self
+        %
+        %   A single sensor.
+        %
 
             if inner_radius > outer_radius
 
-                error("Tried to construct a Sensor with an inner radius greater than its outer radius. Aborting.")
+                error("Tried to construct a Sensor with an inner radius greater than its outer radius. Aborting...");
 
             end
 
@@ -190,7 +208,165 @@ classdef Sensor
 
         function self = default()
 
-            self = zef_as_class.Sensor([0, 0 ,0], [1, 0, 0], 0, 0, Inf);
+        % default
+        %
+        % Provides a default instance of a sensor, positioned at origin,
+        % pointing upwards and in PEM mode.
+
+            self = zef_as_class.Sensor([0, 0 ,0], [0, 0, 1], 0, 0, Inf);
+
+        end % function
+
+        function sensors = from_arrays(positions, directions, inner_radii, outer_radii, impedances)
+
+        % from_array
+        %
+        % Builds a set of sensors from a given array.
+        %
+        % Input:
+        %
+        % - positions
+        %
+        %   The 3D positions of the arrays.
+        %
+        % - directions
+        %
+        %   The 3D directions of the electrodes. If given as a single 3D
+        %   vector, all electrodes are given the same direction.
+        %
+        % - inner radii (optional, default = [])
+        %
+        %   The inner radii of the electrodes. If given as a scalar, all
+        %   electrodes are given the same inner radius.
+        %
+        % - outer_radii (optional, default = [])
+        %
+        %   The outer radii of the electrodes. If given as a scalar, all
+        %   electrodes are given the same inner radius.
+        %
+        % - impedances (optional, default = [])
+        %
+        %   The impedances of the electrodes. If given as a scalar, all
+        %   electrodes are given the same inner radius.
+        %
+
+            arguments
+
+                positions (:,3) double { mustBeReal }
+
+                directions (:,3) double { mustBeReal }
+
+                inner_radii (:,1) double { mustBeReal, mustBeNonnegative } = 0;
+
+                outer_radii (:,1) double { mustBeReal, mustBeNonnegative } = 0;
+
+                impedances (:,1) double { mustBeReal, mustBePositive } = Inf;
+
+            end
+
+            % Determine sensor array size.
+
+            n_of_rows = size(positions, 1);
+
+            n_of_cols = size(positions, 2);
+
+            % Determine the mode of the electrodes (PEM vs. CEM). If the
+            % electrodes do not have a given size or impedances, they must be
+            % in PEM mode.
+
+            if isempty(outer_radii) || isempty(impedances)
+
+                use_pem = true;
+
+            else
+
+                use_pem = false;
+
+            end
+
+            % Preallocate a sensor array.
+            %
+            % TODO: maybe the impedances and other scalars could be set here
+            % via "vectorization" as well, based on whether they are given as
+            % a single scalar or not, but that might complicate the function
+            % logic and require more nested ifs.
+
+            sensors = repmat(zef_as_class.Sensor.default, n_of_rows, 1);
+
+            % Then build electrodes from the given arrays.
+
+            for row = 1 : n_of_rows
+
+                sensors(row).position = positions(row, :);
+
+                if size(directions) == [1 3]
+
+                    sensors(row).direction = directions;
+
+                elseif size(directions) == size(positions)
+
+                    sensors(row).direction = directions(row, :);
+
+                else
+
+                    % Do nothing and use the default direction set during
+                    % pre-allocation.
+
+                end
+
+                if ~ use_pem % we are using CEM
+
+                    if isscalar(inner_radii)
+
+                        inner_radius = inner_radii;
+
+                    elseif numel(inner_radii) == n_of_rows
+
+                        inner_radius = inner_radii(row);
+
+                    else
+
+                        error("The number of given inner radii is not scalar or is different from the given number of electrode positions.")
+
+                    end
+
+                    sensors(row).set_inner_radius_to(inner_radius);
+
+                    if isscalar(outer_radii)
+
+                        outer_radius = outer_radii;
+
+                    elseif numel(outer_radii) == n_of_rows
+
+                        outer_radius = outer_radii(row);
+
+                    else
+
+                        error("The number of given outer radii is not scalar or is different from the given number of electrode positions.")
+
+                    end
+
+                    sensors(row).set_outer_radius_to(outer_radius);
+
+                    if isscalar(impedances)
+
+                        impedance = impedances;
+
+                    elseif numel(impedances) == n_of_rows
+
+                        impedance = impedances(row);
+
+                    else
+
+                        error("The number of given impedances is not scalar or is different from the given number of electrode positions.")
+
+                    end
+
+                    sensors(row).set_impedance_to(impedance);
+
+                end
+
+            end
 
         end
 
