059a58d6759f899d6b39ea3c4d9cde5554b74bdc
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-09-21 13:42:34 +0300

    Zef.refine_surface: use Zef.surface_triangles to construct said triangles

--- +zef_as_class/@Zef/refine_surface.m
+++ +zef_as_class/@Zef/refine_surface.m
@@ -140,35 +140,7 @@ function self = perform_refinement(self, compartment_ind)
 
     J_c = [];
 
-    ind_m = [
-        2 4 3 ;
-        1 3 4 ;
-        1 4 2 ;
-        1 2 3
-    ];
-
-    tetra_sort = [
-        tetra(:,[2 4 3]) ones(size(tetra,1),1) [1:size(tetra,1)]';
-        tetra(:,[1 3 4]) 2*ones(size(tetra,1),1) [1:size(tetra,1)]';
-        tetra(:,[1 4 2]) 3*ones(size(tetra,1),1) [1:size(tetra,1)]';
-        tetra(:,[1 2 3]) 4*ones(size(tetra,1),1) [1:size(tetra,1)]';
-    ];
-
-    tetra_sort(:,1:3) = sort(tetra_sort(:,1:3),2);
-    tetra_sort = sortrows(tetra_sort,[1 2 3]);
-    tetra_ind = zeros(size(tetra_sort,1),1);
-
-    I = find(sum(abs(tetra_sort(2:end,1:3)-tetra_sort(1:end-1,1:3)),2)==0);
-
-    tetra_ind(I) = 1;
-
-    tetra_ind(I+1) = 1;
-
-    I = find(tetra_ind == 0);
-
-    tetra_ind = sub2ind(size(tetra),repmat(tetra_sort(I,5),1,3),ind_m(tetra_sort(I,4),:));
-
-    surface_triangles = [ tetra(tetra_ind)];
+    surface_triangles = zef_as_class.Zef.surface_triangles(tetra); % [ tetra(tetra_ind)];
 
     J_c = [J_c ;  unique(surface_triangles)];
 
