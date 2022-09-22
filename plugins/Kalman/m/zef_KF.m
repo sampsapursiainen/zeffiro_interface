9c93e0ce5833460de25037b1c3994d4e92ec12d4
Paavo Ronni <paavo.ronni@gmail.com>
2022-09-22 12:29:07 +0300

    Remove multiresolution decomposition

--- plugins/Kalman/m/zef_KF.m
+++ plugins/Kalman/m/zef_KF.m
@@ -1,7 +1,6 @@
 function [z, reconstruction_information] = zef_KF(q_value)
 % Optimal q_value as parameter
 %% Initial parameters
-mne_prior = evalin('base','zef.mne_prior');
 snr_val = evalin('base','zef.inv_snr');
 pm_val = evalin('base','zef.inv_prior_over_measurement_db');
 amplitude_db = evalin('base','zef.inv_amplitude_db');
@@ -27,22 +26,8 @@ reconstruction_information.high_pass = evalin('base','zef.inv_low_cut_frequency'
 reconstruction_information.number_of_frames = evalin('base','zef.number_of_frames');
 reconstruction_information.source_direction_mode = evalin('base','zef.source_direction_mode');
 reconstruction_information.source_directions = evalin('base','zef.source_directions');
-% reconstruction_information.ias_hyperprior = evalin('base','zef.ias_hyperprior');
 reconstruction_information.snr_val = evalin('base','zef.inv_snr');
 reconstruction_information.pm_val = evalin('base','zef.inv_prior_over_measurement_db');
-
-%%
-multires_dec =  evalin('base','zef.kf_multires_dec');
-multires_ind =  evalin('base','zef.kf_multires_ind');
-multires_count = evalin('base','zef.kf_multires_count'); 
-n_multires = evalin('base','zef.inv_multires_n_levels');
-sparsity_factor = evalin('base','zef.inv_multires_sparsity');
-n_decompositions = evalin('base','zef.inv_multires_n_decompositions');
-
-
-weight_vec_aux = (sparsity_factor.^[0:n_multires-1]');
-
-norms = [];
 
 %%
 [L,n_interp, procFile] = zef_processLeadfields(source_direction_mode);
@@ -53,129 +38,54 @@ timeSteps = arrayfun(@(x) zef_getTimeStep(f_data, x, true), 1:number_of_frames, 
 
 z_inverse_results = cell(0);
 %% CALCULATION STARTS HERE
-% zef_waitbar for iterations 
-h = zef_waitbar(0,('Kalman iterations.'));
-
-%z_vec_aux = zeros(size(L_aux,2),1);
-iter_ind = 0;
-source_count_aux = 0;
-
-for n_rep = 1:n_decompositions
-    zef_waitbar([n_rep/n_decompositions, 0],h,['Kalman decompositions ' int2str(n_rep) ' of ' int2str(n_decompositions) '.']);
-    iter_ind = iter_ind + 1;
-    n_mr_dec = length(multires_dec{n_rep}{1});
+% m_0 = prior mean
+m = zeros(size(L,2), 1);
     
-    if source_direction_mode == 1 || source_direction_mode == 2
-    mr_dec = [multires_dec{n_rep}{1}; multires_dec{n_rep}{1}+n_interp ; multires_dec{n_rep}{1} + 2*n_interp];
-    mr_dec = mr_dec(:);
-    mr_ind = [multires_ind{n_rep}{1} ; multires_ind{n_rep}{1} + n_mr_dec ; multires_ind{n_rep}{1} + 2*n_mr_dec];
-    mr_ind = mr_ind(:);
-    end
+[theta0] = zef_find_gaussian_prior(snr_val-pm_val,L,size(L,2),evalin('base','zef.normalize_data'),0);
+    
+% Transition matrix is Identity matrix
+P = eye(size(L,2)) * theta0;
 
-    if source_direction_mode == 3 
-    mr_dec = multires_dec{n_rep}{1}; 
-    mr_dec = mr_dec(:);
-    mr_ind = multires_ind{n_rep}{1}; 
-    mr_ind = mr_ind(:);
-    end
-    
-    L_aux = L(:,mr_dec);
+A = eye(size(L,2));
 
-    % m_0 = prior mean
-    m = zeros(size(L_aux,2), 1);
-    
-    [theta0] = zef_find_gaussian_prior(snr_val-pm_val,L_aux,size(L_aux,2),evalin('base','zef.normalize_data'),0);
-    
-    % Transition matrix is Identity matrix
-    P = eye(size(L_aux,2)) * theta0;
+% If q_value given in the function call
+if nargin > 0
+    Q = q_value*eye(size(L,2));
+else
+    evalin('base','zef_init_gaussian_prior_options');
+    evolution_prior_db = evalin('base', 'zef.inv_evolution_prior');
+    q_value = find_evolution_prior(L, theta0, time_step, evolution_prior_db);
+    Q = q_value*eye(size(L,2));
+end
+reconstruction_information.Q = q_value;
 
-    A = eye(size(L_aux,2));
-    % If q_value given in the function call
-    if nargin > 0
-        Q = q_value*eye(size(L_aux,2));
-    else
-        evalin('base','zef_init_gaussian_prior_options');
-        evolution_prior_db = evalin('base', 'zef.inv_evolution_prior');
-        q_value = find_evolution_prior(L_aux, theta0, time_step, evolution_prior_db);
-        q_value = 100;
-        Q = q_value*eye(size(L_aux,2));
-    end
-    reconstruction_information.Q = q_value;
-    % std_lhood
-    R = std_lhood^2 * eye(size(L_aux,1));
-    
-    useGpu = false;
-    if useGpu
-        R = gpuArray(R);
-        A = gpuArray(A);
-        Q = gpuArray(Q);
-        P = gpuArray(P);
-        L_aux = gpuArray(L_aux);
-        m = gpuArray(m);
-    end
+% std_lhood
+R = std_lhood^2 * eye(size(L,1));
 
 Q_Store = cell(0);
-%% sLORETA OLD VERSION
-s_loreta = str2double(evalin('base', 'zef.KF.sLORETA.Value'));
-if s_loreta
-    [P, L_aux, Q] = zef_kf_sLORETA_OLD(L_aux, Q, std_lhood, theta0);
-end
+
 %% KALMAN FILTER
 filter_type = evalin('base', 'zef.KF.filter_type.Value');
+
 if filter_type == '1'
-    [P_store, z_inverse] = kalman_filter(m,P,A,Q,L_aux,R,timeSteps, number_of_frames);
+    [P_store, z_inverse] = kalman_filter(m,P,A,Q,L,R,timeSteps, number_of_frames);
 elseif filter_type == '2'
     n_ensembles = str2double(evalin('base', 'zef.KF.number_of_ensembles.Value'));
-    z_inverse = EnKF(m,A,P,Q,L_aux,R,timeSteps,number_of_frames, n_ensembles);
+    z_inverse = EnKF(m,A,P,Q,L,R,timeSteps,number_of_frames, n_ensembles);
 elseif filter_type == '3'
-    [P_store, z_inverse] = kalman_filter_sLORETA(m,P,A,Q,L_aux,R,timeSteps, number_of_frames);
+    [P_store, z_inverse] = kalman_filter_sLORETA(m,P,A,Q,L,R,timeSteps, number_of_frames);
 end
-
-
-
-
 
 %% RTS SMOOTHING
 smoothing = evalin('base','zef.kf_smoothing');
 if (smoothing == 2)
-Q = gather(Q);
-A = gather(A);
-[P_s_store, m_s_store, G_store] = RTS_smoother(P_store, z_inverse, A, Q, number_of_frames);
+[~, m_s_store, ~] = RTS_smoother(P_store, z_inverse, A, Q, number_of_frames);
 z_inverse = m_s_store;
 end
-%% Q ESTIMATION
-if false
-[sigma, phi, B, C, D] =Q_quantities(P_s_store,m_s_store,G_store,timeSteps);
-Q_est = sigma - C * A' - A * C' + A * phi * A';
-Q_est = diag(Q_est);
-%q_estimation = 3;
-norms = [norms, norm(Q-Q_est, 'fro')];
-Q_Store{q_iter} = Q;
-Q = Q_est;
-end
-
-for i= 1:number_of_frames
-    z_inverse_results{i}{n_rep} = z_inverse{i}(mr_ind);
-end
-
-
-%% COMPOSITIONS
-
-% average
-% for i = 1:size(z_inverse_results,2)
-%     z_inverse_results{i} = mean([z_inverse_results{i}{:}],2);
-% end
-
-%last
-for i = 1:size(z_inverse_results,2)
-    z_inverse_results{i} = z_inverse_results{i}{end};
-end
-
 
 %% POSTPROCESSING
-[z] = zef_postProcessInverse(z_inverse_results, procFile);
+[z] = zef_postProcessInverse(z_inverse, procFile);
 %normalize the reconstruction so that the highest value is equal to 1
 [z] = zef_normalizeInverseReconstruction(z);
 %% CALCULATION ENDS HERE
-close(h);
 end
--- plugins/Kalman/m/zef_kf_start.m
+++ plugins/Kalman/m/zef_kf_start.m
@@ -5,7 +5,6 @@
 zef.KF = zef_kf_app;
 
 %_ Initial values _
-zef.KF.mne_prior.Value = '1';
 zef.KF.inv_snr.Value = '30';
 zef.KF.inv_sampling_frequency.Value = '1025';
 zef.KF.inv_low_cut_frequency.Value = '7';
@@ -14,9 +13,6 @@ zef.KF.inv_time_1.Value = '0';
 zef.KF.inv_time_2.Value = '0';
 zef.KF.number_of_frames.Value = '0';
 zef.KF.inv_time_3.Value = '0';
-zef.KF.inv_multires_n_levels.Value = '1';
-zef.KF.inv_multires_sparsity.Value = '1';
-zef.KF.inv_multires_n_decompositions.Value = '1';
 zef.KF.filter_type.Value = '1';
 zef.KF.number_of_ensembles.Value = '100';
 
@@ -34,11 +30,6 @@ end
 clear zef_props zef_i
 
 %_ Functions _
-zef.KF.inv_multires_n_levels.ValueChangedFcn = 'zef.inv_multires_n_levels = str2num(zef.KF.inv_multires_n_levels.Value);';
-zef.KF.inv_multires_n_decompositions.ValueChangedFcn = 'zef.inv_multires_n_decompositions = str2num(zef.KF.inv_multires_n_decompositions.Value);';
-zef.KF.inv_multires_sparsity.ValueChangedFcn = 'zef.inv_multires_sparsity = str2num(zef.KF.inv_multires_sparsity.Value);';
-zef.KF.CreatemultiresolutiondecompositionButton.ButtonPushedFcn = '[zef.kf_multires_dec, zef.kf_multires_ind, zef.kf_multires_count] = zef_make_multires_dec();';
-zef.KF.mne_prior.ValueChangedFcn = 'zef.mne_prior = str2num(zef.KF.mne_prior.Value);';
 zef.KF.inv_snr.ValueChangedFcn = 'zef.inv_snr = str2num(zef.KF.inv_snr.Value);';
 zef.KF.inv_sampling_frequency.ValueChangedFcn = 'zef.inv_sampling_frequency = str2num(zef.KF.inv_sampling_frequency.Value);';
 zef.KF.inv_low_cut_frequency.ValueChangedFcn = 'zef.inv_low_cut_frequency = str2num(zef.KF.inv_low_cut_frequency.Value);';
@@ -47,7 +38,6 @@ zef.KF.inv_time_1.ValueChangedFcn = 'zef.inv_time_1 = str2num(zef.KF.inv_time_1.
 zef.KF.inv_time_2.ValueChangedFcn = 'zef.inv_time_2 = str2num(zef.KF.inv_time_2.Value);';
 zef.KF.number_of_frames.ValueChangedFcn = 'zef.number_of_frames = str2num(zef.KF.number_of_frames.Value);';
 zef.KF.inv_time_3.ValueChangedFcn = 'zef.inv_time_3 = str2num(zef.KF.inv_time_3.Value);';
-zef.KF.normalize_data.ValueChangedFcn = 'zef.normalize_data = str2num(zef.KF.normalize_data.Value);';
 zef.KF.kf_smoothing.ValueChangedFcn = 'zef.kf_smoothing = str2num(zef.KF.kf_smoothing.Value);';
 zef.KF.StartButton.ButtonPushedFcn = '[zef.reconstruction, zef.reconstruction_information] = zef_KF;';
 zef.KF.ApplyButton.ButtonPushedFcn = 'zef_props = properties(zef.KF); for zef_i = 1:length(zef_props); if isprop(zef.KF.(zef_props{zef_i}),''Value''); zef.(zef_props{zef_i}) = str2num(zef.KF.(zef_props{zef_i}).Value); end; end; clear zef_props zef_i;';
