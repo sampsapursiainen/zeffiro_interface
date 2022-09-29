98a3ff071ffb8c9708aba0bbc3e00f11bc8dd9ac
Sampsa Pursiainen (TAU) <pursiain@wks-95038-mac.ad.tuni.fi>
2022-09-29 16:00:14 +0300

    Sampsa: A regular push adding the changes made in the current local repository to the remote origin. Contents of the folders ./data/ and ./profile/ are ignored. The update necessitates creating a personal access token.

--- scripts/zef_rec_max_diff.m
+++ scripts/zef_rec_max_diff.m
@@ -1,6 +1,7 @@
-function rec_vec = zef_rec_max_diff(zef,method_name)
+function [dist_vec,angle_vec] = zef_rec_max_diff(zef,method_name)
 
-rec_vec = zeros(3*size(zef.source_positions,1),1);
+dist_vec = zeros(3*size(zef.source_positions,1),1);
+angle_vec = zeros(3*size(zef.source_positions,1),1);
 
 h_waitbar = zef_waitbar(0,'Creating synthetic measurements');
 
@@ -8,6 +9,7 @@ n_sources = size(zef.source_positions,1);
 zef.measurements = zeros(size(zef.L,1),3*n_sources);
 
 zef.inv_synth_source = [ 0 0 0 0 0 0 10   0    3    5];
+dir_mat = eye(3);
 
 for i = 1 : n_sources
  
@@ -17,14 +19,8 @@ for i = 1 : n_sources
     
     for j = 1 : 3
 
-   switch j
-       case 1
- zef.inv_synth_source(1:6) = [zef.source_positions(i,:)     1     0     0  ];
-       case 2
-            zef.inv_synth_source(1:6) = [zef.source_positions(i,:)     0     1     0  ];
-       case 3
-            zef.inv_synth_source(1:6) = [zef.source_positions(i,:)     0     0     1 ];
-   end
+
+ zef.inv_synth_source(1:6) = [zef.source_positions(i,:)     dir_mat(j,:) ];
    
    zef.measurements(:,3*(i-1)+j) = zef_find_source_legacy(zef);
    
@@ -42,8 +38,15 @@ close(h_waitbar);
           
    z_norm = sqrt(sum(reshape(z{3*(i-1) + j},3,size(zef.source_positions,1)).^2));
    [~, I] = max(z_norm);
-
-   rec_vec(3*(i-1) + j) = (1/sqrt(3))*sqrt(sum((zef.source_positions(i,:) - zef.source_positions(I,:)).^2,2));
+   
+   dir_vec_rec = z{3*(i-1) + j}(3*(I-1)+1:3*(I-1)+3);
+   dir_vec_rec = dir_vec_rec/norm(dir_vec_rec,2);
+   dir_vec_source = dir_mat(:,j);
+    dir_vec_source = dir_vec_source/norm(dir_vec_source,2);
+  
+   dist_vec(3*(i-1) + j) = (1/sqrt(3))*sqrt(sum((zef.source_positions(i,:) - zef.source_positions(I,:)).^2,2));
+   angle_vec(3*(i-1) + j) = acosd(dot(dir_vec_rec,dir_vec_source));
+   
    
       end
   end
