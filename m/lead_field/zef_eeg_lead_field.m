e52fe91a3fa339c35fa25185b95ef2dda4e3ee62
Sampsa Pursiainen (TAU) <pursiain@wks-95038-mac.ad.tuni.fi>
2022-09-30 15:20:04 +0300

    Zeffiro developer: A regular push adding the changes made in the current local repository to the remote origin. Contents of the folders ./data/ and ./profile/ are ignored. The update necessitates creating a personal access token.

--- m/lead_field/zef_eeg_lead_field.m
+++ m/lead_field/zef_eeg_lead_field.m
@@ -5,7 +5,7 @@ zef.lead_field_type = 1;
 zef.imaging_method = 1;
 zef_delete_original_field;
 zef = zef_process_meshes(zef);
-zef.sensors = zef_attach_sensors_volume(zef,zef.sensors);
+zef.sensors_attached_volume = zef_attach_sensors_volume(zef,zef.sensors);
 zef = zef_lead_field_matrix(zef);
 [zef.L,zef.source_positions,zef.source_directions] = zef_lead_field_filter(zef.L,zef.source_positions,zef.source_directions,zef.lead_field_filter_quantile);
 if zef.source_interpolation_on
