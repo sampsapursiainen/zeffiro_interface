95040b11644ff5afc81de40a3b03fa00d71ec972
Sampsa Pursiainen (TAU) <pursiain@wks-95038-mac.ad.tuni.fi>
2022-09-30 20:11:11 +0300

    Zeffiro developer: A regular push adding the changes made in the current local repository to the remote origin. Contents of the folders ./data/ and ./profile/ are ignored. The update necessitates creating a personal access token.

--- scripts/KalmanDemo.m
+++ scripts/KalmanDemo.m
@@ -12,17 +12,16 @@ project_struct.mesh_smoothing_on = 1;
 project_struct.refinement_on = 1;
 project_struct.use_gpu = 1;
 project_struct.forward_simulation_selected = 1;
-project_struct.refinement_surface_compartments = [ -1    22    21    10    11    12    13    14     1];
+project_struct.refinement_surface_compartments = [10 -1 1 18 17];
 project_struct.refinement_surface_on = 1;
-
 
 % Create finite element mesh
 project_struct = zef_create_finite_element_mesh(project_struct);
 
-
-
-%project_struct = zef_eeg_lead_field(project_struct);
+% Create lead field matrix
+project_struct = zef_eeg_lead_field(project_struct);
 
 % Save project
-zef_save(project_struct,'example_project.mat','data/');
+project_struct = zef_save(project_struct,'example_project.mat','data/');
+
 end
