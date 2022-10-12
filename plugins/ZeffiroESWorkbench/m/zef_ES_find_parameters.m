01c1e54e27ee53c8f0fc4700ded80e0cd7445fad
GitFernandoGalaz <fernando.galazprieto@tuni.fi>
2022-10-12 20:20:19 +0300

    Update zef_ES_find_parameters.m

--- plugins/ZeffiroESWorkbench/m/zef_ES_find_parameters.m
+++ plugins/ZeffiroESWorkbench/m/zef_ES_find_parameters.m
@@ -6,17 +6,15 @@ end
 
 alpha       = zef.ES_alpha;
 beta        = zef.ES_beta;
-
-
 step_size   = zef.ES_step_size;
 
-%if evalin('base','zef.ES_search_type') == 2
-    alpha_max   = zef.ES_alpha_max;
-    beta_min    = zef.ES_beta_min;
+alpha_max   = zef.ES_alpha_max;
+beta_min    = zef.ES_beta_min;
        
-    if alpha == 0
-        alpha = 1;
-    end
-    alpha = exp(log(alpha):(log(alpha_max)-log(alpha))/(step_size-1):log(alpha_max))';
-    beta  = exp(log(beta):(log(beta_min)-log(beta))/(step_size-1):log(beta_min))';
+if alpha == 0
+    alpha = 1;
+end
+
+alpha = exp(log(alpha):(log(alpha_max)-log(alpha))/(step_size-1):log(alpha_max))';
+beta  = exp(log(beta):(log(beta_min)-log(beta))/(step_size-1):log(beta_min))';
 end
