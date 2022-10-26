b33ad305a5a09617a19995b97f9b03d99a94f629
Sampsa Pursiainen (TAU) <pursiain@wks-95038-mac.ad.tuni.fi>
2022-10-26 21:14:11 +0300

    Zeffiro developer: A regular push adding the changes made in the current local repository to the remote origin. Contents of the folders ./data/ and ./profile/ are ignored. The update necessitates creating a personal access token.

--- m/lead_field/zef_field_downsampling.m
+++ m/lead_field/zef_field_downsampling.m
@@ -7,7 +7,7 @@ zef.source_directions_original_field = zef.source_directions;
 zef.L_original_field = zef.L;
 zef.source_interpolation_ind_original_field = zef.source_interpolation_ind;
 
-else
+else 
 
 zef.source_positions = zef.source_positions_original_field;
 zef.source_directions = zef.source_directions_original_field;
@@ -30,7 +30,7 @@ zef.rand_vec_aux = randperm(size(zef.source_positions,1));
 zef.rand_vec_aux = zef.rand_vec_aux(1:zef.n_sources);
 zef.source_positions = zef.source_positions(zef.rand_vec_aux,:);
 
-if ismember(zef.source_direction_mode,[1 2])
+if ismember(zef.source_direction_mode,2)
 zef.rand_vec_aux = 3*(zef.rand_vec_aux(:)'-1);
 zef.rand_vec_aux = [zef.rand_vec_aux + 1; zef.rand_vec_aux + 2; zef.rand_vec_aux + 3];
 zef.rand_vec_aux = zef.rand_vec_aux(:);
