96bf0aaded489801a11f5ef72850ad8c3ff3c48d
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2022-11-23 13:00:02 +0200

    Add files via upload

--- plugins/ZeffiroESWorkbench/m/zef_ES_optimizer_properties_show.m
+++ plugins/ZeffiroESWorkbench/m/zef_ES_optimizer_properties_show.m
@@ -13,16 +13,17 @@ for i_aux = 1 : size(vec, 2)-1 % Last one are the ES channels.
     if not(isempty(vec{:, i_aux}))
         
         if i_aux == 13 % alpha
-            zef.h_ES_optimizer_properties_table.Data{i_aux, 2} = round(vec{:, i_aux}(sc),4);
+            zef.h_ES_optimizer_properties_table.Data{i_aux, 2} = round(vec{:, i_aux}{1}(sc),4);
         elseif i_aux == 14 % beta or k-val
-            zef.h_ES_optimizer_properties_table.Data{i_aux, 2} = round(vec{:, i_aux}(sr),4);
+            zef.h_ES_optimizer_properties_table.Data{i_aux, 2} = round(vec{:, i_aux}{1}(sr),4);
         else
-            zef.h_ES_optimizer_properties_table.Data{i_aux, 2} = vec{:, i_aux}(sr,sc);
+
+            zef.h_ES_optimizer_properties_table.Data{i_aux, 2} = vec{:, i_aux}{1}(sr,sc);
         end
 
         if i_aux <= 12
-            zef.h_ES_optimizer_properties_table.Data{i_aux, 3} = zef_lattice_deviation(vec{:, i_aux},'avg',0.5,sr,sc);
-            zef.h_ES_optimizer_properties_table.Data{i_aux, 4} = zef_lattice_deviation(vec{:, i_aux},'max',0.5,sr,sc);
+            zef.h_ES_optimizer_properties_table.Data{i_aux, 3} = zef_lattice_deviation(vec{:, i_aux}{1},'avg',0.5,sr,sc);
+            zef.h_ES_optimizer_properties_table.Data{i_aux, 4} = zef_lattice_deviation(vec{:, i_aux}{1},'max',0.5,sr,sc);
         end
 
     else
@@ -33,7 +34,7 @@ end
 i_aux = i_aux + 1;
 
 zef.h_ES_optimizer_properties_table.Data{i_aux,1} = 'Lattice run time (s)';
-zef.h_ES_optimizer_properties_table.Data{i_aux,2} = sum(vec{:,12},'all');
+zef.h_ES_optimizer_properties_table.Data{i_aux,2} = sum(vec{:,12}{1},'all');
 
 zef.h_ES_optimizer_properties.Name = [zef.h_ES_optimizer_properties.Name ' . ' vec.Properties.Description];
 
