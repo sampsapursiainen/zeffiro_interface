9bf403e97e0a1bee2e275cbfe67bf5f4055726a3
Santtu Söderholm <santtu.soderholm@tuni.fi>
2022-11-29 08:26:46 +0200

    zef_process_inversion: remove many unnecessary if-blocks
    These can be added back later if they are deemed necessary, but for now
    considering different numbers of inputs and outputs and worrying about the
    contents of the input inverter are unnecessary. The structure of the inverter
    class is partly checked by the class itself, and the isAnInverter validator
    method.
    
    Also simplified how the waitbars are updated.
    
    NOTE: it seems that z_vec was invisible to Matlab in the previous update, when
    it was being read into z_inverse. It is weird that sometimes Matlab decides it
    is lexically scoped and sometimes it is not.

--- m/inverse/zef_process_inversion.m
+++ m/inverse/zef_process_inversion.m
@@ -24,32 +24,19 @@ function [zef,MethodClassObj] = zef_process_inversion(zef,MethodClassObj)
 
     end
 
-    % Initialize the waiting bar
+    % Initialize the waitbar with a cleanup object that automatically closes
+    % the waitbar, if there is an interruption with Ctrl + C or when this
+    % function exits.
 
-    if isprop(MethodClassObj,'tag')
+    waitbar_title = "Building reconstructions with " + class(MethodClassObj) + ".";
 
-        if ischar(MethodClassObj.tag)
-
-            waitbar_handle = zef_waitbar(0,[MethodClassObj.tag,' Reconstruction.']);
-
-        else
-
-            waitbar_handle = zef_waitbar(0,MethodClassObj.tag + " Reconstruction.");
-
-        end
-
-    else
-
-        waitbar_handle = zef_waitbar(0,'Reconstruction.');
-
-    end
-
-    % A cleanup object that automatically closes the waitbar, if there is an
-    % interruption with Ctrl + C or when this function exits.
+    waitbar_handle = zef_waitbar(0, waitbar_title);
 
     cleanup_fn = @(h) close(h);
 
     cleanup_obj = onCleanup(@() cleanup_fn(waitbar_handle));
+
+    % Get needed parameters from zef.
 
     source_direction_mode = zef.source_direction_mode;
 
@@ -77,7 +64,7 @@ function [zef,MethodClassObj] = zef_process_inversion(zef,MethodClassObj)
         L = L(:,s_reorder_ind);
     end
 
-    %set up the source space of selected brain compartments:
+    % Set up the source space of selected brain compartments:
 
     source_positions = zef.source_positions(procFile.s_ind_0,:);
 
@@ -85,110 +72,51 @@ function [zef,MethodClassObj] = zef_process_inversion(zef,MethodClassObj)
         L = gpuArray(L);
     end
 
-    if zef.number_of_frames > 1
-        z_inverse = cell(zef.number_of_frames,1);   %can we transpose this cell bc then you can transform it to matrix with cell2mat without much extra effort. Many other softwares uses the matrix format.
-    else
-        zef.number_of_frames = 1;
-    end
+    % The inverse result, which will be post-processed.
+    %
+    % TODO: can we transpose this cell bc then you can transform it to matrix
+    % with cell2mat without much extra effort. Many other softwares uses the
+    % matrix format.
+
+    z_inverse = cell(zef.number_of_frames,1);
 
     f_data = zef_getFilteredData(zef);
 
     tic;
 
-    for f_ind = 1 : zef.number_of_frames
+    for f_ind = 1 : MethodClassObj.number_of_frames
 
         time_val = toc;
 
-        if f_ind > 1;
-            date_str = datestr(datevec(now+(zef.number_of_frames/(f_ind-1) - 1)*time_val/86400));
-        end
-
         f = zef_getTimeStep(f_data, f_ind, zef);
-
-        if f_ind == 1
-            zef_waitbar(0,waitbar_handle,['MNE reconstruction. Time step ' int2str(f_ind) ' of ' int2str(zef.number_of_frames) '.']);
-        end
 
         if zef.use_gpu && zef.gpu_count > 0
             f = gpuArray(f);
         end
 
-        % BE MORE INTELLIGENT WITH THIS:
+        % Update waitbar.
 
-        if f_ind > 1;
-            zef_waitbar(f_ind/zef.number_of_frames,waitbar_handle,['Step ' int2str(f_ind) ' of ' int2str(zef.number_of_frames) '. Ready: ' date_str '.' ]);
+        if f_ind > 1
+            date_str = " Ready: " + datestr(datevec(now+(MethodClassObj.number_of_frames/(f_ind-1) - 1)*time_val/86400));
         else
-            zef_waitbar(f_ind/zef.number_of_frames,waitbar_handle,['MNE reconstruction. Time step ' int2str(f_ind) ' of ' int2str(zef.number_of_frames) '.' ]);
+            date_str = "";
         end
 
-        %SETVI TÄMÄN MUOTOILU:
-        %nargin(inverse)
-        %nargout(inverse)
+        zef_waitbar( ...
+            f_ind/zef.number_of_frames, ...
+            waitbar_handle, ...
+            waitbar_title ...
+                + " Time step " ...
+                +  int2str(f_ind) ...
+                + " of " ...
+                + int2str(zef.number_of_frames) ...
+                + "." ...
+                + date_str ...
+        );
 
-        metaInformation = eval(['?',class(MethodClassObj)]);   %If someone figure out a better way to have "?inverse.ParticularMethod" be my guest
+        % Build this frame's reconstruction.
 
-        N = length(metaInformation.MethodList);
-
-        for n = 1:(N+1)
-
-            if strcmp(metaInformation.MethodList(n).Name,'invert')
-
-                num_inputs = length(metaInformation.MethodList(n).InputNames);      %can/could we use the actual names of inputs to declare correct variables in correct order?
-                output_names = metaInformation.MethodList(n).OutputNames;
-                num_outputs = length(output_names);
-                break;
-
-            elseif n > N
-
-                error('Class object does not have a method named ''invert''.')
-
-            end
-
-        end % for
-
-        if num_outputs == 1
-
-            switch num_inputs
-
-                case 4
-                    z_vec = MethodClassObj.invert(f,L);
-                case 5
-                    z_vec = MethodClassObj.invert(f,L,procFile);
-                case 6
-                    z_vec = MethodClassObj.invert(f,L,procFile,source_direction_mode);
-                case 7
-                    z_vec = MethodClassObj.invert(f,L,procFile,source_direction_mode,source_positions);
-            end
-
-        else % multiple outputs
-
-            outputs = cell(1,num_outputs);
-
-            switch num_inputs
-                case 3
-                    [outputs{:}] = MethodClassObj.invert(f,L);
-                case 4
-                    [outputs{:}] = MethodClassObj.invert(f,L,procFile);
-                case 5
-                    [outputs{:}] = MethodClassObj.invert(f,L,procFile,source_direction_mode);
-                case 6
-                    [outputs{:}] = MethodClassObj.invert(f,L,procFile,source_direction_mode,source_positions);
-            end
-
-            z_vec = outputs{1};     %first one must be the reconstruction itself
-
-            for n = 2:num_outputs
-
-                if strcmp(output_names{n},'self')
-                    MethodClassObj = outputs{n};
-                else
-                    %save on the ZI bc I don't know how else to deal with them
-                    zef.(output_names{n}) = outputs{n};
-                end
-
-            end
-
-        end % if
+        z_vec = MethodClassObj.invert(f,L,procFile,source_direction_mode,source_positions);
 
         z_inverse{f_ind} = z_vec(s_back_ind);
 
