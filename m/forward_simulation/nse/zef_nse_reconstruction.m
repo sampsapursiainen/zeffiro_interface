2753e31fa022ada16c234ee282ed758d7b1e6f96
Sampsa Pursiainen (TAU) <pursiain@wks-95038-mac.ad.tuni.fi>
2023-04-23 03:24:36 +0300

    Zeffiro developer: A regular push adding the changes made in the current local repository to the remote origin. Contents of the folders ./data/ and ./profile/ are ignored. The update necessitates creating a personal access token.

--- m/forward_simulation/nse/zef_nse_reconstruction.m
+++ m/forward_simulation/nse/zef_nse_reconstruction.m
@@ -60,7 +60,20 @@ elseif isequal(type,5)
     
        reconstruction{1} = reconstruction{1}(:)/size(nse_field.bp_vessels,2);
        
-  elseif isequal(type,6)
+       elseif isequal(type,6)
+   
+    reconstruction{1} = zeros(3,size(nse_field.bp_vessels{1}(:),1));
+    
+    for i = 1 : size(nse_field.bp_vessels,2)
+  
+  aux_vec = zef_nse_threshold_distribution(nse_field.bp_vessels{i}(:),nse_field.min_reconstruction_quantile,nse_field.max_reconstruction_quantile);
+   reconstruction{1} = max(reconstruction{1},(1/sqrt(3))*aux_vec(:,[1 1 1])');
+   
+    end
+    
+       reconstruction{1} = reconstruction{1}(:);
+       
+  elseif isequal(type,7)
    
     mean_data = zeros(3,size(nse_field.bp_vessels{1}(:),1));
     
@@ -84,7 +97,7 @@ elseif isequal(type,5)
     
     reconstruction{1} = sqrt(reconstruction{1}(:)/size(nse_field.bp_vessels,2));
    
-   elseif isequal(type,7)
+   elseif isequal(type,8)
    
     reconstruction{1} = zeros(3,size(nse_field.bv_vessels_1{1}(:),1));
     
@@ -99,8 +112,27 @@ elseif isequal(type,5)
     end
     
        reconstruction{1} = reconstruction{1}(:)/size(nse_field.bv_vessels_1,2);
+   
        
-       elseif isequal(type,8)
+          
+   elseif isequal(type,9)
+   
+    reconstruction{1} = zeros(3,size(nse_field.bv_vessels_1{1}(:),1));
+    
+    for i = 1 : size(nse_field.bv_vessels_1,2)
+  
+ aux_vec_1 = zef_nse_threshold_distribution(nse_field.bv_vessels_1{i}(:),nse_field.min_reconstruction_quantile,nse_field.max_reconstruction_quantile);
+ aux_vec_2 = zef_nse_threshold_distribution(nse_field.bv_vessels_2{i}(:),nse_field.min_reconstruction_quantile,nse_field.max_reconstruction_quantile);
+ aux_vec_3 = zef_nse_threshold_distribution(nse_field.bv_vessels_3{i}(:),nse_field.min_reconstruction_quantile,nse_field.max_reconstruction_quantile);
+  
+  reconstruction{1} = max(reconstruction{1},(1/sqrt(3))*[aux_vec_1 aux_vec_2 aux_vec_3]');
+   
+    end
+    
+       reconstruction{1} = reconstruction{1}(:);
+   
+       
+       elseif isequal(type,10)
    
     mean_data = zeros(3,size(nse_field.bv_vessels_1{1}(:),1));
     
@@ -129,7 +161,7 @@ elseif isequal(type,5)
   reconstruction{1} = sqrt(reconstruction{1}(:)/size(nse_field.bv_vessels_1,2));
 
   
-elseif isequal(type,9)
+elseif isequal(type,11)
    
     reconstruction{1} = zeros(3,size(nse_field.mu_vessels{1}(:),1));
     
@@ -141,8 +173,21 @@ elseif isequal(type,9)
     end
     
   reconstruction{1} = reconstruction{1}(:)/size(nse_field.mu_vessels,2);
+  
+  elseif isequal(type,12)
+   
+    reconstruction{1} = zeros(3,size(nse_field.mu_vessels{1}(:),1));
+    
+    for i = 1 : size(nse_field.mu_vessels,2)
+  
+  aux_vec = zef_nse_threshold_distribution(nse_field.mu_vessels{i}(:),nse_field.min_reconstruction_quantile,nse_field.max_reconstruction_quantile);
+   reconstruction{1} = max(reconstruction{1}, (1/sqrt(3))*aux_vec(:,[1 1 1])');
+   
+    end
+    
+  reconstruction{1} = reconstruction{1}(:);
        
-       elseif isequal(type,10)
+       elseif isequal(type,13)
    
     mean_data = zeros(3,size(nse_field.mu_vessels{1}(:),1));
     
@@ -166,7 +211,7 @@ elseif isequal(type,9)
     
        reconstruction{1} = sqrt(reconstruction{1}(:)/size(nse_field.mu_vessels,2));
   
- elseif isequal(type,11)
+ elseif isequal(type,14)
    
     reconstruction{1} = zeros(3,size(nse_field.bf_capillaries{1}(:),1));
     
@@ -179,7 +224,21 @@ elseif isequal(type,9)
     
        reconstruction{1} = reconstruction{1}(:)/size(nse_field.bf_capillaries,2);
        
-       elseif isequal(type,12)
+         
+ elseif isequal(type,15)
+   
+    reconstruction{1} = zeros(3,size(nse_field.bf_capillaries{1}(:),1));
+    
+    for i = 1 : size(nse_field.bf_capillaries,2)
+  
+  aux_vec = zef_nse_threshold_distribution(nse_field.bf_capillaries{i}(:),nse_field.min_reconstruction_quantile,nse_field.max_reconstruction_quantile);
+   reconstruction{1} = max(reconstruction{1},(1/sqrt(3))*aux_vec(:,[1 1 1])');
+   
+    end
+    
+       reconstruction{1} = reconstruction{1}(:);
+       
+       elseif isequal(type,16)
    
     mean_data = zeros(3,size(nse_field.bf_capillaries{1}(:),1));
     
@@ -192,16 +251,16 @@ elseif isequal(type,9)
     
     mean_data = mean_data/size(nse_field.bp_vessels,2); 
     
-    reconstruction{1} = zeros(3,size(nse_field.bp_vessels{1}(:),1));
+    reconstruction{1} = zeros(3,size(nse_field.bf_capillaries{1}(:),1));
     
-    for i = 1 : size(nse_field.bp_vessels,2)
+    for i = 1 : size(nse_field.bf_capillaries,2)
   
-  aux_vec = zef_nse_threshold_distribution(nse_field.bp_vessels{i}(:),nse_field.min_reconstruction_quantile,nse_field.max_reconstruction_quantile);
+  aux_vec = zef_nse_threshold_distribution(nse_field.bf_capillaries{i}(:),nse_field.min_reconstruction_quantile,nse_field.max_reconstruction_quantile);
    reconstruction{1}  =  reconstruction{1} + ((1/sqrt(3))*aux_vec(:,[1 1 1])' - mean_data).^2;
    
     end
     
-       reconstruction{1} = sqrt(reconstruction{1}(:)/size(nse_field.bp_vessels,2));
+       reconstruction{1} = sqrt(reconstruction{1}(:)/size(nse_field.bf_capillaries,2));
   
        
        
