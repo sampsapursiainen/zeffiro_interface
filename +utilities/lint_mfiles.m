a3e65e452192130678e7bd5ddc1593c1f817d659
Santtu Söderholm <santtu.soderholm@tuni.fi>
2023-05-14 14:48:05 +0300

    Add beginnings of function utilities.lint_mfiles
    - This might be usable in a linting GitHub action.

---
+++ +utilities/lint_mfiles.m
@@ -0,0 +1,46 @@
+function lint_mfiles(folder)
+%
+% lint_mfiles
+%
+% Runs checkcode on all .m files in the given folder and its subfolders.
+% Throws and error at the end, if unacceptable linter messages were discovered
+% in the process.
+%
+
+arguments
+
+    folder (1,1) string { mustBeFolder }
+
+end
+
+    mfile_paths = utilities.get_mfile_paths ( folder ) ;
+
+    unacceptable_message_found = false ;
+
+    UNACCEPTABLE_MESSAGES = [ "NODEF" ; "EVLDOT" ] ;
+
+    for fpi = 1 : numel ( mfile_paths )
+
+        fpath = mfile_paths ( fpi ) ;
+
+        linter_message_structs = checkcode ( fpath, "-id" ) ;
+
+        for lmi = 1 : numel ( linter_message_structs )
+
+            message = linter_message_structs ( lmi ) ;
+
+            if ismember ( message.id, UNACCEPTABLE_MESSAGES )
+
+                unacceptable_message_found = true ;
+
+                disp ( "Found unacceptable linter message in " + fpath + ":" ) ;
+
+                disp ( message ) ;
+
+            end % if
+
+        end % for
+
+    end % for
+
+end % function
