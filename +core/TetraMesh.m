a100b5a017d689af61af9f10f394416b0c0fe5f9
Santtu Söderholm <santtu.soderholm@tuni.fi>
2023-09-12 11:18:40 +0300

    Add method core.TetraMesh.vertex_coordinates
    -- Also fixed a typo in doc comment of core.TetraMesh.validate.

--- +core/TetraMesh.m
+++ +core/TetraMesh.m
@@ -54,7 +54,7 @@ classdef TetraMesh
 
         function self = validate ( self )
         %
-        % validate_mesh ( self )
+        % validate ( self )
         %
         % Checks that the mesh is valid.
         %
@@ -124,6 +124,50 @@ classdef TetraMesh
                 error ( "There are node indices that are not referenced by the given tetrahedra." )
 
             end
+
+        end % function
+
+        function [ coords ] = vertex_coordinates ( self, inds )
+        %
+        % vertex_coordinates ( self, inds )
+        %
+        % Returns the vertex coordinates of the tetrahedra indicated by the given indices.
+        %
+        % Inputs:
+        %
+        % - self
+        %
+        %   A tetrahedral mesh.
+        %
+        % - inds
+        %
+        %   The indices of the tetrahedra whose vertex coordinates one wishes to obtain.
+        %
+        % Output:
+        %
+        % - coords
+        %
+        %   A 3-by-(4 numel inds) matrix of node coordinates. In other words, the columns of the
+        %   output value contain the node coordinates of the indicated tetrahedra in groups of 4.
+        %
+
+            arguments
+
+                self (1,1) core.TetraMesh
+
+                inds (:,1) uint64 { mustBePositive }
+
+            end
+
+            n_of_tetra = size ( self.tetra, 2 ) ;
+
+            if any ( n_of_tetra < inds )
+
+                error ( "The given index set contains tetrahedral indices that are too large. The mesh only contains " + n_of_tetra + " tetrahedra."  ) ;
+
+            end
+
+            coords = self.nodes ( :, self.tetra ( :, inds ) ) ;
 
         end % function
 
