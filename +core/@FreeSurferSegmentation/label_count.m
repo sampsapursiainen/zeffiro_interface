40bc863e1609ec9863408717c8848227fcb2d78e
Santtu Söderholm <santtu.soderholm@tuni.fi>
2023-09-13 11:02:30 +0300

    Add methods core.FreeSurferSegmentation.{label,node,triangle}_count

---
+++ +core/@FreeSurferSegmentation/label_count.m
@@ -0,0 +1,10 @@
+function count = label_count ( self )
+%
+% count = node_count ( self )
+%
+% Returns the number of labels currently in the segmentation.
+%
+
+    count = numel ( self.labels ) ;
+
+end % function
---
+++ +core/@FreeSurferSegmentation/node_count.m
@@ -0,0 +1,10 @@
+function count = node_count ( self )
+%
+% count = node_count ( self )
+%
+% Returns the number of nodes currently in the segmentation.
+%
+
+    count = size ( self.nodes, 2 ) ;
+
+end % function
