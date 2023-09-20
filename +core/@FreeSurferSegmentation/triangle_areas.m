e0434af92a021a70b69e44290376a6352666ce2f
Santtu Söderholm <santtu.soderholm@tuni.fi>
2023-09-20 13:52:21 +0300

    Add method core.FreeSurferSegmentation.triangle_areas

---
+++ +core/@FreeSurferSegmentation/triangle_areas.m
@@ -0,0 +1,20 @@
+function areas = triangle_areas ( self, inds )
+%
+% areas = triangle_areas ( self, inds )
+%
+% Computes the areas of the triangles indicated by the given inds in this
+% surface segmentation.
+%
+
+    arguments
+        self (1,1) core.FreeSurferSegmentation
+        inds (:,1) uint64 { mustBePositive } = 1 : self.triangle_count
+    end
+
+    normals = self.surface_normals ( inds ) ;
+
+    norms = sqrt ( sum ( normals .^ 2 , 2 ) ) ;
+
+    areas = normals ./ norms / 2 ;
+
+end % function
