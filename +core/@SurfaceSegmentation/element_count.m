57e9649374828530094bfd77a322486bf19612c4
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2023-10-01 11:24:13 +0300

    Add method core.SurfaceSegmentation.element_count

---
+++ +core/@SurfaceSegmentation/element_count.m
@@ -0,0 +1,10 @@
+function count = triangle_count ( self )
+%
+% count = node_count ( self )
+%
+% Returns the number of triangles currently in the segmentation.
+%
+
+    count = size ( self.triangles, 2 ) ;
+
+end % function
