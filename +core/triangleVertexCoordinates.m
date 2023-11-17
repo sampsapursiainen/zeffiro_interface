776765cd7ed7f014c8fddd73ddd76a201de702ea
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2023-11-17 15:32:36 +0200

    Add function core.triangleVertexCoordinates

---
+++ +core/triangleVertexCoordinates.m
@@ -0,0 +1,17 @@
+function [ c1, c2, c3 ] = triangleVertexCoordinates ( nodes, triangles )
+%
+% [ c1, c2, c3 ] = triangleVertexCoordinates ( nodes, triangles )
+%
+% Retrieves the vertex or corner coordinates of a given set of triangles.
+%
+
+    arguments
+        nodes       (:,3) double { mustBeNonNan }
+        triangles   (3,:) uint32 { mustBePositive }
+    end
+
+    c1 = nodes ( triangles ( 1 : 3 : end ), : ) ;
+    c2 = nodes ( triangles ( 2 : 3 : end ), : ) ;
+    c3 = nodes ( triangles ( 3 : 3 : end ), : ) ;
+
+end % function
