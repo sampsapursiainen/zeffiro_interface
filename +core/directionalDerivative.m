45b038e6dc2aaf3eee035bd947a67ed57dccba25
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2023-11-23 14:15:24 +0200

    core.directionalDerivative: add argument validation

--- +core/directionalDerivative.m
+++ +core/directionalDerivative.m
@@ -5,6 +5,11 @@ function out = directionalDerivative ( gradients, directions )
 % Computes directional derivatives ∇f ⋅ d based on given gradients and direction vectors.
 %
 
+    arguments
+        gradients  (:,3) double { mustBeFinite }
+        directions (:,3) double { mustBeFinite }
+    end
+
     out = sum ( gradients  .* directions, 2 ) ;
 
 end % function
