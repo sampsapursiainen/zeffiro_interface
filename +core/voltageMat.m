64915f3ad353215f5db82a9cb06ceddf90fc6613
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2023-11-24 12:00:17 +0200

    Add function core.voltageMat

---
+++ +core/voltageMat.m
@@ -0,0 +1,39 @@
+function C = voltageMat ( Znum, impedances )
+%
+% C = pemVoltageMat ( Znum, impedances )
+%
+% Builds a sparse electrode voltage matrix C, which contains the ungrounded
+% voltages of a given set of point electrodes (PEM) to finite element nodes. In
+% EEG and tES literature, if we have a discretized finite element system
+%
+%   [ A -B ; -B' C ] * x = b ,
+%
+% then the output corresponds to C.
+%
+% Inputs:
+%
+% - Znum
+%
+%   The numerator in the expression Z / (conj(Z) * Z), if the input impedances
+%   are complex. If the impedances are real, this should equal 1.
+%
+% - impedances
+%
+%   The impedances of the electrodes.
+%
+
+    arguments
+        impedances (:,1) double { mustBeNonNan }
+    end
+
+    eN = numel ( impedances ) ;
+
+    if isreal ( impedances )
+        Zden = impedances ;
+    else
+        Zden = conj(impedances) .* impedances ;
+    end
+
+    C = sparse ( 1 : eN, 1 : eN, Znum ./ Zden, eN, eN ) ;
+
+end % function
