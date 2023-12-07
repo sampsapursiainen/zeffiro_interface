d335eac0bbd97461c410ca6a427f7562f9b1cab5
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2023-12-07 15:11:03 +0200

    Add enumeration core.LeadFieldModality

---
+++ +core/LeadFieldModality.m
@@ -0,0 +1,16 @@
+classdef LeadFieldModality
+%
+% LeadFieldModality
+%
+% An enumeration of the imaging modalities that Zeffiro Interface supports.
+%
+
+    enumeration
+        EEG     % Electroenceplahography
+        MEG     % Magnetoencephalography
+        gMEG    % Magnetoencephalography performed with gradiometers
+        EIT     % Electrical Impedance Tomography
+        tES     % Transcranial Electric Stimulation.
+    end
+
+end % classdef
