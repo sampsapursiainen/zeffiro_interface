846a559042c45ad06d6692b05707fb3ca8d3e1a9
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2023-12-08 10:19:19 +0200

    Add skeletons for the functions core.{meg,gMeg,eit,tes}LeadField

---
+++ +core/eitLeadField.m
@@ -0,0 +1,21 @@
+function L = eitLeadField ( nodes, tetra, electrodes, triA, eA, e2nI, t2nI, A, params )
+%
+% L = eitLeadField ( nodes, tetra, gradiometers, triA, eA, e2nI, t2nI, A, params )
+%
+% Computes an uninterpolated gradiometric electrical impedance tomography lead field L.
+%
+
+    arguments
+        nodes      (:,3) double { mustBeFinite }
+        tetra      (:,4) double { mustBePositive, mustBeInteger }
+        electrodes (:,1) core.ElectrodeSet
+        triA       (:,1) double { mustBePositive }
+        e2nI       (:,1) double { mustBePositive, mustBeInteger }
+        t2nI       (:,1) double { mustBePositive, mustBeInteger }
+        A          (:,:) double { mustBeFinite }
+        params     (1,1) core.LeadFieldParams = core.LeadFieldParams
+    end
+
+    L = [] ;
+
+end % function
