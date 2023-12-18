64e5538af09d69d316d51babd6963ff22ae62f42
Santtu Söderholm <santtu.soderholm@tuni.fi>
2023-12-18 13:22:31 +0200

    Make core.peelSourcePositions depend only on functions in the core module

--- +core/peelSourcePositions.m
+++ +core/peelSourcePositions.m
@@ -3,18 +3,17 @@ function [ ...
     out_deep_node_inds, ...
     out_deep_tetra, ...
     out_deep_tetra_inds ...
-    ] = zef_deep_nodes_and_tetra( ...
+] = peelSourcePositions ( ...
     in_nodes, ...
     in_tetra, ...
     in_volume_inds, ...
     in_acceptable_depth_mm ...
-    )
-
-% Documentation
+)
 %
-% Produces the nodes and tetra which are deep enough within a given
-% volume, and also their indices in the given global tetra and node data
-% structures.
+% [ out_deep_nodes, out_deep_node_inds, out_deep_tetra, out_deep_tetra_inds ] = peelSourcePositions ( in_nodes, in_tetra, in_volume_inds, in_acceptable_depth_mm )
+%
+% Produces the nodes and tetra which are deep enough within a given volume, and
+% also their indices in the given global tetra and node data structures.
 %
 % Input:
 %
@@ -31,22 +30,29 @@ function [ ...
 %
 % Output:
 %
-% - out_tetra_ind: the indices of the tetrahedra that are deep enough
-%   inside the given brain segment.
+% - out_deep_nodes
+%
+%   The coordinates of the nodes that are deep enough inside the given volume.
+%
+% - out_deep_node_inds
+%
+%   The global indices of the nodes that are deep enough in the given volume.
+%
+% - out_deep_tetra
+%
+%   The tetrahedra that are deep enough in the given volume.
+%
+% - out_tetra_ind
+%
+%   The indices of the tetrahedra that are deep enough inside the given brain segment.
+%
 
 arguments
     in_nodes (:,3) double
-    in_tetra (:,4) double {mustBeInteger, mustBePositive}
-    in_volume_inds (:,1) double {mustBeInteger, mustBePositive}
-    in_acceptable_depth_mm (1,1) double {mustBeNonnegative}
+    in_tetra (:,4) double { mustBeInteger, mustBePositive }
+    in_volume_inds (:,1) double { mustBeInteger, mustBePositive }
+    in_acceptable_depth_mm (1,1) double { mustBeNonnegative }
 end
-
-% Set empty return values.
-
-out_deep_node_inds = [];
-out_deep_nodes = [];
-out_deep_tetra_inds = [];
-out_deep_tetra = [];
 
 % Find out the boundary- and non-boundary nodes of the volume. Note that
 % the indices generated here should reference the "global" set of input
@@ -56,7 +62,7 @@ volume_tetra = in_tetra(in_volume_inds, :);
 
 volume_node_inds = unique(volume_tetra(:));
 
-surface_triangles = zef_surface_mesh(volume_tetra);
+[ surface_triangles, ~ ] = core.tetraSurfaceTriangles ( in_tetra, in_volume_inds ) ;
 
 surface_node_inds = unique(surface_triangles);
 
@@ -69,48 +75,70 @@ non_surface_nodes = in_nodes(non_surface_node_inds,:);
 % Find out non-surface nodes deep enough within the volume with
 % rangesearch.
 
-non_surface_node_inds_too_near_to_surface = zef_nearest_points( ...
+non_surface_node_inds_too_near_to_surface = nearestPoints ( ...
     surface_nodes, ...
     non_surface_nodes, ...
-    in_acceptable_depth_mm, ...
-    'range' ...
-    );
+    in_acceptable_depth_mm ...
+);
 
 non_surface_node_inds_too_near_to_surface = non_surface_node_inds(non_surface_node_inds_too_near_to_surface);
 
 % Also take into account surface nodes themselves. D'uh.
 
-surface_node_inds_too_near_to_surface = zef_nearest_points( ...
+surface_node_inds_too_near_to_surface = nearestPoints ( ...
     surface_nodes, ...
     surface_nodes, ...
-    in_acceptable_depth_mm, ...
-    'range' ...
-    );
+    in_acceptable_depth_mm ...
+) ;
 
 surface_node_inds_too_near_to_surface = surface_node_inds(surface_node_inds_too_near_to_surface);
 
-node_inds_too_near_to_surface = union( ...
+node_inds_too_near_to_surface = union ( ...
     non_surface_node_inds_too_near_to_surface, ...
     surface_node_inds_too_near_to_surface ...
-    );
+);
 
-out_deep_node_inds = setdiff( ...
+out_deep_node_inds = setdiff ( ...
     volume_node_inds, ...
     node_inds_too_near_to_surface, ...
     'rows' ...
-    );
+);
 
 out_deep_nodes = in_nodes(out_deep_node_inds, :);
 
 % Find tetra in the volume which only contain acceptable (deep) nodes.
 % Acceptable tetra consist of 4 deep nodes.
 
-vertices_in_deep_nodes = ismember(in_tetra, out_deep_node_inds);
+vertices_in_deep_nodes = ismember ( in_tetra, out_deep_node_inds ) ;
 
-vertex_row_sums = sum(vertices_in_deep_nodes, 2);
+vertex_row_sums = sum ( vertices_in_deep_nodes, 2 ) ;
 
-out_deep_tetra_inds = find(vertex_row_sums == 4);
+out_deep_tetra_inds = find ( vertex_row_sums == 4 ) ;
 
 out_deep_tetra = in_tetra(out_deep_tetra_inds, :);
 
-end % zef_deep_tetra
+end % function
+
+%% Helper functions.
+
+function pI = nearestPoints ( points, neighbour_points, radius )
+%
+% pI = nearestPoints ( points, neighbour_points, radius )
+%
+% Finds the indices of the points in a neighbouring set, that are within a
+% given radius from the points of a "current" set.
+%
+
+    arguments
+        points (:,3) double { mustBeFinite }
+        neighbour_points (:, 3) double { mustBeFinite }
+        radius (1,1) double { mustBeReal, mustBeNonnegative }
+    end
+
+    pI = rangesearch ( neighbour_points, points, radius ) ;
+
+    % Reshape and -size
+
+    pI = unique ( [ pI{:} ]' ) ;
+
+end % function
