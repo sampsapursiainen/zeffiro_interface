d9dfc0e8734b0ed33e2fdbb9a4d934bc9a498232
Santtu Söderholm <santtu.soderholm@tuni.fi>
2024-03-13 11:22:54 +0200

    core.StiffnessMatrix: implement some algebraic operations and add size assertion to constructor

--- +core/StiffnessMatrix.m
+++ +core/StiffnessMatrix.m
@@ -1,3 +1,5 @@
+
+
 classdef StiffnessMatrix
 %
 % StiffnessMatrix
@@ -36,8 +38,129 @@ classdef StiffnessMatrix
 
             end % for
 
+            rsize = size (self.realPart) ;
+
+            isize = size (self.imagPart) ;
+
+            sizeIsValid = all ( rsize == isize ) || isempty (self.realPart) || isempty (self.imagPart) ;
+
+            assert ( sizeIsValid, "StiffnessMatrix: either the real and imaginary parts need to be the same size, or one of them needs to be empty. Now size (realPart) = (" + strjoin (string ( rsize ),",") + ") and size (imagPart) = (" + strjoin ( string ( isize ),",") + ")." )
+
+        end % function
+
+        function out = real ( self )
+        %
+        % out = real ( self )
+        %
+        % Returns the real part of this stiffness matrix.
+        %
+
+            out = self.realPart ;
+
+        end % function
+
+
+        function out = imag ( self )
+        %
+        % out = imag ( self )
+        %
+        % Returns the imaginary part of this stiffness matrix.
+        %
+
+            out = self.imagPart ;
+
+        end % function
+
+        function out = double ( self )
+        %
+        % out = double ( self )
+        %
+        % Returns self as a sparse (complex-valued) matrix of doubles.
+        %
+
+            out = self.realPart + i * self.imagPart ;
+
+        end % function
+
+        function out = plus (left,right)
+        %
+        % out = plus (left,right)
+        %
+        % Implements addition for this class.
+        %
+
+            dl = double ( left ) ;
+
+            dr = double ( right ) ;
+
+            out = core.StiffnessMatrix.fromDouble ( dl + dr ) ;
+
+        end % function
+
+        function out = uplus (self)
+        %
+        % out = uplus (in)
+        %
+        % Implements unary plus for this class.
+        %
+
+            rself = real (self) ;
+
+            iself = imag (self) ;
+
+            out = core.StiffnessMatrix.fromDouble ( + rself + iself ) ;
+
+        end % function
+
+        function out = minus (left,right)
+        %
+        % out = minus (left,right)
+        %
+        % Implements subtraction for this class.
+        %
+
+            dl = double ( left ) ;
+
+            dr = double ( right ) ;
+
+            out = core.StiffnessMatrix.fromDouble ( dl - dr ) ;
+
+        end % function
+
+        function out = uminus (self)
+        %
+        % out = uminus (in)
+        %
+        % Implements unary minus or negation for this class.
+        %
+
+            rself = real (self) ;
+
+            iself = imag (self) ;
+
+            out = core.StiffnessMatrix.fromDouble ( - rself - iself ) ;
+
         end % function
 
     end % methods
+
+    methods (Static)
+
+        function self = fromDouble ( array )
+        %
+        % self = fromDouble ( array )
+        %
+        % Constructs a StiffnessMatrix from a given array of doubles.
+        %
+
+            arguments
+                array (:,:) double { mustBeFinite }
+            end
+
+            self = core.StiffnessMatrix ( realPart=real(array), imagPart=imag(array) )
+
+        end % function
+
+    end % methods (Static)
 
 end % classdef
