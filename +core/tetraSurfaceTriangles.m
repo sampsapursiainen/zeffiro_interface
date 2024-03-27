794ded36f34395195e97a28250533d3487ce09df
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2024-03-27 10:53:47 +0200

    core.tetraSurfaceTriangles: make variable names more descriptive

--- +core/tetraSurfaceTriangles.m
+++ +core/tetraSurfaceTriangles.m
@@ -40,29 +40,29 @@ function [surface_triangles, tetra_ind] = tetraSurfaceTriangles ( tetra, tI )
 
     tetra = tetra(tI,:) ;
 
-    tetra_sort_1 = [
+    faces = [
         tetra(:,[2 4 3]);
         tetra(:,[1 3 4]);
         tetra(:,[1 4 2]);
         tetra(:,[1 2 3]);
     ];
 
-    tetra_sort_2 = uint32([
+    faceLabels = uint32([
         1*ones(size(tetra,1),1) (1:size(tetra,1))';
         2*ones(size(tetra,1),1) (1:size(tetra,1))';
         3*ones(size(tetra,1),1) (1:size(tetra,1))';
         4*ones(size(tetra,1),1) (1:size(tetra,1))';
     ]);
 
-    tetra_sort_1 = sort(tetra_sort_1,2);
+    faces = sort(faces,2);
 
-    [ tetra_sort_1, J ] = sortrows ( tetra_sort_1, [1 2 3] );
+    [ faces, J ] = sortrows ( faces, [1 2 3] );
 
-    tetra_ind = zeros(size(tetra_sort_1,1),1);
+    tetra_ind = zeros(size(faces,1),1);
 
-    I = find ( sum ( abs ( tetra_sort_1(2:end,1:3) - tetra_sort_1(1:end-1,1:3) ), 2 ) == 0 );
+    I = find ( sum ( abs ( faces(2:end,1:3) - faces(1:end-1,1:3) ), 2 ) == 0 );
 
-    tetra_sort_2 = tetra_sort_2(J,:);
+    faceLabels = faceLabels(J,:);
 
     tetra_ind(I) = 1;
 
@@ -70,12 +70,12 @@ function [surface_triangles, tetra_ind] = tetraSurfaceTriangles ( tetra, tI )
 
     I = find(tetra_ind == 0);
 
-    tetra_ind = sub2ind(size(tetra),repmat(tetra_sort_2(I,2),1,3),ind_m(tetra_sort_2(I,1),:));
+    tetra_ind = sub2ind(size(tetra),repmat(faceLabels(I,2),1,3),ind_m(faceLabels(I,1),:));
 
     surface_triangles = tetra(tetra_ind);
 
     surface_triangles = uint32(surface_triangles(:,[1 3 2]));
 
-    tetra_ind = tetra_sort_2(I,2);
+    tetra_ind = faceLabels(I,2);
 
 end % function
