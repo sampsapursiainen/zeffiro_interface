c543f3e63c16babf3bba9d14b95f5e2634259d78
Santtu Söderholm <santtu.soderholm@tuni.fi>
2024-04-18 09:06:35 +0300

    Add class core.SuperNode

---
+++ +core/@SuperNode/SuperNode.m
@@ -0,0 +1,142 @@
+classdef SuperNode
+%
+% SuperNode
+%
+% A supernode in a finite element mesh is a structure that holds onto a central
+% mesh node index, a set of finite elements around the central node and the
+% surface of the volume defined by the elements. This could represent the
+% contact surface of a sensor with a finite element domain, for example.
+%
+
+    properties
+        centralNodeI         (1,1) uint32 { mustBeNonnegative } = 0  % A central node of this supernode.
+        radius               (1,1) double { mustBeNonnegative } = 0  % A radius of the largest sphere that is entirely contained in this supernode.
+        meshElements         (4,:) uint32 { mustBePositive }    = [] % Elements such as triangles or tetrahedra around the central node.
+        indInElements        (:,:) logical                      = [] % A logical array whose columns indicate which nodes in corresponding elements are within kwargs.radius of the supernode center.
+        surfaceTriangles     (3,:) uint32 { mustBePositive }    = [] % The surface of the volume defined by elements.
+        surfaceTriangleAreas (1,:) double { mustBeNonnegative } = [] % Areas of the surface triangles.
+    end % properties
+
+    methods
+
+        function self = SuperNode (kwargs)
+        %
+        % SuperNode
+        %
+        % A constructor for this class. Keyword arguments with default values
+        % handle the no input argument case required by MATLAB.
+        %
+
+            arguments
+                kwargs.centralNodeI = 0
+                kwargs.meshElements = []
+                kwargs.indInElements = []
+                kwargs.surfaceTriangles = []
+                kwargs.surfaceTriangleAreas = []
+            end
+
+            fieldNames = string ( fieldnames (kwargs) ) ;
+
+            for fni = 1 : numel (fieldNames)
+
+                fieldName = fieldNames (fni) ;
+
+                self.(fieldName) = kwargs.(fieldName) ;
+
+            end % for
+
+        end % function
+
+    end % methods
+
+    methods (Static)
+
+        function self = fromMeshAndPos (meshNodes,meshElements,superNodePos,kwargs)
+        %
+        % self = fromMeshAndPos (meshNodes,meshElements,superNodePos,kwargs)
+        %
+        % Constructs a vector of SuperNodes by attaching a set of superNodePos
+        % to a given mesh, by finding their nearest nodes in the mesh.
+        %
+
+            arguments
+                meshNodes            (3,:) double { mustBeFinite }
+                meshElements         (4,:) uint32 { mustBePositive }
+                superNodePos         (3,:) double { mustBeFinite }
+                kwargs.nodeRadii     (1,:) double { mustBeNonnegative, mustBeFinite } = 0
+                kwargs.attachNodesTo (1,1) string { mustBeMember(kwargs.attachNodesTo,["surface","volume"]) } = "volume"
+            end
+
+            % Extract array sizes.
+
+            superNodeN = size (superNodePos,2) ;
+
+            % Expand radii to the number of nodes, if given as a scalar.
+
+            if isscalar (kwargs.nodeRadii)
+
+                nodeRadii = repmat ( kwargs.nodeRadii, 1, superNodeN ) ;
+
+            else
+
+                nodeRadii = kwargs.radii ;
+
+            end % if
+
+            % Preallocate vector of objects with default property values.
+
+            superNodes (superNodeN) = core.SuperNode ;
+
+            % Find supernode centers.
+
+            if kwargs.attachNodesTo == "volume"
+
+                [~, centralNodeIs] = core.attachSensors (superNodePos,meshNodes,[]);
+
+            else % use surface triangles.
+
+                % First find surface triangles and their coordinates.
+
+                surfTri = transpose ( core.tetraSurfaceTriangles (meshElements') ) ;
+
+                surfTriCoords = transpose ( meshNodes (:,surfTri) ) ;
+
+                % Then attach supernodes to surface triangles and map the result to global node indices.
+
+                [~, centralNodeIs] = core.attachSensors (superNodePos, surfTriCoords, []);
+
+                centralNodeIs = surfTri (centralNodeIs) ;
+
+            end % if
+
+            % Compute and set properties for all supernodes.
+
+            for ii = 1 : superNodeN
+
+                nI = centralNodeIs (ii) ;
+
+                superNodes (ii) . centralNodeI = nI ;
+
+                radius = nodeRadii (ii) ;
+
+                superNodes (ii) . radius = radius ;
+
+                [whichElements,surfTri,indInElements] = core.superNode (meshElements,nI,radius=radius,nodes=meshNodes') ;
+
+                superNodes (ii) . indInElements = indInElements ;
+
+                superNodes (ii) . meshElements = whichElements ;
+
+                superNodes (ii) . surfaceTriangles = surfTri ;
+
+                [superNodes(ii).surfaceTriangleAreas, ~] = core.triangleAreas (meshNodes,surfTri) ;
+
+            end % for
+
+            self = superNodes ;
+
+        end % function
+
+    end % methods (Static)
+
+end % classdef SuperNode
