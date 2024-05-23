3303904f42873509f37eb5601aa0dcc5fd5ce089
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2024-05-23 15:09:32 +0300

    core.preconditioners.ssor: return 2 triangular matrices instead of 1 full
    - The caller needs to manually perform back-substitution with the output
      factors to apply the pre-conditioner to both sides of the equation Ax = b, as
      is instructed in the modified documentation comment.

--- +core/+preconditioners/ssor.m
+++ +core/+preconditioners/ssor.m
@@ -1,15 +1,22 @@
-function prec = ssor ( A, kwargs )
+function [P1, P2] = ssor ( A, kwargs )
 %
-% prec = ssor ( A, kwargs )
+% [P1, P2] = ssor ( A, kwargs )
 %
-% Computes the SSOR preconditioner for the matrix A of a system Ax = b.
+% Computes the SSOR preconditioner M, split into 2 factors P1 and P2 for the
+% matrix A of a symmetric system Ax = b. To actually apply the preconditioner,
+% as in to solve z = inv M * x for z, one needs solve the following system by 2
+% back-substitutions:
+%
+%   P1 * c = x
+%   P2 * z = c
 %
 % kwargs:
 %
 % - coeff = 1
 %
-%   The preconditioner constant used in the computation. The default value of 1
-%   corresponds to the Gauss--Seidel preconditioner.
+%   The preconditioner constant used in the computation. Must be in the
+%   interval [0,2]. The default value of 1 corresponds to the Gauss--Seidel
+%   preconditioner.
 %
 
     arguments
@@ -17,16 +24,22 @@ function prec = ssor ( A, kwargs )
         kwargs.coeff (1,1) double { mustBeInRange(kwargs.coeff, 0, 2) } = 1
     end
 
-    L = tril ( A ) ;
+    N = size (A,1) ;
 
-    U = triu ( A ) ;
+    L = tril ( A, 1 ) ;
 
-    D = diag ( diag ( A ) ) ;
+    U = triu ( A, 1 ) ;
 
-    invD = 1 ./ D ;
+    Dvec = diag ( A ) ;
+
+    invD = sparse ( 1:N, 1:N, 1 ./ Dvec, N, N ) ;
+
+    D = sparse ( 1:N, 1:N, Dvec, N, N ) ;
 
     coeff = kwargs.coeff ;
 
-    prec = ( D + coeff * L ) * invD * ( D + coeff * U ) ;
+    P1 = speye (N) + coeff * L * invD ;
+
+    P2 = D + coeff * U ;
 
 end % function
