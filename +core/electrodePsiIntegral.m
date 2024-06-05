b66f664b07549ef250c5882183099ed2f9558fce
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2024-06-05 08:23:46 +0300

    Add function core.electrodePsiIntegral

---
+++ +core/electrodePsiIntegral.m
@@ -0,0 +1,85 @@
+function B = electrodePsiIntegral (Nn,superNodes,kwargs)
+%
+% B = electrodePsiIntegral (Nn,superNodes,kwargs)
+%
+% Assembles a matrix of integrals
+%
+%   ∫ ψᵢ dS
+%
+% over each electrode--mesh contact surface, represented by the surface
+% triangles of the given supernodes.
+%
+% Inputs:
+%
+% - Nn
+%
+%   The number of rows in the output matrix.
+%
+% - superNodes
+%
+%   Contain the contact surfaces between the FE mesh and the electrodes.
+%
+% - kwargs.areaThreshold = eps
+%
+%   The threshold, below which an electrode will be interpreted as a point electrode.
+%
+%
+
+    arguments
+        Nn (1,1) double { mustBeInteger, mustBePositive }
+        superNodes (1,:) core.SuperNode
+        kwargs.areaThreshold (1,1) double { mustBeReal, mustBePositive, mustBeFinite } = eps
+    end
+
+    % Number of electrode contact surfaces.
+
+    Ne = numel (superNodes) ;
+
+    % This is the value of the integral over the surface triangle of a standard
+    % tetrahedron.
+
+    psiIntegral = 1 / 3 ;
+
+    % Place integral values scaled by areas into the output matrix.
+
+    for snI = uint32 (1 : Ne)
+
+        % Find out the nodes that this supernode is made of and its area. If
+        % the area of the supernode is too small, it is interpreted as a point
+        % electrode and only the supernode center will be used (Agsten 2018).
+
+        totalArea = superNodes (snI) . totalSurfaceArea ;
+
+        isPointElectrode = totalArea <= kwargs.areaThreshold ;
+
+        superNodeCenter = superNodes (snI) . centralNodeI ;
+
+        if isPointElectrode
+
+            nodeI = superNodeCenter ;
+            triArea = 1 ;
+            maxVertexI = 1 ;
+
+        else
+
+            nodeI = superNodes (snI) . surfaceTriangles ;
+            triArea = superNodes (snI) . surfaceTriangleAreas ;
+            maxVertexI = 3 ;
+
+        end % if isPointElectrode
+
+        % Distribute the impedance to the node positions in B.
+
+        entry = psiIntegral .* triArea ;
+
+        for vi = 1 : maxVertexI
+
+            vertexI = nodeI (vi,:) ;
+
+            B = B + sparse ( vertexI, snI, entry, Nn, Ne) ;
+
+        end % for vi
+
+    end % for snI
+
+end % function
