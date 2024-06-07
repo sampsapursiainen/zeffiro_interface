19deafac6313ee98f1e95411b3cc9cd1a2ce02c0
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2024-06-07 14:48:40 +0300

    Add function core.dBdZ

---
+++ +core/dBdZ.m
@@ -0,0 +1,32 @@
+function diffB = dBdZ (Bint, Z, eA)
+%
+% diffB = dBdZ ()
+%
+% Computes the derivative of the matrix
+%
+%   B = ∫ ψi dS / Z / eA
+%
+% Inputs:
+%
+% - Bint
+%
+%   The integral ∫ ψi dS corresponding to the contact surface of an electrode.
+%
+% - Z
+%
+%   The impedance of the electrode.
+%
+% - eA
+%
+%   The total area of the electrode.
+%
+
+    arguments
+        Bint (:,:) double { mustBeFinite }
+        Z    (1,:) double { mustBeNonNan }
+        eA   (1,:) double { mustBeNonnegative, mustBeFinite }
+    end
+
+    diffB = - intB / Z ^ 2 / eA ;
+
+end % function
