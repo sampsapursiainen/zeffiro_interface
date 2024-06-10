065763f6543d0cc3beb75a710018e968cb0b7886
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2024-06-10 09:26:53 +0300

    core.dRdZ: initial version ready?

--- +core/dRdZ.m
+++ +core/dRdZ.m
@@ -1,75 +1,126 @@
-function D = dRdZ ( R, M, B, Bint, T, S, Z, eA )
+function D = dRdZ ( A, dAdZ, R, dBdZ, invS, dSdZ, kwargs )
 %
-% D = dRdZ ( R, M, B, Bint, T, Z, eA )
+% DR = dRdZ ( A, dAdZ, R, dBdZ, invS, dSdZ, kwargs )
 %
 % Computes the derivative of the resistivity matrix R with respect to impedance
 % Z.
 %
 % Inputs:
 %
+% - A
+%
+%   The stiffness matrix of a system [ A, B ; B', C ] * [ u ; v ] = [ x  ; y ].
+%
+% - dAdZ
+%
+%   The derivative of the stiffness matrix with respect to the impedance Z.
+%
 % - R
 %
 %   The current value of the resistivity matrix.
 %
-% - M
+% - dBdZ
 %
-%   A mass matrix ∫ ψi ψj dS corresponding to a single electrode with impedance
-%   Z.
+%   The derivative of the matrix B = ∫ ψi dS / Z / eA with respect to the impedance Z.
 %
-% - B
+% - invS
 %
-%   The matrix ∫ ψi dS / Z / eA with all electrodes integrated into it.
+%   The inverse of the Schur complement of the stiffness matrix A.
 %
-% - Bint
+% - dSdZ
 %
-%   A matrix containing the contact surface integral ∫ ψi dS for a single
-%   electrode.
+%   The derivative of the Schur complement with respect to the impedance Z.
 %
-% - T
-%
-%   A transfer matrix A \ B.
-%
-% - S
-%
-%   The Schur complement of the stiffness matrix A.
-%
-% - Z
-%
-%   The impedance we are differentiating with respect to.
-%
-% - eA
-%
-%   The total surface area ∫ dS of the electrode with impedance Z.
-%
-% - eI
-%
-%   The index of the electrode whose impedance Z we are differentiating with
-%   respect to.
-%
-% - L
-%
-%   The size of derivatives with respect to C
 %
 
     arguments
-        R (:,:) { mustBeNumeric, mustBeFinite }
-        M (:,:) { mustBeNumeric, mustBeFinite }
-        B (:,:) { mustBeNumeric, mustBeFinite }
-        Bint (:,:) { mustBeNumeric, mustBeFinite }
-        T (:,:) { mustBeNumeric, mustBeFinite }
-        S (:,:) { mustBeNumeric, mustBeFinite }
-        Z (1,1) double { mustBeNonNan }
-        eA (1,1) double { mustBeFinite, mustBeReal }
-        eI (1,1) double { mustBeInteger, mustBePositive, mustBeFinite }
-        L (1,1) double { { mustBeInteger, mustBePositive, mustBeFinite } }
+        A    (:,:) { mustBeNumeric, mustBeFinite }
+        dAdZ (:,:) { mustBeNumeric, mustBeFinite }
+        R    (:,:) { mustBeNumeric, mustBeFinite }
+        dBdZ (:,:) { mustBeNumeric, mustBeFinite }
+        invS (:,:) { mustBeNumeric, mustBeFinite }
+        dSdZ (:,:) { mustBeNumeric, mustBeFinite }
+        kwargs.useGPU (1,1) logical = true
+        kwargs.solverTol (1,1) double { mustBePositive, mustBeFinite } = 1e-8
+        kwargs.TdAdZRsolver (1,1) function_handle = @core.biConjugateGradientStabilized
+        kwargs.TdBdZinvSsolver (1,1) function_handle = @core.biConjugateGradientStabilized
     end
 
-    dAdZ = core.dAdZ (M, Z, eA) ;
+    disp ("Computing dRdZ…") ;
 
-    dBdZ = core.dAdZ (Bint, Z, eA) ;
+    Nn = size (A,1) ;
 
-    dCdZ = core.dAdZ (Z, eI, L) ;
+    Ne = size (R,2) ;
 
-    % TODO
+    xA = zeros (Nn,1) ;
+
+    xB = xA ;
+
+    bA = zeros (Nn,1) ;
+
+    bB = zeros (Nn,1) ;
+
+    dAdZR = dAdZ * R ;
+
+    dBdZinvS = dBdZ * invS ;
+
+    % Copy data to GPU if possible.
+
+    tolerance = kwargs.solverTol ;
+
+    if kwargs.useGPU && gpuDeviceCount("available") > 0
+        A = gpuArray (A) ;
+        xA = gpuArray (xA) ;
+        xB = gpuArray (xB) ;
+        bA = gpuArray (bA) ;
+        bB = gpuArray (bB) ;
+        tolerance = gpuArray (tolerance) ;
+    end % if
+
+    % Run solvers.
+
+    disp ("  Inverting A with respect to dAdZ * R and dBdZ * invS…")
+
+    Ta = zeros (Nn,Ne) ;
+
+    Tb = zeros (Nn,Ne) ;
+
+    for col = 1 : Ne
+
+        xA (:) = 0 ;
+
+        xB (:) = 0 ;
+
+        bA (:) = full ( dAdZR (:,col) ) ;
+
+        bB (:) = full ( dBdZinvS (:,col) ) ;
+
+        disp ("    dAdZ * R, column " + col + " / " + Ne) ;
+
+        [ xA(:), relResNormA, itersA ]  = kwargs.TdAdZRsolver (A, xA, bA, tolerance=tolerance) ;
+
+        disp ("    dBdZ * invS, column " + col + " / " + Ne) ;
+
+        [ xB(:), relResNormB, itersB ]  = kwargs.TdBdZinvSsolver (A, xB, bB, tolerance=tolerance) ;
+
+        if relResNormA > tolerance
+
+            error ( "Solver for dAdZ * R did not converge after the theoretical maximum number of iterations " + itersA + ". The relative residual norm was " + gather (relResNorm) + "." ) ;
+
+        end
+
+        if relResNormB > tolerance
+
+            error ( "Solver for dBdZ * R did not converge after the theoretical maximum number of iterations " + itersB + ". The relative residual norm was " + gather (relResNorm) + "." ) ;
+
+        end
+
+        Ta (:,col) = xA ;
+
+        Tb (:,col) = xB ;
+
+    end % for col
+
+    D = Ta + Tb + R * dSdZ * invS ;
 
 end % function
