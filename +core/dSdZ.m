5aa4b04eee064e94ebeb70c8af9d119821f65663
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2024-06-11 14:59:12 +0300

    core.dSdZ: pass in required values to improve separation of concerns

--- +core/dSdZ.m
+++ +core/dSdZ.m
@@ -1,6 +1,6 @@
-function DS = dSdZ ( dCdZ, dCHdZ, Be, T, B, A, Z, Me, eA, eI, kwargs )
+function DS = dSdZ ( dCdZ, dCHdZ, Be, T, B, invAdAdZ, invAdBdZ )
 %
-% DS = dSdZ ( dCdZ, dCHdZ, Be, T, B, A, Z, Me, eA, kwargs )
+% DS = dSdZ ( dCdZ, dCHdZ, Be, T, B, invAdAdZ, invAdBdZ )
 %
 % Computes the derivative of the Schur complement of A in a system
 %
@@ -10,37 +10,18 @@ function DS = dSdZ ( dCdZ, dCHdZ, Be, T, B, A, Z, Me, eA, eI, kwargs )
 %
 
     arguments
-        dCdZ   (:,:)
-        dCHdZ  (:,:)
-        Be     (:,:)
-        T      (:,:)
-        B      (:,:)
-        A      (:,:)
-        Z      (1,1)
-        Me     (:,:)
-        eA     (1,1)
-        eI     (1,1)
-        kwargs.tolerance (1,1) double { mustBePositive, mustBeFinite } = 1e-8
+        dCdZ     (:,:)
+        dCHdZ    (:,:)
+        Be       (:,:)
+        T        (:,:)
+        B        (:,:)
+        invAdAdZ (:,:)
+        invAdBdZ (:,:)
     end
 
     cB = ctranspose (B) ;
 
-    MT = Me * T / Z ^ 2 / eA ;
-
-    T1 = core.transferMatrix ( A, MT, useGPU=true, tolerances=kwargs.tolerance) ;
-
-    t2 = core.transferMatrix ( A, Be (:,eI), useGPU=true, tolerances=kwargs.tolerance) ;
-
-    [Tr,Tc] = size (T) ;
-
-    T2 = sparse ( Tr, Tc ) ;
-
-    T2 (:,eI) = t2 ;
-
-    DS = dCdZ ...
-        + dCHdZ * transpose (Be) * T ...
-        - cB * T1 ...
-        - cB * T2 ;
+    DS = dCdZ + dCHdZ * transpose (Be) * T - cB * invAdAdZ * T - cB * invAdBdZ ;
 
 
 end % function
