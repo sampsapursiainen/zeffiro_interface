55bb070ac01adf0dc27ef7e4433973d9fb822535
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2024-06-11 15:46:15 +0300

    tests.dRdZtest: precompute partial derivatives used across different functions

--- +tests/dRdZtest.m
+++ +tests/dRdZtest.m
@@ -2,12 +2,16 @@ col = 100 ;
 
 dAdZ = core.dAdZ ( Ms{col}, electrodes.impedances(col), superNodes(col).totalSurfaceArea ) ;
 
+invAdAdZ = core.invAY (A,dAdZ) ;
+
 dBdZ = core.dBdZ ( Bs{col}, electrodes.impedances(col) ) ;
 
-dCdZ = core.dCdZ ( Z(col), col,numel(superNodes) ) ;
+invAdBdZ = core.invAY (A,dBdZ) ;
+
+dCdZ = core.dCdZ ( Z(col), col, numel(superNodes) ) ;
 
 dCHdZ = core.dCHdZ ( Z(col), col, numel(superNodes) ) ;
 
-dSdZ = core.dSdZ ( dCdZ, dCHdZ, Bs{col}, TM, B, A, Z(col), Ms{col}, superNodes(col).totalSurfaceArea, col ) ;
+dSdZ = core.dSdZ ( dCdZ, dCHdZ, Bs{col}, TM, B, invAdAdZ, invAdBdZ ) ;
 
-dRdZ = core.dRdZ ( A, dAdZ, R, dBdZ, invSchurC, dSdZ ) ;
+dRdZ = core.dRdZ ( invAdAdZ, R, invAdBdZ, invSchurC, dSdZ ) ;
