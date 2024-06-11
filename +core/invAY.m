6a4e20cc0dd9ae0f81c3f2ece83792f6b244d861
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2024-06-11 11:38:19 +0300

    Add function core.invAY
    - ... for inverting A against a column-sparse matrix Y.

---
+++ +core/invAY.m
@@ -0,0 +1,29 @@
+function X = invAY (A, Y, kwargs)
+%
+% X = invAY (A, Y, kwargs)
+%
+% Inverts a sparse matrix A against a supposedly column-sparse matrix Y.
+%
+
+    arguments
+        A (:,:)
+        Y (:,:)
+        kwargs.tolerance (1,1) double { mustBeReal, mustBePositive, mustBeFinite } = 1e-10
+        kwargs.useGPU    (1,1) logical = true
+    end
+
+    [ Nr, Nc ] = size (A) ;
+
+    [ ~, cols ] = find (Y) ;
+
+    ucols = unique (cols) ;
+
+    rhs = Y (:,ucols) ;
+
+    T = core.transferMatrix (A, rhs, tolerances=kwargs.tolerance,useGPU=kwargs.useGPU) ;
+
+    X = sparse (Nr,Nc) ;
+
+    X (:,ucols) = T ;
+
+end % function
