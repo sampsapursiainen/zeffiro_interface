5629b5c1f2d3c03bdbb5608b99495116bdf45016
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2024-07-02 11:15:03 +0300

    leadFieldComparison: allow passing in comparison function and display its name on the x-axis

--- +examples/+studies/+Rlinearization/leadFieldComparison.m
+++ +examples/+studies/+Rlinearization/leadFieldComparison.m
@@ -12,11 +12,12 @@ function leadFieldComparison(dataFilePattern,dataFileName,outFolderName,lowerQ,u
         outFolderName (1,1) string { mustBeFolder }
         lowerQ (1,1) double { mustBeInRange(lowerQ, 0, 1) } = 0.10
         upperQ (1,1) double { mustBeGreaterThan(upperQ,lowerQ), mustBeLessThanOrEqual(upperQ,1) } = 0.90
-        kwargs.numBins (1,1) int32 { mustBePositive } = 30
+        kwargs.numBins (1,1) int32 { mustBePositive } = 50
         kwargs.figHandle (1,1) double { mustBeInteger, mustBePositive } = 100
         kwargs.initLName (1,1) string = "L"
-        kwargs.refLName (1,1) string = "newL"
+        kwargs.refLName (1,1) string = "refL"
         kwargs.linLName (1,1) string = "linL"
+        kwargs.comparisonFn (1,1) function_handle = @tests.relativeError
     end
 
     disp ("Loading reference lead field " + dataFileName + " from disk...") ;
@@ -49,6 +50,8 @@ function leadFieldComparison(dataFilePattern,dataFileName,outFolderName,lowerQ,u
 
     aN = numel (dataFileNames) ;
 
+    xString = string ( char (kwargs.comparisonFn) ) ;
+
     for ii = 1 : aN
 
         dataFileName = dataFileNames (ii) ;
@@ -57,7 +60,7 @@ function leadFieldComparison(dataFilePattern,dataFileName,outFolderName,lowerQ,u
 
         disp ("File " + dataFileName + "(" + ii + " / " + aN + ")") ;
 
-        [ realDiff, imagDiff ] = performComparison (refL, dataFileName,kwargs.refLName,kwargs.linLName) ;
+        [ realDiff, imagDiff ] = performComparison (refL, dataFileName,kwargs.refLName,kwargs.linLName,kwargs.comparisonFn) ;
 
         realDiffDisp = realDiff ( realDiff >= quantile (realDiff,lowerQ) & realDiff <= quantile (realDiff,upperQ) ) ;
 
@@ -75,7 +78,7 @@ function leadFieldComparison(dataFilePattern,dataFileName,outFolderName,lowerQ,u
 
         title (realAx, fname, Interpreter="none" ) ;
 
-        xlabel (imagAx,"( (LLin - Lref) - (Lnew - Lref) ) / (Lnew - Lref)", Interpreter="none") ;
+        xlabel (imagAx, xString, Interpreter="none") ;
 
         ylabel (realAx,"real samples", Interpreter="none") ;
 
@@ -97,13 +100,14 @@ end % function
 
 %% Helper functions.
 
-function [ realDiff, imagDiff ] = performComparison (origL, dataFileName, refLName, linLName)
+function [ realDiff, imagDiff ] = performComparison (origL, dataFileName, refLName, linLName, comparisonFn)
 
     arguments
         origL (:,:) double { mustBeFinite }
         dataFileName (1,1) string { mustBeFile }
         refLName (1,1) string
         linLName (1,1) string
+        comparisonFn (1,1) function_handle
     end
 
     disp ("Opening file " + dataFileName + "...") ;
@@ -130,9 +134,9 @@ function [ realDiff, imagDiff ] = performComparison (origL, dataFileName, refLNa
 
     disp ("Computing relative difference of differences...") ;
 
-    realDiff = tests.relativeError (real(linDiff), real(refDiff)) ;
+    realDiff = comparisonFn (real(linDiff), real(refDiff)) ;
 
-    imagDiff = tests.relativeError (imag(linDiff), imag(refDiff)) ;
+    imagDiff = comparisonFn (imag(linDiff), imag(refDiff)) ;
 
 end % function
 
