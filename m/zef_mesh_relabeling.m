d391279506d0f6ccff15ee328f2b23b6effa6072
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2024-08-13 02:14:48 +0300

    Zeffiro developer: A regular push adding the changes made in the current local repository to the remote origin. Contents of the folders ./data/ and ./profile/ are ignored. The update necessitates creating a personal access token.

--- m/zef_mesh_relabeling.m
+++ m/zef_mesh_relabeling.m
@@ -64,45 +64,6 @@ domain_labels_original = domain_labels;
 % compartment_counter = 0;
 % relabeling_pool = zeros(size(domain_labels));
 
-if zef.extensive_relabeling
-
-tic;
-for i_labeling =  1 : length(zef.reuna_p)-1
-    for k_labeling =  1 : length(zef.reuna_submesh_ind{i_labeling})
-
-compartment_counter = compartment_counter + 1;
-domain_label_ind = find(ismember(unique_domain_labels,compartment_counter));
-
-if domain_label_ind
-
-                   if k_labeling == 1
-                        reuna_t_aux = zef.reuna_t{i_labeling}(1:zef.reuna_submesh_ind{i_labeling},:);
-                   else
-                        reuna_t_aux = zef.reuna_t{i_labeling}(zef.reuna_submesh_ind{i_labeling}(k_labeling-1)+1: zef.reuna_submesh_ind{i_labeling}(k_labeling),:);
-                   end
-
-                tetra_ind_aux = 0;
-                test_ind = -ones(size(nodes,1),1);
-
-                    I_1 = find(domain_labels <= compartment_counter);
-                    [I_2] = zef_surface_mesh(tetra(I_1,:));
-                    I_1 = setdiff([1:size(domain_labels,1)]',I_1);
-                    [I_2, ~] = find(ismember(tetra(I_1,:),I_2));
-                    relabeling_pool(I_1(I_2)) = 1;
-                    time_val = toc;
-                    zef_waitbar(compartment_counter,n_compartments,h,['Extracting refined surfaces ' int2str(compartment_counter) ' of ' int2str(n_compartments) '. Ready: ' datestr(datevec(now+(n_compartments/compartment_counter - 1)*time_val/86400)) '.']);
-                   
-end
-    end
-end
-
-         relabeling_pool_ind = find(relabeling_pool);
-                [nodes_aux, tetra_aux] = zef_get_submesh(nodes, tetra, relabeling_pool_ind);
-                [label_ind_aux] = zef_solid_angle_labeling(zef, tetra_aux, nodes_aux, h);
-                domain_labels(relabeling_pool_ind) = zef_choose_domain_labels(zef,label_ind_aux(tetra_aux),use_labeling_priority);
-
-end
-
     compartment_counter = 0;
 
 for i_labeling =  1 : length(zef.reuna_p)-1
@@ -110,6 +71,7 @@ for i_labeling =  1 : length(zef.reuna_p)-1
 
 compartment_counter = compartment_counter + 1;
 domain_label_ind = find(ismember(unique_domain_labels,compartment_counter));
+domain_label_ind_2 = find(ismember(unique_domain_labels,compartment_counter+1));
 
 if domain_label_ind
 
@@ -119,8 +81,32 @@ if domain_label_ind
                         reuna_t_aux = zef.reuna_t{i_labeling}(zef.reuna_submesh_ind{i_labeling}(k_labeling-1)+1: zef.reuna_submesh_ind{i_labeling}(k_labeling),:);
                    end
 
+if zef.extensive_relabeling
+
                 tetra_ind_aux = 0;
                 test_ind = -ones(size(nodes,1),1);
+
+ while not(isempty(tetra_ind_aux))
+                    I_1 = find(domain_labels == compartment_counter+1);
+                    I_8 = find(domain_labels <= compartment_counter);
+                    [I_2] = zef_surface_mesh(tetra(I_1,:));
+                    I_1 = setdiff(I_8,I_1);
+                    [I_2, ~] = find(ismember(tetra(I_1,:),I_2));
+                    I_3 = tetra(I_1(I_2),:);
+                    [I_4,~,I_5] = unique(I_3);
+                    I_6 = find(test_ind(I_4) < 0);
+                    I_7 = zef_point_in_compartment(zef,zef.reuna_p{i_labeling},reuna_t_aux,nodes(I_4(I_6),:),[compartment_counter n_compartments]);
+                    test_ind(I_4(I_6)) = 0;
+                    test_ind(I_4(I_6(I_7))) = 1;
+                    I_5 = reshape(test_ind(I_4(I_5)),size(I_3));
+                    tetra_ind_aux = I_1(I_2(find(sum(I_5,2)<4)));
+                    domain_labels(tetra_ind_aux) = min(n_compartments, unique_domain_labels(domain_label_ind_2));
+ end
+
+end
+
+  tetra_ind_aux = 0;
+  %              test_ind = -ones(size(nodes,1),1);
 
                 while not(isempty(tetra_ind_aux))
                     I_1 = find(domain_labels <= compartment_counter);
