742629bdc1e1a0e8aba8d10d8ca759487e7fa13b
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2024-08-27 12:25:15 +1000

    Zeffiro developer: A regular push adding the changes made in the current local repository to the remote origin. Contents of the folders ./data/ and ./profile/ are ignored. The update necessitates creating a personal access token.

--- m/forward_simulation/lead_field/zef_tes_lead_field.m
+++ m/forward_simulation/lead_field/zef_tes_lead_field.m
@@ -2,11 +2,11 @@ warning('off');
 zef.lead_field_type = 5;
 zef.imaging_method = 1;
 zef_delete_original_field;
-zef_process_meshes;
-zef_attach_sensors_volume(zef,zef.sensors);
+zef = zef_process_meshes(zef);
+zef.sensors_attached_volume = zef_attach_sensors_volume(zef,zef.sensors);
 zef_lead_field_matrix;
 [zef.L,zef.source_positions,zef.source_directions] = zef_lead_field_filter(zef.L,zef.source_positions,zef.source_directions,zef.lead_field_filter_quantile);
 if zef.source_interpolation_on
-    zef_source_interpolation;
+   zef = zef_source_interpolation(zef);
 end
 warning('on');
