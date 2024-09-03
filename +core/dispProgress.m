e115a0d7f50faa8817dd2308c64a81631e32623e
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2024-09-03 09:43:18 +0300

    Add function core.dispProgress

---
+++ +core/dispProgress.m
@@ -0,0 +1,23 @@
+function dispProgress (currentI, maxI, printInterval, kwargs)
+%
+% dispProgress (currentI, maxI, printInterval=100, kwargs.indent=2)
+%
+% Can be used to display the progress of a loop.
+%
+
+    arguments
+        currentI (1,1) double { mustBeInterger, mustBePositive }
+        maxI (1,1) double { mustBeInterger, mustBePositive, mustBeGreaterThanOrEqual(maxI,currentI) }
+        printInterval (1,1) double { mustBeInteger, mustBePositive } = 100
+        kwargs.indent (1,1) double { mustBeInteger, mustBeNonnegative } = 2
+    end
+
+    indent = strjoin ( repmat ( " ", 1, kwargs.indent ), "" ) ;
+
+    if currentI == 1 || currentI == maxI || mod ( currentI, printInterval ) == 0
+
+        disp ( indent + currentI + " / " + maxI )
+
+    end % if
+
+end % function
