12eb7494501911ba899803a20ea55005a886eacf
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2024-09-11 12:06:06 +0300

    linearizeResistivityMatrix: use core.tesLeadField in the direct computation of R and L

--- +examples/+studies/+Rlinearization/linearizeResistivityMatrix.m
+++ +examples/+studies/+Rlinearization/linearizeResistivityMatrix.m
@@ -18,12 +18,17 @@ function linearizeResistivityMatrix (nodes, tetra, elePos, volumeCurrentI, sigma
         epsilon (:,:) double { mustBeFinite }
         superNodes (:,1) core.SuperNode
         kwargs.electrodeI (:,:) double { mustBeInteger, mustBePositive, mustBeFinite } = transpose ( [ 4 , 8 ; 3 , 7 ] )
-        kwargs.startFreq (1,1) double { mustBePositive, mustBeFinite } = 1000
+        kwargs.startFreq (1,1) double { mustBeNonnegative, mustBeFinite } = 1000
         kwargs.dFreqs (1,:) double  { mustBePositive, mustBeFinite } = [1, 10, 100, 1000,10000]
         kwargs.capacitance (1,1) double { mustBePositive, mustBeFinite } = 3.5e-6
         kwargs.solverTol (1,1) double { mustBePositive, mustBeFinite } = 1e-9
         kwargs.sourceTetraI (:,1) double { mustBePositive } = 1 : numel (volumeCurrentI)
+        kwargs.electrodeSize (:,1) double { mustBePositive, mustBeFinite } = 1e-3
     end
+
+    disp ("Positioning sources…")
+
+    [ ~, sourceTetI ] = core.positionSources ( nodes', tetra (volumeCurrentI,:)', kwargs.sourceN ) ;
 
     tetV = core.tetraVolume (nodes, tetra,true) ;
 
@@ -37,35 +42,13 @@ function linearizeResistivityMatrix (nodes, tetra, elePos, volumeCurrentI, sigma
 
     % C = 1.66 to 6.65 μF
 
+    resistance = 2e3 ;
+
     inductance = 0 ;
 
-    Ne = size (elePos,2) ;
+    Zs = core.impedanceFromRwLC ( resistance, angFreq, inductance, kwargs.capacitance ) ;
 
-    Zs = core.impedanceFromRwLC (2e3*ones(Ne,1), angFreq, inductance, kwargs.capacitance) ;
-
-    electrodes = core.ElectrodeSet ( positions=elePos, impedances=Zs, outerRadii=1e-3 ) ;
-
-    %%
-
-    disp ("Computing volume currents…") ;
-
-    [G1, G2, G3] = core.tensorNodeGradient (nodes, tetra, tetV, core.reshapeTensor (conductivity), volumeCurrentI) ;
-
-    G = [ G1 ; G2 ; G3 ] ;
-
-    % disp( "Count numbers of source indices in output lead field." ) ;
-
-    % uniqueSourceTetraI = unique ( kwargs.sourceTetraI ) ;
-
-    % ustN = numel (uniqueSourceTetraI) ;
-
-    % sourcesPerTetra = zeros ( ustN,1 ) ;
-
-    % for ii = 1 : ustN
-
-    %     sourcesPerTetra (ii) = sum ( kwargs.sourceTetraI == uniqueSourceTetraI (ii) ) ;
-
-    % end % for
+    electrodes = core.ElectrodeSet ( positions=elePos, impedances=Zs, outerRadii=kwargs.electrodeSize ) ;
 
     %%
 
@@ -73,45 +56,23 @@ function linearizeResistivityMatrix (nodes, tetra, elePos, volumeCurrentI, sigma
 
     tic ;
 
-    [ A, B, C, T, S, invS, R ] = matricesDependingOnZ (nodes, tetra, tetV, conductivity, electrodes, superNodes) ;
+    [Lini, Rini, Gx, Gy, Gz] = core.tesLeadField ( nodes, tetra, tetV, volumeCurrentI, sourceTetI, electrodes, superNodes, conductivity ) ;
 
     toc ;
 
-    %%
-
-    disp ("Computing initial lead field...") ;
-
-    L = intersperseRowsXYZ (- G * R) ;
+    G = [ Gx ; Gy ; Gz ] ;
 
     %%
 
     disp ("Saving initial matrices to their own files...")
 
-    Afile = matfile ("A.mat",Writable=true) ;
-    Bfile = matfile ("B.mat",Writable=true) ;
-    Cfile = matfile ("C.mat",Writable=true) ;
-    Tfile = matfile ("T.mat",Writable=true) ;
-    Sfile = matfile ("S.mat",Writable=true) ;
-    invSfile = matfile ("invS.mat",Writable=true) ;
     Rfile = matfile ("R.mat",Writable=true) ;
     Lfile = matfile ("L.mat",Writable=true) ;
 
-    disp ("A...")
-    Afile.A = A ;
-    disp ("B...")
-    Bfile.B = B ;
-    disp ("C...")
-    Cfile.C = C ;
-    disp ("T...")
-    Tfile.T = T ;
-    disp ("S...")
-    Sfile.S = S ;
-    disp ("inv S...")
-    invSfile.invS = invS ;
     disp ("R...")
-    Rfile.R = R ;
+    Rfile.R = Rini ;
     disp ("L...")
-    Lfile.L = L ;
+    Lfile.L = Lini ;
 
     %%
 
@@ -163,7 +124,7 @@ function linearizeResistivityMatrix (nodes, tetra, elePos, volumeCurrentI, sigma
 
             mf = matfile ( fileName, Writable=true ) ;
 
-            linR = R ;
+            linR = Rini ;
 
             disp ( newline + "linR = R") ;
 
@@ -189,7 +150,7 @@ function linearizeResistivityMatrix (nodes, tetra, elePos, volumeCurrentI, sigma
 
                 dSdZ = core.dSdZ ( dCdZ, dCHdZ, Bs{col}, T, B, invAdAdZ, invAdBdZ ) ;
 
-                dRdZ = core.dRdZ ( invAdAdZ, R, invAdBdZ, invS, dSdZ ) ;
+                dRdZ = core.dRdZ ( invAdAdZ, Rini, invAdBdZ, invS, dSdZ ) ;
 
                 newZs (col) = core.impedanceFromRwLC ( real ( newZs (col) ), iiAngFreq, inductance, kwargs.capacitance ) ;
 
@@ -199,17 +160,15 @@ function linearizeResistivityMatrix (nodes, tetra, elePos, volumeCurrentI, sigma
 
             end % for jj
 
+            disp ("Computing linearized lead field...")
+
+            linL = core.intersperseArray (- G * linR, 1, 3) ;
+
             disp ("Computing new R directly with updated impedances…")
 
             newElectrodes = electrodes.withImpedances (newZs) ;
 
-            [ ~, ~, ~, ~, ~, ~, refR ] = matricesDependingOnZ (nodes, tetra, tetV, conductivity, newElectrodes, superNodes) ;
-
-            disp ("Computing lead fields…") ;
-
-            refL = intersperseRowsXYZ (- G * refR) ;
-
-            linL = intersperseRowsXYZ (- G * linR) ;
+            [refL, refR, ~, ~, ~] = core.tesLeadField ( nodes, tetra, tetV, volumeCurrentI, sourceTetI, newElectrodes, superNodes, conductivity, sourceN = 5000 ) ;
 
             disp ("Saving new Rs and Ls to file " + fileName + "…") ;
 
@@ -240,102 +199,5 @@ function linearizeResistivityMatrix (nodes, tetra, elePos, volumeCurrentI, sigma
         end % for ii
 
     end % for eI
-
-end % function
-
-%% Helper functions.
-
-function [A, B, C, T, S, invS, R] = matricesDependingOnZ (nodes, tetra, tetV, conductivity, electrodes, superNodes)
-%
-% [A, B, C, T, S, invS, R] = matricesDependingOnZ (nodes, tetra, tetV, conductivity, volumeCurrentI, electrodes, superNodes)
-%
-
-    disp("Initializing impedances for sensors…")
-
-    Z = electrodes.impedances ;
-
-    disp("Computing stiffness matrix components reA and imA…")
-
-    conductivity = core.reshapeTensor (conductivity) ;
-
-    A = core.stiffnessMat (nodes,tetra,tetV,conductivity);
-
-    disp("Applying boundary conditions to reA…")
-
-    A = core.stiffMatBoundaryConditions ( A, Z, superNodes ) ;
-
-    disp("Computing electrode potential matrix B for real and imaginary parts…")
-
-    B = core.potentialMat ( superNodes, Z, size (nodes,1) );
-
-    disp("Computing electrode voltage matrix C…")
-
-    C = core.voltageMat (Z);
-
-    disp("Computing transfer matrix and Schur complement for real part. This will take a (long) while.")
-
-    T = core.transferMatrix (A,B,tolerances=1e-7,useGPU=true) ;
-
-    disp ("Computing resistivity matrix R…") ;
-
-    S = (C - ctranspose (B) * T) ;
-
-    I = eye ( size (S) ) ;
-
-    invS = S \ I ;
-
-    R = T * invS ;
-
-end % function
-
-function M = intersperseRowsXYZ(M)
-
-    Mcopy = M ;
-
-    N = size (M,1) / 3 ;
-
-    M (1:3:end,:) = Mcopy (1:N,:) ;
-    M (2:3:end,:) = Mcopy (N+1:2*N,:) ;
-    M (3:3:end,:) = Mcopy (2*N+1:3*N,:) ;
-
-end % function
-
-function M = intersperseColsXYZ(M)
-
-    Mcopy = M ;
-
-    N = size (M,1) / 3 ;
-
-    M (:,1:3:end) = Mcopy (:,1:N) ;
-    M (:,2:3:end) = Mcopy (:,N+1:2*N) ;
-    M (:,3:3:end) = Mcopy (:,2*N+1:3*N) ;
-
-end % function
-
-function L = parcellateLeadField (L, uniqueSourceTetraI, sourcesPerTetra)
-%
-% L = parcellateLeadField (L, uniqueSourceTetraI, sourcesPerTetra)
-%
-% Restricts a given lead field to a given set of source positions and
-% normalizes its rows based on how many sources were placed into each active
-% element.
-%
-    arguments
-        L (:,:) double { mustBeFinite }
-        uniqueSourceTetraI (1,:) double { mustBePositive, mustBeInteger }
-        sourcesPerTetra (1,:) double { mustBePositive, mustBeInteger }
-    end
-
-    % Restrict lead field to the wanted source positions.
-
-    ustI = uniqueSourceTetraI ;
-
-    restrI = intersperseRowsXYZ ( [ ustI ; (ustI+1) ; (ustI+2) ] ) ;
-
-    L = L (restrI,:) ;
-
-    % Normalize the rows based on how many sources there are in each active element.
-
-    L = L ./ repelem ( sourcesPerTetra, 3, 1 ) ;
 
 end % function
