0dd1ea72fd1f801174530ee8a0b718cc8d76bd8e
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2024-09-23 12:01:02 +0300

    Rlinearization.workspaceSetup: load project file and generate electrodes according to the new structure

--- +examples/+studies/+Rlinearization/workspaceSetup.m
+++ +examples/+studies/+Rlinearization/workspaceSetup.m
@@ -1,5 +1,9 @@
 % A script for retrieving values from zef and converting them to a suitable format.
 
+projectPath = fullfile ("data", "head_for_R_linearization.mat") ;
+
+zef = matfile (projectPath) ;
+
 electrodePos1 = zef.s2_points([23,27],:);
 
 electrodePos2 = zef.s2_points([34,48],:);
@@ -12,9 +16,9 @@ superNodes1 = zefCore.SuperNode.fromMeshAndPos (zef.nodes',zef.tetra',electrodeP
 
 superNodes2 = zefCore.SuperNode.fromMeshAndPos (zef.nodes',zef.tetra',electrodePos2',nodeRadii=1,attachNodesTo="surface") ;
 
-ee1 = zefCore.ElectrodeSet (innerRadii=0,outerRadii=1,capacitances=3.5e-6,inductances=0,contactSurfaces=superNodes1,positions=[superNodes1.centralNodePos],impedances=2e3,frequencies=f1) ;
+ee1 = zefCore.ElectrodeSet (innerRadii=0,outerRadii=1,wetResistances=400, doubleLayerResistances=15,capacitances=3.5e-6,contactSurfaces=superNodes1,frequencies=f1) ;
 
-ee2 = zefCore.ElectrodeSet (innerRadii=0,outerRadii=1,capacitances=3.5e-6,inductances=0,contactSurfaces=superNodes2,positions=[superNodes2.centralNodePos],impedances=2e3,frequencies=f2) ;
+ee2 = zefCore.ElectrodeSet (innerRadii=0,outerRadii=1,wetResistances=400, doubleLayerResistances=15,capacitances=3.5e-6,contactSurfaces=superNodes2,frequencies=f2) ;
 
 conductivity = zefCore.reshapeTensor (zef.sigma(:,1)) ;
 
