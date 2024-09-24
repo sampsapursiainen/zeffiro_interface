3f577d1a782f601c2b9405e55b8fbc48f5dcca5c
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2024-09-24 15:17:48 +0300

    zefCore.dispProgress: update progress on a single line

--- +zefCore/dispProgress.m
+++ +zefCore/dispProgress.m
@@ -10,13 +10,36 @@ function dispProgress (currentI, maxI, printInterval, kwargs)
         maxI (1,1) double { mustBeInteger, mustBePositive, mustBeGreaterThanOrEqual(maxI,currentI) }
         printInterval (1,1) double { mustBeInteger, mustBePositive } = ceil (maxI / 100)
         kwargs.indent (1,1) double { mustBeInteger, mustBeNonnegative } = 2
+        kwargs.fileDescriptor (1,1) double { mustBeInteger, mustBeFinite } = 1
     end
 
-    indent = strjoin ( repmat ( " ", 1, kwargs.indent ), "" ) ;
+    if currentI == 1
+
+        fprintf ( kwargs.fileDescriptor, newline ) ;
+
+    end % if
 
     if currentI == 1 || currentI == maxI || mod ( currentI, printInterval ) == 0
 
-        disp ( indent + currentI + " / " + maxI )
+        indent = strjoin ( repmat ( " ", 1, kwargs.indent ), "" ) ;
+
+        maxLog10Str = string ( ceil ( log10 (maxI) ) ) ;
+
+        formatStr = "%s%" + maxLog10Str + "d / %" + maxLog10Str + "d" ;
+
+        outStr = sprintf ( formatStr, indent, currentI, maxI ) ;
+
+        outLen = strlength (outStr) ;
+
+        backspaces = repmat ('\b', 1, outLen) ;
+
+        fprintf ( kwargs.fileDescriptor, backspaces + outStr ) ;
+
+    end % if
+
+    if currentI == maxI
+
+        fprintf ( kwargs.fileDescriptor, newline ) ;
 
     end % if
 
