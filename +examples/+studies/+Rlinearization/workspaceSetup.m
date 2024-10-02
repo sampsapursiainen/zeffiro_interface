26af0670a89d570fe1e3ec2e7aa825a7406fbec3
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2024-10-02 12:28:47 +0300

    Rlinearization.workspaceSetup: rename zef to mf and drastically increase the number of sources.

--- +examples/+studies/+Rlinearization/workspaceSetup.m
+++ +examples/+studies/+Rlinearization/workspaceSetup.m
@@ -1,18 +1,18 @@
-% A script for retrieving values from zef and converting them to a suitable format.
+% A script for retrieving values from mf and converting them to a suitable format.
 
 f1 = 100 ;
 
 projectPath = fullfile ("data", "head_for_R_linearization.mat") ;
 
-zef = matfile (projectPath) ;
+mf = matfile (projectPath) ;
 
-nodes = zef.nodes / 1e3 ;
+nodes = mf.nodes / 1e3 ;
 
-tetra = zef.tetra ;
+tetra = mf.tetra ;
 
-electrodePos1 = zef.s2_points([41,42],:) / 1e3 ; % zef.s2_points([23,27],:) / 1e3;
+electrodePos1 = mf.s2_points([41,42],:) / 1e3 ; % mf.s2_points([23,27],:) / 1e3;
 
-% electrodePos2 = zef.s2_points([34,48],:) / 1e3 ;
+% electrodePos2 = mf.s2_points([34,48],:) / 1e3 ;
 
 f2 = 1010 ;
 
@@ -22,7 +22,7 @@ doubleLayerResistance = 1e4 ;
 
 capacitance = 1e-7 ;
 
-contactSurfaceRadii = 0.5e-3 ;
+contactSurfaceRadii = 5e-3 ;
 
 superNodes1 = zefCore.SuperNode.fromMeshAndPos (nodes',tetra',electrodePos1',nodeRadii=contactSurfaceRadii, attachNodesTo="surface") ;
 
@@ -30,15 +30,13 @@ superNodes1 = zefCore.SuperNode.fromMeshAndPos (nodes',tetra',electrodePos1',nod
 
 ee1 = zefCore.ElectrodeSet (contactResistances=contactResistance, doubleLayerResistances=doubleLayerResistance,capacitances=capacitance, contactSurfaces=superNodes1,frequencies=f1) ;
 
-conductivity = zefCore.reshapeTensor (zef.sigma(:,1)) ;
+conductivity = zefCore.reshapeTensor (mf.sigma(:,1)) ;
 
 eps0 = 8.8541878188e-12 ;
 
-permittivity = zefCore.reshapeTensor (zef.epsilon(:,1)) * eps0;
+permittivity = zefCore.reshapeTensor (mf.epsilon(:,1)) * eps0;
 
-activeI = zef.brain_ind ;
-
-sourceN = 5000 ;
+activeI = mf.brain_ind ;
 
 f1s = ee1.frequencies ;
 
@@ -72,6 +70,8 @@ S1 = zefCore.schurComplement (T1, ctranspose(B1), C1) ;
 
 [ Lx, Ly, Lz, R1 ] = zefCore.tesLeadField ( T1, S1, Gx1, Gy1, Gz1 ) ;
 
+sourceN = size (tetra,1) ;
+
 [ sourcePos, aggregationN, aggregationI, ~ ] = zefCore.positionSourcesRectGrid (nodes, tetra, activeI, sourceN) ;
 
 [ pLx, pLy, pLz ] = zefCore.parcellateLeadField (Lx, Ly, Lz, aggregationI, aggregationN, 1) ;
@@ -82,14 +82,14 @@ L = transpose (iL) ;
 
 %% Linearization bit.
 
-targetF = 1e5 + eps (1e5) + 1 ;
-
-stepSize = targetF - f1 ;
-
-dfs = [ stepSize, stepSize ] ;
-
-invS1 = S1 \ eye ( size (S1) ) ;
-
-newR1 = zefCore.linearizeResistivityMatrix (R1, A1, B1, T1, invS1, ee1, dfs, 1:2) ;
+% targetF = 1e5 + eps (1e5) + 1 ;
+%
+% stepSize = targetF - f1 ;
+%
+% dfs = [ stepSize, stepSize ] ;
+%
+% invS1 = S1 \ eye ( size (S1) ) ;
+%
+% newR1 = zefCore.linearizeResistivityMatrix (R1, A1, B1, T1, invS1, ee1, dfs, 1:2) ;
 
 save("f=" + f1 + "Hz.mat", "-v7.3") ;
