d6f830cdfba6a8843d3b183bdd77bfe597ac2119
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2024-10-02 19:44:17 +0300

    Zeffiro developer: A regular push adding the changes made in the current local repository to the remote origin. Contents of the folders ./data/ and ./profile/ are ignored. The update necessitates creating a personal access token.

--- m/zef_turn_compartment_onoff.m
+++ m/zef_turn_compartment_onoff.m
@@ -3,6 +3,10 @@ function [zef] = zef_turn_compartment_onoff(zef,compartment_onoff_vec)
 c_t = zef.compartment_tags; 
 n_c_t = length(c_t);
 
+if isempty(compartment_onoff_vec)
+compartment_onff_vec = ones(1,n_c_t);
+end
+
 for i = 1 : n_c_t
 
 zef.([c_t{i} '_on']) = compartment_onoff_vec(i);
