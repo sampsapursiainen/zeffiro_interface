582b977fe022e7cbc7e26f3cd52e26957b15c6cb
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2024-10-08 10:14:20 +0300

    Initial implementation of zefCore.eitLeadField
    - This should actually be renamed to eitLinearizedLeadField, ebcause the output
      is the derivative for linearization purposes with respect to conductivity.

--- +zefCore/eitLeadField.m
+++ +zefCore/eitLeadField.m
@@ -1,19 +1,17 @@
-function L = eitLeadField ( T, invS, dAds, R, currentPattern )
+function L = eitLeadField ( T, dAds, R, currentPattern, activeI, nodeI )
 %
-% L = eitLeadField ( T, invS, dAds, R, currentPattern )
+% L = eitLeadField ( T, invS, dAds, R, currentPattern, nodeI )
 %
 % Computes an uninterpolated gradiometric electrical impedance tomography lead field L
-% for the system [ A, B ; B' C ] * [ z ; v ] = [ - Gx ; I ]
+% for the system [ A, B ; B' C ] * [ z ; v ] = [ - Gx ; I ].
+%
+% See https://doi.org/10.1007/s12021-019-09436-9 for details.
 %
 % Inputs:
 %
 % - T
 %
 %   A transfer matrix A \ B of the above system .
-%
-% - invS
-%
-%   The inverse of a Schur complement of A of in the above system.
 %
 % - dAds
 %
@@ -28,6 +26,14 @@ function L = eitLeadField ( T, invS, dAds, R, currentPattern )
 %
 %   The current pattern vector I of the above system.
 %
+% - activeI
+%
+%   The indices of the elements into which sources are to be positioned.
+%
+% - nodeI
+%
+%   The node incides of the elements indicated by activeI.
+%
 % Outputs:
 %
 % - L
@@ -37,12 +43,55 @@ function L = eitLeadField ( T, invS, dAds, R, currentPattern )
 
     arguments
         T (:,:) double { mustBeFinite }
-        invS (:,:) double { mustbeFinite }
         dAds (:,10) double { mustBeFinite }
         R (:,:) double { mustBeFinite }
         currentPattern (:,1) double { mustBeFinite }
+        activeI (:,1) double { mustBeInteger, mustbeFinite, mustbePositive }
+        nodeI (:,4) double { mustBeInteger, mustbeFinite, mustbePositive }
     end
 
-    L = [] ;
+    disp ( newline + "Building linearized EIT lead field:" + newline ) ;
+
+    cpN = numel (currentPattern) ;
+
+    activeN = numel (activeI) ;
+
+    L = zeros (cpN,activeN) ;
+
+    for ii = 1 : activeN
+
+        zefCore.dispProgress (ii,activeN) ;
+
+        aI = activeI (ii) ;
+
+        iidAdsVec = dAds (aI,:);
+
+        dDiagonal = [
+            iidAdsVec(1) 0 0 0 ;
+            0 iidAdsVec(5) 0 0 ;
+            0 0 iidAdsVec(8) 0 ;
+            0 0 0 iidAdsVec(10)
+        ] ;
+
+        dOffDiagonal = [
+            0 iidAdsVec(2) iidAdsVec(3) iidAdsVec(4) ;
+            0 0 iidAdsVec(6) iidAdsVec(7) ;
+            0 0          0 iidAdsVec(9) ;
+            0 0 0 0
+        ] ;
+
+        iidAdsMat = dDiagonal + dOffDiagonal + transpose (dOffDiagonal);
+
+        iiNodeI = nodeI (ii,:);
+
+        iiT = T (:, iiNodeI) ;
+
+        % L = - R * invS * transpose (B) * inv(A) * dAds * T * invS * currentPattern ;
+
+        iiL = - R * iiT * iidAdsMat * transpose (iiT) * currentPattern ;
+
+        L (:,ii) = L (:,ii) + iiL (:);
+
+    end % for ii
 
 end % function
