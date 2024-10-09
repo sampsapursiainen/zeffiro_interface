8c90dd6239ffafc591feaf798736831d53c7ca06
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2024-10-10 00:45:52 +0300

    Zeffiro developer: A regular push adding the changes made in the current local repository to the remote origin. Contents of the folders ./data/ and ./profile/ are ignored. The update necessitates creating a personal access token.

--- +utilities/+brainstorm2zef/+m/zef_bst_create_project.m
+++ +utilities/+brainstorm2zef/+m/zef_bst_create_project.m
@@ -9,15 +9,15 @@ if nargin < 5
 end
 
 if nargin < 4
-input_mode = 1;
+    input_mode = 1;
 end
 
 if nargin < 3
-run_type = 1;
+    run_type = 1;
 end
 
 if nargin < 2
-project_file_name = '';
+    project_file_name = '';
 end
 
 zef_bst = utilities.brainstorm2zef.m.zef_bst_get_settings(settings_file_name, zef_bst);
@@ -25,11 +25,11 @@ zef_bst = utilities.brainstorm2zef.m.zef_bst_get_settings(settings_file_name, ze
 zeffiro_path = fileparts(which('zeffiro_interface'));
 
 if evalin("base","exist('zef', 'var');")
-run([zeffiro_path filesep 'm' filesep 'zef_close_all.m']);
+    run([zeffiro_path filesep 'm' filesep 'zef_close_all.m']);
 end
 
 if isempty(fieldnames(zef))
-zef = zeffiro_interface('start_mode','nodisplay','verbose_mode',zef_bst.verbose_mode,'use_gpu',zef_bst.use_gpu,'parallel_processes',zef_bst.parallel_processes,'always_show_waitbar',zef_bst.always_show_waitbar);
+    zef = zeffiro_interface('start_mode','nodisplay','verbose_mode',zef_bst.verbose_mode,'use_gpu',zef_bst.use_gpu,'parallel_processes',zef_bst.parallel_processes,'always_show_waitbar',zef_bst.always_show_waitbar);
 end
 
 zef = zef_add_bounding_box(zef);
@@ -44,20 +44,20 @@ end
 
 [aux_path, aux_file] = fileparts(settings_file_name);
 if isequal(run_type,1)
-[compartment_settings, surface_meshes, zef] = utilities.brainstorm2zef.m.zef_bst_create_compartment_data(settings_file_name, zef_bst, zef);
-writecell(compartment_settings, fullfile(aux_path,[aux_file '_compartment_settings.dat']));
-save(fullfile(aux_path,[aux_file '_surface_meshes.mat']),'surface_meshes','-v7.3');
+    [compartment_settings, surface_meshes, zef] = utilities.brainstorm2zef.m.zef_bst_create_compartment_data(settings_file_name, zef_bst, zef);
+    writecell(compartment_settings, fullfile(aux_path,[aux_file '_compartment_settings.dat']));
+    save(fullfile(aux_path,[aux_file '_surface_meshes.mat']),'surface_meshes','-v7.3');
 else
-compartment_settings = readcell(fullfile(aux_path,[aux_file '_compartment_settings.dat']));  
-load(fullfile(aux_path,[aux_file '_surface_meshes.mat']));
+    compartment_settings = readcell(fullfile(aux_path,[aux_file '_compartment_settings.dat']));
+    load(fullfile(aux_path,[aux_file '_surface_meshes.mat']));
 end
 
 n_surface_meshes = size(compartment_settings,1);
 ind_vec_aux = zeros(n_surface_meshes,1);
 for i = 1 : n_surface_meshes
-if size(surface_meshes(i).Points,1) >= 4
-ind_vec_aux(i) = 1; 
-end
+    if size(surface_meshes(i).Points,1) >= 4
+        ind_vec_aux(i) = 1;
+    end
 end
 ind_vec_aux = find(ind_vec_aux);
 surface_meshes = surface_meshes(ind_vec_aux);
@@ -71,31 +71,29 @@ ref_vec_surf = flipud(cell2mat(compartment_settings(:,6)));
 ind_vec_aux = zeros(n_surface_meshes,1);
 h_waitbar = zef_waitbar(0, 'Creating project.');
 for i = 1 : n_surface_meshes
-zef_waitbar(i/n_surface_meshes,h_waitbar,'Creating project.');
-surface_names_aux = {surface_meshes.Name};
-i_aux = find(ismember(surface_names_aux,compartment_settings{i,2}),1);
-if not(isempty(i_aux))
-    ind_vec_aux(i) = 1;
-zef = zef_add_compartment(zef);
-zef.([zef.compartment_tags{1} '_name']) = [compartment_settings{i,4} ' : ' compartment_settings{i,2}];
-zef.([zef.compartment_tags{1} '_points']) = surface_meshes(i_aux).Points;
-zef.([zef.compartment_tags{1} '_triangles']) = surface_meshes(i_aux).Triangles(:,[1 3 2]);
-zef.([zef.compartment_tags{1} '_submesh_ind']) = size(surface_meshes(i_aux).Triangles,1);
-zef.([zef.compartment_tags{1} '_sigma']) = compartment_settings{i,10};
-zef.([zef.compartment_tags{1} '_activity']) = compartment_settings{i,12};
-if not(isempty(surface_meshes(i_aux).Color))
-zef.([zef.compartment_tags{1} '_color']) = surface_meshes(i_aux).Color;
-end
-zef = zef_build_compartment_table(zef);
-end
+    zef_waitbar(i/n_surface_meshes,h_waitbar,'Creating project.');
+    surface_names_aux = {surface_meshes.Name};
+    i_aux = find(ismember(surface_names_aux,compartment_settings{i,2}),1);
+    if not(isempty(i_aux))
+        ind_vec_aux(i) = 1;
+        zef = zef_add_compartment(zef);
+        zef.([zef.compartment_tags{1} '_name']) = [compartment_settings{i,4} ' : ' compartment_settings{i,2}];
+        zef.([zef.compartment_tags{1} '_points']) = surface_meshes(i_aux).Points;
+        zef.([zef.compartment_tags{1} '_triangles']) = surface_meshes(i_aux).Triangles(:,[1 3 2]);
+        zef.([zef.compartment_tags{1} '_submesh_ind']) = size(surface_meshes(i_aux).Triangles,1);
+        zef.([zef.compartment_tags{1} '_sigma']) = compartment_settings{i,10};
+        zef.([zef.compartment_tags{1} '_activity']) = compartment_settings{i,12};
+        if not(isempty(surface_meshes(i_aux).Color))
+            zef.([zef.compartment_tags{1} '_color']) = surface_meshes(i_aux).Color;
+        end
+        zef = zef_build_compartment_table(zef);
+    end
 end
 
-ind_vec_aux = find(ind_vec_aux);
+I_on = find(zef_bst.compartment_on_vec);
+ind_vec_aux = find(ind_vec_aux(I_on));
 ref_vec_vol = find(ref_vec_vol(ind_vec_aux));
 ref_vec_surf = find(ref_vec_surf(ind_vec_aux));
-
-ref_vec_vol = find(ref_vec_vol);
-ref_vec_surf = find(ref_vec_surf);
 
 zef.refinement_on = 1;
 zef.mesh_smoothing_on = zef_bst.mesh_smoothing_on;
@@ -111,18 +109,18 @@ zef.refinement_surface_compartments = ref_vec_surf;
 zef.refinement_volume_compartments = ref_vec_vol;
 zef.mesh_resolution = zef_bst.mesh_resolution;
 zef.use_fem_mesh_inflation = zef_bst.inflation_on;
-zef.fem_mesh_inflation_strength = zef_bst.inflation_strength;
+zef.fem_mesh_inflation_strength = zef_bst.inflation_strength;   if zef.([zef.temp_var_0 '_on'])
 zef.max_surface_face_count = zef_bst.surface_mesh_density;
 zef.priority_mode = zef_bst.priority_mode;
 zef.extensive_relabeling = zef_bst.extensive_relabeling;
 
 run(zef_bst.import_settings);
 
-zef = zef_turn_compartment_onoff(zef,zef_bst.compartment_on_vec)
+zef = zef_turn_compartment_onoff(zef,[zef_bst.compartment_on_vec 1]);
 zef = zef_process_meshes(zef);
 
 if not(isempty(zef_bst.labeling_priority))
-zef = zef_update_labeling_priority(zef,[],zef_bst.labeling_priority);
+    zef = zef_update_labeling_priority(zef,[],zef_bst.labeling_priority);
 end
 
 if not(isempty(char(project_file_name)))
