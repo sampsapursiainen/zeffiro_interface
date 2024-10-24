ff1960d1efe31bd57e113d1ff3f10c0cdb064d8b
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2024-10-24 11:48:09 +0300

    Zeffiro developer: A regular push adding the changes made in the current local repository to the remote origin. Contents of the folders ./data/ and ./profile/ are ignored. The update necessitates creating a personal access token.

--- plugins/Kalman/m/find_evolution_prior.m
+++ plugins/Kalman/m/find_evolution_prior.m
@@ -3,8 +3,8 @@ function [q] = find_evolution_prior(L, theta0, time_step, evolution_prior_db)
 % q to evolution prior
 % db(q/time_step/(svds(L,1)^2 * theta0))
 
-q = time_step*svds(L,1).^(2)*10^(evolution_prior_db/20) * theta0;
-%q = time_step*10^(evolution_prior_db/20) * theta0;
+%q = time_step*svds(L,1).^(2)*10^(evolution_prior_db/20) * sqrt(theta0);
+q = time_step*10^(evolution_prior_db/20) * theta0;
 
 end
 
