f7857269cf75fde22d7d5d71a82313e0cce5557e
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2024-11-04 14:48:10 +0200

    Add function zefCore.inDecibels

---
+++ +zefCore/inDecibels.m
@@ -0,0 +1,20 @@
+function dB = inDecibels (quantity,reference)
+%
+% dB = inDecibels (quantity,reference)
+%
+% Converts a given quantity into "decibels":
+%
+%   dB = 20 * log10 (quantity / reference)
+%
+% The quantity and reference must be positive and finite for this to make
+% sense.
+%
+
+    arguments
+        quantity { mustBeFinite, mustBePositive }
+        reference { mustBeFinite, mustBePositive }
+    end
+
+    dB = 20 * log10 (quantity / reference) ;
+
+end % function
