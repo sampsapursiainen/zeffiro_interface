80a39d23402e1844f4edc96eaa8e250d6e01c7d9
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2024-12-10 12:26:11 +0200

    Update find_evolution_prior.m

--- plugins/Kalman/m/find_evolution_prior.m
+++ plugins/Kalman/m/find_evolution_prior.m
@@ -1,10 +1,7 @@
-function [q] = find_evolution_prior(L, theta0, time_step, evolution_prior_db)
+function [q] = find_evolution_prior(L, theta0, number_of_frames, evolution_prior_db)
 
 % q to evolution prior
-% db(q/time_step/(svds(L,1)^2 * theta0))
-
-%q = time_step*svds(L,1).^(2)*10^(evolution_prior_db/20) * sqrt(theta0);
-q = time_step*10^(evolution_prior_db/20) * theta0;
+q = (1./number_of_frames)*10^(evolution_prior_db/20) * theta0;
 
 end
 
