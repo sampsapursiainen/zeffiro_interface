262a9cb2b63a6056c6c620de63761a6d2647281e
Santtu Söderholm <santtu.soderholm@tuni.fi>
2024-12-13 11:45:18 +0200

    utilities.figure_without_colorbar_fn: allow axis color limits as input arguments

--- +utilities/figure_without_colorbar_fn.m
+++ +utilities/figure_without_colorbar_fn.m
@@ -1,6 +1,6 @@
-function figure_without_colorbar_fn(figtool, filename, filetypes, resolution)
+function figure_without_colorbar_fn(figtool, filename, filetypes, resolution, kwargs)
 %
-% figure_without_colorbar_fn
+% figure_without_colorbar_fn(figtool, filename, filetypes, resolution, kwargs)
 %
 % Saves a given Zeffiro Interface figure tool image without the colobar
 % included.
@@ -24,6 +24,11 @@ function figure_without_colorbar_fn(figtool, filename, filetypes, resolution)
 %
 %   The resolution used for saving the image.
 %
+% - kwargs.colorLims = []
+%
+%   The color limits for the axes in the given figure tool. If this is empty,
+%   the existing color limis in the axes themselves will be used.
+%
 % Outputs:
 %
 % - None.
@@ -38,6 +43,8 @@ function figure_without_colorbar_fn(figtool, filename, filetypes, resolution)
         filetypes (:,1) string { mustBeMember( filetypes, [".pdf", ".eps", ".png", ".jpg"] ) }
 
         resolution (1,1) double { mustBePositive } = 400
+
+        kwargs.colorLims (2,:) double { mustBeFinite } = []
 
     end
 
@@ -58,6 +65,36 @@ function figure_without_colorbar_fn(figtool, filename, filetypes, resolution)
         cbar.Visible = false ;
 
     end
+
+    % Find all axes in the figure tool and set their color limits to the specified ones.
+
+    allAxesInFigure = findall (figtool, 'type', 'axes') ;
+
+    axisN = numel (allAxisInFigure) ;
+
+    [ ~, colorLimN ] = size (kwargs.colorLims) ;
+
+    for ii = 1 : axisN
+
+        axes = allAxesInFigure (ii) ;
+
+        if axisN > colorLimN && colorLimN == 1
+
+            axes.Clim = colorLimN (:,1) ;
+
+        elseif axisN == colorLimN
+
+            axes.Clim = kwargs.colorLims (:,ii) ;
+
+        else
+
+            % Nothing to do.
+
+        end % if
+
+    end % for ii
+
+    % Export figure in desired formats.
 
     for si = 1 : numel ( filetypes )
 
