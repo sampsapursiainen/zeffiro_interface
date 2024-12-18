2bf78860e0d6e4e3290e69a49e2de579c67a5f38
Santtu  Söderholm <santtu.soderholm@tuni.fi>
2024-12-18 11:42:27 +0200

    Add argument lowerQ to utilities.quantileFilteredFieldFn

--- +utilities/quantileFilteredFieldFn.m
+++ +utilities/quantileFilteredFieldFn.m
@@ -1,6 +1,6 @@
-function [filteredField, filteredSourcePos] = quantileFilteredFieldFn (field, sourcePos, quantileVal)
+function [filteredField, filteredSourcePos] = quantileFilteredFieldFn (field, sourcePos, lowerQ, upperQ)
 %
-% [filteredField, filteredSourcePos] = quantileFilteredFieldFn (field, sourcePos, quantileVal)
+% [filteredField, filteredSourcePos] = quantileFilteredFieldFn (field, sourcePos, upperQ)
 %
 % Restricts a given field below a certain quantile. Also selects the subset of
 % source positions that correspond to the filtered columns of the field.
@@ -15,15 +15,20 @@ function [filteredField, filteredSourcePos] = quantileFilteredFieldFn (field, so
 %
 %   The source positions corresponding to the groups of 3 columns in field.
 %
-% - quantileVal
+% - lowerQ
+%
+%   The lower quantile used for filtering.
+%
+% - upperQ
 %
 %   The quantile used for filtering.
 %
 
     arguments
-        field (:,:) double { mustBeFinite }
+        field (1,:) double { mustBeFinite }
         sourcePos (3,:) double { mustBeFinite }
-        quantileVal (1,1) double { mustBeInRange(quantileVal,0,1) }
+        lowerQ (1,1) double { mustBeInRange(lowerQ,0,1) }
+        upperQ (1,1) double { mustBeInRange(upperQ,lowerQ,1) }
     end
 
     fieldX = field(1:3:end);
@@ -32,11 +37,11 @@ function [filteredField, filteredSourcePos] = quantileFilteredFieldFn (field, so
 
     fieldZ = field(3:3:end);
 
-    filterXI = fieldX <= quantile(fieldX, quantileVal) ;
+    filterXI = fieldX >= quantile(fieldX, lowerQ) & fieldX <= quantile(fieldX, upperQ) ;
 
-    filterYI = fieldY <= quantile(fieldY, quantileVal) ;
+    filterYI = fieldY >= quantile(fieldY, lowerQ) & fieldY <= quantile(fieldY, upperQ) ;
 
-    filterZI = fieldZ <= quantile(fieldZ, quantileVal) ;
+    filterZI = fieldZ >= quantile(fieldZ, lowerQ) & fieldZ <= quantile(fieldZ, upperQ) ;
 
     sourceFilterI = filterXI & filterYI & filterZI ;
 
