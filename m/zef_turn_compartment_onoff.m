dd68e3b70c48add1583144ae90e3c9dfac13698e
Fernando Galaz Prieto <fernando.galazprieto@tuni.fi>
2025-01-06 20:26:32 +0200

    Update zef_turn_compartment_onoff.m
    Commended the if condition because it was giving an error. Pending for evaluation.

--- m/zef_turn_compartment_onoff.m
+++ m/zef_turn_compartment_onoff.m
@@ -3,9 +3,9 @@ function [zef] = zef_turn_compartment_onoff(zef,compartment_onoff_vec)
 c_t = zef.compartment_tags;
 n_c_t = length(c_t);
 
-if isempty(compartment_onoff_vec)
+% if isempty(compartment_onoff_vec)
     compartment_onoff_vec = ones(1,n_c_t);
-end
+% end
 
 for i = 1 : n_c_t
 
