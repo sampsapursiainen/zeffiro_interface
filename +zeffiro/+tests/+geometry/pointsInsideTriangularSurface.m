82d63975710f061dd5ce1c595fd0fcb805f36116
Santtu Söderholm <santtu.soderholm@tuni.fi>
2026-03-26 09:07:39 +0200

    zeffiro.tests.geometry.pointsInsideTriangularSurface: add a test for 2D input array

--- +zeffiro/+tests/+geometry/pointsInsideTriangularSurface.m
+++ +zeffiro/+tests/+geometry/pointsInsideTriangularSurface.m
@@ -7,23 +7,40 @@ function pointsInsideTriangularSurface
 
     % A surface of a standard tetrahedron.
 
-    triangles3D = [ 0 0 0 ; 1 0 0 ; 0 0 1 ]' ;
+    vertex1 = [ 0 0 0 ] ;
 
-    triangles3D(:,:,2) = [ 0 0 0 ; 1 0 0 ; 0 1 0 ]' ;
+    vertex2 = [ 1 0 0 ] ;
 
-    triangles3D(:,:,3) = [ 0 0 0 ; 0 0 1 ; 0 1 0 ]' ;
+    vertex3 = [ 0 1 0 ] ;
 
-    triangles3D(:,:,4) = [ 1 0 0 ; 0 1 0 ; 0 0 1 ]' ;
+    vertex4 = [ 0 0 1 ] ;
+
+    triangles2D = [ vertex1 ; vertex2 ; vertex4 ; vertex1 ; vertex2 ; vertex3 ; vertex1 ; vertex4 ; vertex3 ;  vertex2 ; vertex3 ; vertex4 ]' ;
+
+    triangles3D = [ vertex1 ; vertex2 ; vertex4 ]' ;
+
+    triangles3D(:,:,2) = [ vertex1 ; vertex2 ; vertex3 ]' ;
+
+    triangles3D(:,:,3) = [ vertex1 ; vertex4 ; vertex3 ]' ;
+
+    triangles3D(:,:,4) = [ vertex2 ; vertex3 ; vertex4 ]' ;
 
     % Centroid of a standard tetrahedron.
 
-    centroid = sum([ 0 0 0 ; 1 0 0 ; 0 1 0 ; 0 0 1 ]', 2) / 4
+    centroid = sum([ vertex1 ; vertex2 ; vertex3 ; vertex4 ]', 2) / 4
 
-    isInside = zeffiro.geometry.pointsInsideTriangularSurface(centroid, triangles3D)
+    isInside2D = zeffiro.geometry.pointsInsideTriangularSurface(centroid, triangles2D)
+
+    isInside3D = zeffiro.geometry.pointsInsideTriangularSurface(centroid, triangles3D)
 
     assert( ...
-        all(isInside), ...
-        "test pointsInsideTriangularSurface: the centroid of a standard tetrahedron was not inside of it." ...
+        all(isInside2D), ...
+        "test pointsInsideTriangularSurface: the centroid of a standard tetrahedron described by a 2D array was not inside of it." ...
+    )
+
+    assert( ...
+        all(isInside3D), ...
+        "test pointsInsideTriangularSurface: the centroid of a standard tetrahedron described by a 3D array was not inside of it." ...
     )
 
 end % function
