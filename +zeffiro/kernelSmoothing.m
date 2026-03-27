1e2b5966010464dc92123d88f022d8c6db849862
Santtu Söderholm <santtu.soderholm@tuni.fi>
2026-03-27 08:26:16 +0200

    zeffiro.kernelSmoothing: take separate centerPositions as an argument
    Makes it easier to run the function in memory-constrained situations
    Also fixed the doc comment so it matches the implementation.

--- +zeffiro/kernelSmoothing.m
+++ +zeffiro/kernelSmoothing.m
@@ -1,31 +1,36 @@
-function smoothedField = kernelSmoothing(sourcePositions, field, kwargs)
+function smoothedField = kernelSmoothing(centerPositions, fieldPositions, field, kwargs)
 %
-%   smoothedField = kernelSmoothing(field, kernelFn, kwargs)
+%   smoothedField = kernelSmoothing(centerPositions, fieldPositions, field, kwargs)
 %
 % Smooths a given vector field using a Gaussian kernel function.
 %
 % Arguments:
 %
-%   sourcePositions (3,:) double { mustBeFinite }
+%   centerPositions (3,:) double { mustBeFinite }
 %
-% Positions that a source is evaluated at.
+% The center positions of the Gaussian kernels.
+%
+%   fieldPositions (3,:) double { mustBeFinite }
+%
+% Positions that a field is evaluated at.
 %
 %   field (3,:) double { mustBeFinite }
 %
 % The field that is to be smoothed.
 %
-%   kwargs.variances (3,:) double { mustBeFinite } = ones(size(sourcePositions))
+%   kwargs.variances (3,:) double { mustBeFinite } = ones(size(centerPositions))
 %
 % The widths squared of the wanted kernel functions.
 %
 
     arguments
-        sourcePositions (3,:) double { mustBeFinite }
+        centerPositions (3,:) double { mustBeFinite }
+        fieldPositions (3,:) double { mustBeFinite }
         field (3,:) double { mustBeFinite }
-        kwargs.variances (3,:) double { mustBeFinite } = ones(size(sourcePositions))
+        kwargs.variances (3,:) double { mustBeFinite } = ones(size(centerPositions))
     end
 
-    kernels = zeffiro.kernels.gaussianKernel(sourcePositions, sourcePositions, kwargs.variances);
+    kernels = zeffiro.kernels.gaussianKernel(centerPositions, fieldPositions, kwargs.variances);
 
     smoothedField = field * kernels ;
 
