e31a7f7198904ba7fa0b4ca53d139626cf155613
Santtu Söderholm <santtu.soderholm@tuni.fi>
2026-07-20 13:16:39 +0300

    examples.studies.metroxraine2026extended.sourceSpacePerturbationExperiment: start implementing opposing vertex discovery

--- +examples/+studies/+metroxraine2026extended/sourceSpacePerturbationExperiment.m
+++ +examples/+studies/+metroxraine2026extended/sourceSpacePerturbationExperiment.m
@@ -42,6 +42,8 @@ function sourceSpacePerturbationExperiment(projectFilePath, compartmentOfInteres
 
     localElementNeighbours(1:10,:)
 
+    localConnectingFacets(1:10,:)
+
     disp("Mapping local element neighbour indices to global ones...")
 
     globalElementNeighbours = localElementNeighbours ;
@@ -56,29 +58,50 @@ function sourceSpacePerturbationExperiment(projectFilePath, compartmentOfInteres
 
     disp("Counting elements in compartment adjacency array with all 4 neighbours within " + compartmentOfInterest + "...")
 
-    counts = histcounts(localElementNeighbours(:,1), size(compartmentTetra,1)) ;
+    neighbourCounts = histcounts(localElementNeighbours(:,1), size(compartmentTetra,1)) ;
 
-    counts = counts(:) ;
+    neighbourCounts = neighbourCounts(:) ;
 
     disp("Taking elements which have 4 neighbours...")
 
-    elementsWith4Neighbours = compartmentTetra(counts > 3,:) ;
+    elementsWith4Neighbours = compartmentTetra(neighbourCounts > 3,:) ;
 
-    disp("Finding centroids of 4-neighbour elements...")
+    size(elementsWith4Neighbours)
 
-    centroidsOfElementsWith4Neighbours = transpose(zeffiro.geometry.elementCentroids(transpose(elementsWith4Neighbours),transpose(nodes))) ;
+    disp("Finding vertices of 4-neighbour elements...")
 
-    sourcePositions = centroidsOfElementsWith4Neighbours ;
+    verticesOfElementsWith4Neighbours = zeffiro.geometry.elementVertices(transpose(elementsWith4Neighbours),transpose(nodes)) ;
 
-    disp("Finding nodes in mesh nearest to source positions...")
+    disp("Taking first vertices of each element...")
 
-    nodesNearestToSourcePositions = knnsearch(nodes, sourcePositions) ;
+    firstVertices = verticesOfElementsWith4Neighbours(:,1,:) ;
 
-    tetrasContainingNearestNodes = find(any(ismember(compartmentTetra, nodesNearestToSourcePositions),2)) ;
+    disp("Finding vertices opposing first vertices in adjacent tetra...")
 
-    % TODO: check which of the above tetras actually contains which source position.
+    % Local facet ii of each tetra opposes node ii within the same tetrahedron,
+    % so we first find out where tetra connect to others through node 1 and then
+    % index in the other direction to find which facet in a neighbouring tetrahedron
+    % connects back to the original one. Then pick the opposing node corresponding to this facet.
 
-    % TODO: move a source to the neighbouring element.
+    localStartElements = localConnectingFacets(:,1) ;
+
+    firstFacetMask = localConnectingFacets(:,2) == 1 ;
+
+    neighboursThroughFacet1 = localElementNeighbours(firstFacetMask,2) ;
+
+    backEdgeMask = ismember(localElementNeighbours(:,1), neighboursThroughFacet1) ...
+        & ismember(localElementNeighbours(:,2), localStartElements) ;
+
+    localFacetsConnectingBack = localConnectingFacets( ...
+        backEdgeMask, ...
+        2 ...
+    ) ;
+
+    % TODO: find out why this does not work. The intention is to one column from each vertex array page.
+
+    secondVertices = verticesOfElementsWith4Neighbours(:,localFacetsConnectingBack,:)
+
+    % TODO: form linspaces between first verticves of start elements and opposing vertices in neighbours corresponding to localFacetsConnectingBack.
 
     % TODO: more things to come?
 
